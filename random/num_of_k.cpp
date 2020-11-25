3fc5b6fe574d808c09918a6bd8c6d2b63a310334
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-11-25 15:14:59 +0800

    find out the number of k in a sorted array

---
+++ random/num_of_k.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int GetFirstK(int* data, int length, int k, int start, int end);
+int GetLastK(int* data, int length, int k, int start, int end);
+int GetNumberOfK(int* data, int length, int k)
+{
+    int number = 0;
+
+    if(data != NULL && length > 0)
+    {
+        int first = GetFirstK(data, length, k, 0, length - 1);
+        int last = GetLastK(data, length, k, 0, length - 1);
+
+        if(first > -1 && last > -1)
+            number = last - first + 1;
+    }
+
+    return number;
+}
+//找第一个k
+int GetFirstK(int* data, int length, int k, int start, int end)
+{
+    if(start > end)
+        return -1;
+
+    int middleIndex = (start + end) / 2;
+    int middleData = data[middleIndex];
+
+    if(middleData == k)
+    {
+        if((middleIndex > 0 && data[middleIndex - 1] != k) 
+            || middleIndex == 0)
+            return middleIndex;
+        else
+            end  = middleIndex - 1;
+    }
+    else if(middleData > k)
+        end = middleIndex - 1;
+    else
+        start = middleIndex + 1;
+
+    return GetFirstK(data, length, k, start, end);
+}
+//找最后一个k
+int GetLastK(int* data, int length, int k, int start, int end)
+{
+    if(start > end)
+        return -1;
+
+    int middleIndex = (start + end) / 2;
+    int middleData = data[middleIndex];
+
+    if(middleData == k)
+    {
+        if((middleIndex < length - 1 && data[middleIndex + 1] != k) 
+            || middleIndex == length - 1)
+            return middleIndex;
+        else
+            start  = middleIndex + 1;
+    }
+    else if(middleData < k)
+        start = middleIndex + 1;
+    else
+        end = middleIndex - 1;
+
+    return GetLastK(data, length, k, start, end);
+}
+int main()
+{
+    int array[10] = {1,2,2,3,3,3,3,4,6,7};
+    cout << GetNumberOfK(array, 10, 2);
+    return 0;
+
+}
