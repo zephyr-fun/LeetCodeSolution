b03668de0b30b06f13a6c0cc1223f266528557fc
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-11-25 09:26:48 +0800

    step_up_dp version

---
+++ random/Step_up_dp.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int countDPStages(int n)
+{
+    
+    if(n == 1)
+        return 1;
+    
+    if(n == 2)
+        return 2;
+    
+    if(n == 3)
+        return 4;
+    
+    int a=1;
+    int b=2;
+    int c=4;
+    int temp = 0;
+    
+    for (int i = 4; i<=n; ++i)
+    {
+        temp = a+b+c;
+        a = b;
+        b = c;
+        c = temp;
+    }
+    
+    return temp;
+
+}
+int main()
+{
+    int num = 0;
+    cin >> num;
+    cout << countDPStages(num) << endl;
+    return 0;
+}
