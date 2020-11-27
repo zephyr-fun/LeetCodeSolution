98c1b409fdb0a4e708949645a74b8a5d590bafaa
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-11-27 18:53:13 +0800

    94_Binary_Tree_Inorder_Traversal

---
+++ tree/94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,45 @@
+/*** 
+ * Author: zephyr
+ * Date: 2020-11-27 17:12:12
+ * LastEditors: zephyr
+ * LastEditTime: 2020-11-27 18:49:30
+ * FilePath: \LeetCodeSolution\tree\94_Binary_Tree_Inorder_Traversal.cpp
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void inorder(TreeNode* root, vector<int>& res)
+{
+    if(!root)
+        return 0;
+    inorder(root->left, res);
+    res.push_back(root->val);
+    inorder(root->right, res);
+}
+
+vector<int> inorderTraversal(TreeNode* root)
+{
+    vector<int> res;
+    inorder(root, res);
+    return res;
+}
