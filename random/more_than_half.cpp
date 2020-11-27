4388fa138ff5ec69ef594ca2ec273b593d46b6ad
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-11-27 14:53:14 +0800

    raise another solution

--- random/more_than_half.cpp
+++ random/more_than_half.cpp
@@ -2,11 +2,12 @@
  * Author: zephyr
  * Date: 2020-11-25 09:21:22
  * LastEditors: zephyr
- * LastEditTime: 2020-11-25 17:10:06
+ * LastEditTime: 2020-11-27 14:51:46
  * FilePath: \LeetCodeSolution\random\more_than_half.cpp
  */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 int majorityElement(int* data,int nums) 
 {
@@ -23,10 +24,24 @@ int majorityElement(int* data,int nums)
     }
     return cur;
 }
+int majorityElement2(vector<int>& nums)
+{
+    int votes = 0;
+    int cur = 0;
+    for(int num : nums)
+    {
+        if(votes == 0)
+            cur = num;
+        votes += cur==num ? 1:-1;
+    }
+    return cur;
+}
 int main()
 {
     int data[15] = {1,2,3,2,5,2,7,2,8,2,2,9,2,0,2};
     int num = 15;
-    cout << majorityElement(data, num) << endl;
+    vector<int> datav(data,data+15);
+    // cout << majorityElement(data, num) << endl;
+    cout << majorityElement2(datav) << endl;
     return 0;
 }
