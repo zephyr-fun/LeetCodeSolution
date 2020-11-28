5260b408c99d576c52c3adc44e587d4b54cfa618
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-11-28 12:02:00 +0800

    add non recursion solution

--- tree/94_Binary_Tree_Inorder_Traversal.cpp
+++ tree/94_Binary_Tree_Inorder_Traversal.cpp
@@ -2,7 +2,7 @@
  * Author: zephyr
  * Date: 2020-11-27 17:12:12
  * LastEditors: zephyr
- * LastEditTime: 2020-11-27 18:49:30
+ * LastEditTime: 2020-11-28 11:59:53
  * FilePath: \LeetCodeSolution\tree\94_Binary_Tree_Inorder_Traversal.cpp
  */
 /**
@@ -18,7 +18,7 @@
  */
 #include <iostream>
 #include <vector>
-
+#include <stack>
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -27,7 +27,7 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
+//Recursion 
 void inorder(TreeNode* root, vector<int>& res)
 {
     if(!root)
@@ -42,4 +42,31 @@ vector<int> inorderTraversal(TreeNode* root)
     vector<int> res;
     inorder(root, res);
     return res;
+}
+//Non Recursion
+vector<int> inorderTraversal2(TreeNode* root)
+{
+    vector<int> result;
+    stack<pair<TreeNode*, int>> stk;
+    stk.push(make_pair(root, 0));
+    while(!stk.empty())
+    {
+        auto [node, type] = stk.top();
+        stk.pop();
+        if(node == nullptr)
+            continue;
+        if(type == 0)
+        {
+            stk.push(make_pair(node->right, 0));
+            stk.push(make_pair(node, 1));
+            stk.push(make_pair(node->left, 0));
+
+        }
+        else
+        {
+            result.emplace_back(node->val);
+        }
+        
+    }
+    return result;
 }
