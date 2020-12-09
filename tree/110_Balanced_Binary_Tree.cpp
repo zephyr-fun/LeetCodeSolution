6a9ebab0eb1d0472f6fac8cb01d5b211584be2e9
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-09 09:28:32 +0800

    110_balanced_binary_tree with recursion top<->bottom

---
+++ tree/110_Balanced_Binary_Tree.cpp
@@ -0,0 +1,51 @@
+/*** 
+ * Author: zephyr
+ * Date: 2020-12-09 08:59:00
+ * LastEditors: zephyr
+ * LastEditTime: 2020-12-09 09:27:23
+ * FilePath: \undefinedd:\GithubWorkSpace\LeetCodeSolution\tree\110_Balanced_Binary_Tree.cpp
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// recursion from top to bottom
+int height(TreeNode* root)
+{
+    if(!root)
+        return 0;
+    return max(height(root->left), height(root->right)) + 1;
+}
+bool isBalanced(TreeNode* root)
+{
+    if(!root)
+        return true;
+    return abs(height(root->left)-height(root->right)) <=1 && isBalanced(root->left) && isBalanced(root->right);
+}
+
+//recursion from bottom to top avoid repeated computing
+int height2(TreeNode* root)
+{
+    if(!root)
+        return 0;
+    int leftHeight = height2(root->left);
+    int rightHeight = height2(root->right);
+    if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight-rightHeight)>1)
+        return -1;
+    return max(leftHeight, rightHeight) + 1;
+}
+bool isBalanced2(TreeNode* root)
+{
+    return height(root) >= 0;
+}
