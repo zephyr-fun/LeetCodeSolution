55cd4c95493abd00e1893d9c879c66493b67f370
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-21 15:07:01 +0800

    198_house_robber with dp and less space dp

--- dp/198_House_Robber.cpp
+++ dp/198_House_Robber.cpp
@@ -2,9 +2,10 @@
  * Author: zephyr
  * Date: 2020-12-21 09:00:27
  * LastEditors: zephyr
- * LastEditTime: 2020-12-21 09:32:37
+ * LastEditTime: 2020-12-21 15:05:20
  * FilePath: \undefinedd:\GithubWorkSpace\LeetCodeSolution\dp\198_House_Robber.cpp
  */
+// dp
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -26,4 +27,26 @@ int rob(vector<int>& nums) {
         dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
     }
     return dp[size-1];
+}
+
+// less space
+int rob(vector<int>& nums)
+{
+    if(nums.empty())
+    {
+        return 0;
+    }
+    int size = nums.size();
+    if(size == 1)
+    {
+        return nums[0];
+    }
+    int first = nums[0], second = max(nums[0], nums[1]);
+    for(int i = 2; i < size; i++)
+    {
+        int temp = second;
+        second = max(first + nums[i], second);
+        first = temp;
+    }
+    return second;
 }
