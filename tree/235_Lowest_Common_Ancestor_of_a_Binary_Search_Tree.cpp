8afb9630ed39d89b78a9d73ea3e595bce03ded31
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-27 11:14:26 +0800

    235_lowest_commom_ancestor_of_a_binary_search_tree with two times traversal

---
+++ tree/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -0,0 +1,63 @@
+/*** 
+ * Author: zephyr
+ * Date: 2020-12-27 10:18:49
+ * LastEditors: zephyr
+ * LastEditTime: 2020-12-27 10:51:10
+ * FilePath: \tree\235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+// way 1 : two times traversal with memory usage
+void getPath(TreeNode* root, TreeNode* target, vector<TreeNode*>& path)
+{
+    if(root != target)
+    {
+        path.emplace_back(root);
+        if(root->val > target->val)
+        {
+            getPath(root->left, target, path);
+        }
+        else
+        {
+            getPath(root->right, target, path);
+        }
+    }
+}
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    vector<TreeNode*> p_path;
+    getPath(root, p, p_path);
+    p_path.emplace_back(p);
+    vector<TreeNode*> q_path;
+    getPath(root, q, q_path);
+    q_path.emplace_back(q);
+    TreeNode* res;
+    for(int i = 0; i < p_path.size() && i < q_path.size(); i++)
+    {
+        if(p_path[i] == q_path[i])
+        {
+            res = p_path[i];
+        }
+        else
+            break;
+    }
+    return res;
+}
