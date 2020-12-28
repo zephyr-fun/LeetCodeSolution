45b11088549aadf615e6322c02b7295ea8a03506
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-28 09:30:45 +0800

    98_validate_binary_search_tree with recursion & non_recursion inorder traserval

--- tree/98_Validate_Binary_Search_Tree.cpp
+++ tree/98_Validate_Binary_Search_Tree.cpp
@@ -2,7 +2,7 @@
  * Author: zephyr
  * Date: 2020-12-28 08:56:05
  * LastEditors: zephyr
- * LastEditTime: 2020-12-28 09:03:28
+ * LastEditTime: 2020-12-28 09:28:01
  * FilePath: \pytorch-fm-masterd:\GithubWorkSpace\LeetCodeSolution\tree\98_Validate_Binary_Search_Tree.cpp
  */
 #include <iostream>
@@ -20,7 +20,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
  
-//way 1 : recursion
+// way 1 : recursion
 bool helper(TreeNode* root, long long lower, long long upper)
 {
     if(root == nullptr)
@@ -32,4 +32,26 @@ bool helper(TreeNode* root, long long lower, long long upper)
 
 bool isValidBST(TreeNode* root) {
     return helper(root, LONG_MIN, LONG_MAX);
+}
+
+// way 2 : non_recursion inorder traversal
+bool isValidBST(TreeNode* root)
+{
+    stack<TreeNode*> stk;
+    long long pre_val = (long long)INT_MIN - 1;
+    while(!stk.empty() || root != nullptr)
+    {
+        while(root != nullptr)
+        {
+            stk.push(root);
+            root = root->left;
+        }
+        root = stk.top();
+        stk.pop();
+        if(root->val <= pre_val)
+            return false;
+        pre_val = root->val;
+        root = root->right;
+    }
+    return true;
 }
