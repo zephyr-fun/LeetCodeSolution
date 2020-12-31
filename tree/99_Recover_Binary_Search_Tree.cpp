2c8e03f36061d665e65238ac26f5656b1289a340
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-31 11:30:17 +0800

    99_recover_binary_search_tree with space : O(n)

---
+++ tree/99_Recover_Binary_Search_Tree.cpp
@@ -0,0 +1,67 @@
+/*** 
+ * Author: zephyr
+ * Date: 2020-12-31 10:44:52
+ * LastEditors: zephyr
+ * LastEditTime: 2020-12-31 11:28:39
+ * FilePath: \tree\99_Recover_Binary_Search_Tree.cpp
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// way 1 space : O(n)
+vector<int> res;
+void inorder(TreeNode* root)
+{
+    if(!root)
+        return ;
+    inorder(root->left);
+    res.emplace_back(root->val);
+    inorder(root->right);
+}
+pair<int, int> findswap(vector<int>& num)
+{
+    int x = -1;
+    int y = -1;
+    for(int i = 0; i < num.size() - 1; i++)
+    {
+        if(num[i+1] < num[i])
+        {
+            y = num[i+1];
+            if(x == -1)
+            {
+                x = num[i];
+            }
+        }
+    }
+    return {x, y};
+}
+void recover(TreeNode* root, int count, int x, int y)
+{
+    if(root != nullptr)
+    {
+        if(root->val == x || root->val == y)
+        {
+            root->val = root->val == x ? y : x;
+            if(--count == 0)
+                return ;
+        }
+    
+        recover(root->left, count, x, y);
+        recover(root->right, count, x, y);
+    }
+}
+void recoverTree(TreeNode* root) {
+    inorder(root);
+    pair<int, int> swapped =  findswap(res);
+    recover(root, 2, swapped.first, swapped.second);
+}
