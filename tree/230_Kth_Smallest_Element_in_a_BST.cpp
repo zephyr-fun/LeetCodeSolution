fb0edc35fa364000b64d69ab520dd4483b7c760c
zephyr_Fun <fanxuanzhe0106@gmail.com>
2020-12-31 09:46:58 +0800

    230_kth_smallest_element_in_a_bst with recursion & non recursion(early stop)

--- tree/230_Kth_Smallest_Element_in_a_BST.cpp
+++ tree/230_Kth_Smallest_Element_in_a_BST.cpp
@@ -2,7 +2,7 @@
  * Author: zephyr
  * Date: 2020-12-31 09:21:57
  * LastEditors: zephyr
- * LastEditTime: 2020-12-31 09:44:58
+ * LastEditTime: 2020-12-31 09:46:03
  * FilePath: \undefinedd:\GithubWorkSpace\LeetCodeSolution\tree\230_Kth_Smallest_Element_in_a_BST.cpp
  */
 #include <iostream>
@@ -33,4 +33,23 @@ void helper(TreeNode* root)
 int kthSmallest(TreeNode* root, int k) {
     helper(root);
     return res[k-1];
+}
+
+// way 2 non recursion
+int kthSmallest(TreeNode* root, int k)
+{
+    stack<TreeNode*> stk;
+    while(true)
+    {
+        while(root != nullptr)
+        {
+            stk.push(root);
+            root = root->left;
+        }
+        root = stk.top();
+        stk.pop();
+        if(--k == 0)
+            return root->val;
+        root = root->right;
+    }
 }
