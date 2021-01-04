4cdaf6c3a22cfc3761a1bd48b11bba536f9d3b5c
zephyr_Fun <fanxuanzhe0106@gmail.com>
2021-01-04 09:13:50 +0800

    101_symmetric_tree 2nd with recursion & non recursion

--- tree/101_Symmetric_Tree.cpp
+++ tree/101_Symmetric_Tree.cpp
@@ -2,7 +2,7 @@
  * Author: zephyr
  * Date: 2020-12-01 21:20:23
  * LastEditors: zephyr
- * LastEditTime: 2021-01-04 09:11:35
+ * LastEditTime: 2021-01-04 09:13:12
  * FilePath: \tree\101_Symmetric_Tree.cpp
  */
 #include <iostream>
@@ -58,7 +58,7 @@ bool isSymmetric2(TreeNode* root)
         q.pop();
         if(!u && !v)
             continue;
-        if((!u || !v) || u->val!=v->val)
+        if((!u || !v) || u->val!=v->val)// pay attension to !u || !v
             return false;
         q.push(u->left);
         q.push(v->right);
