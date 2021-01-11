93b8f7b3894c1a0e72251f6fb2f3b51b11b14fa7
zephyr_Fun <fanxuanzhe0106@gmail.com>
2021-01-11 22:27:04 +0800

    108_convert_sorted_array_to_binary_search_tree 2nd with recursion

--- tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
+++ tree/108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -2,8 +2,8 @@
  * Author: zephyr
  * Date: 2020-12-23 10:20:20
  * LastEditors: zephyr
- * LastEditTime: 2020-12-23 10:26:42
- * FilePath: \undefinedd:\GithubWorkSpace\LeetCodeSolution\tree\108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
+ * LastEditTime: 2021-01-11 22:25:57
+ * FilePath: \tree\108_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
  */
 #include <iostream>
 #include <vector>
@@ -18,12 +18,40 @@ using namespace std;
  * };
  */
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 TreeNode* dfs(vector<int>& nums, int left, int right)
 {
     if(left > right)
     {
         return nullptr;
     }
+    int mid = (left + right) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = dfs(nums, left, mid - 1);
+    root->right = dfs(nums, mid + 1, right);
+    return root;
+}
+TreeNode* sortedArrayToBST(vector<int>& nums)
+{
+    if(nums.empty())
+        return nullptr;
+    TreeNode* root = dfs(nums, 0, nums.size() - 1);
+    return root;
+}
+
+// 2nd
+TreeNode* dfs(vector<int>& nums, int left, int right)
+{
+    if(left > right)
+        return nullptr;
     int mid = (left + right) / 2;
     TreeNode* root = new TreeNode(nums[mid]);
     root->left = dfs(nums, left, mid - 1);
