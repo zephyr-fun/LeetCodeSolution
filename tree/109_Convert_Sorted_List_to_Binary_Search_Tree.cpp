3eb02a65d32d075d515fdb9f08a7ef99fe3f5cbd
zephyr_Fun <fanxuanzhe0106@gmail.com>
2021-01-12 22:29:49 +0800

    109_convert_sorted_list_to_binary_search_tree 2nd with fast-slow pointer

--- tree/109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
+++ tree/109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
@@ -2,32 +2,29 @@
  * Author: zephyr
  * Date: 2020-12-23 10:57:27
  * LastEditors: zephyr
- * LastEditTime: 2020-12-23 11:07:00
- * FilePath: \undefinedd:\GithubWorkSpace\LeetCodeSolution\tree\109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
+ * LastEditTime: 2021-01-12 22:28:11
+ * FilePath: \tree\109_Convert_Sorted_List_to_Binary_Search_Tree.cpp
  */
 #include <iostream>
 #include <vector>
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 ListNode* getMid(ListNode* left, ListNode* right)
 {
     ListNode* fast = left;
@@ -56,4 +53,34 @@ TreeNode* sortedListToBST(ListNode* head)
         return nullptr;
     TreeNode* root = dfs(head, nullptr);
     return root;
+}
+
+// 2nd
+ListNode* getMid(ListNode* left, ListNode* right)
+{
+    ListNode* fast = left;
+    ListNode* slow = left;
+    while(fast != right && fast->next != right)
+    {
+        fast = fast->next;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+TreeNode* dfs(ListNode* left, ListNode* right)
+{
+    if(left == right)
+        return nullptr;
+    ListNode* mid = getMid(left, right);
+    TreeNode* root = new TreeNode(mid->val);
+    root->left = dfs(left, mid);
+    root->right = dfs(mid->next, right);
+    return root;
+}
+TreeNode* sortedListToBST(ListNode* head)
+{
+    if(!head)
+        return nullptr;
+    return dfs(head,nullptr);
 }
