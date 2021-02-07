e5062ab359031cd46b77104878009b84e1748541
Zephyr-Fun <fanxuanzhe0106@gmail.com>
2021-02-07 22:11:10 +0800

    135 candy with bi-greedy

---
+++ greedy/135_Candy.cpp
@@ -0,0 +1,36 @@
+/*
+ * @Author: zephyr
+ * @Date: 2021-02-07 21:34:16
+ * @LastEditTime: 2021-02-07 22:08:54
+ * @LastEditors: zephyr
+ * @FilePath: \greedy\135_Candy.cpp
+ * @Description: 
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+int candy(vector<int>& ratings) {
+    int size = ratings.size();
+    if(size < 2)
+    {
+        return size;
+    }
+    vector<int> num(size, 1);
+    for(int i = 1; i < size; i++)
+    {
+        if(ratings[i] > ratings[i-1])
+        {
+            num[i] = num[i-1] + 1;
+        }
+    }
+    for(int i = size-1; i > 0; i--)
+    {
+        if(ratings[i] < ratings[i-1])
+        {
+            num[i-1] = max(num[i]+1, num[i-1]);
+        }
+    }
+    return accumulate(num.begin(), num.end(), 0);
+}
