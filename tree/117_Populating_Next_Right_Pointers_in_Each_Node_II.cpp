83efc09173272cf31c91d8a32c0293498b84d499
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-16 20:54:32 +0800

    Update 117_Populating_Next_Right_Pointers_in_Each_Node_II.cpp

--- tree/117_Populating_Next_Right_Pointers_in_Each_Node_II.cpp
+++ tree/117_Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -78,3 +78,31 @@ Node* connect2(Node* root)
     }
     return root;
 }
+
+// 2022.03.16
+class Solution {
+public:
+    Node* connect(Node* root){
+        if(root == nullptr){
+            return root;
+        }
+        Node* node = root;
+        while(node != nullptr){
+            Node* dummy = new Node(0);
+            Node* pre = dummy;
+            while(node != nullptr){
+                if(node->left != nullptr){
+                    pre->next = node->left;
+                    pre = pre->next;
+                }
+                if(node->right != nullptr){
+                    pre->next = node->right;
+                    pre = pre->next;
+                }
+                node = node->next;
+            }
+            node = dummy->next;
+        }
+        return root;
+    }
+};
