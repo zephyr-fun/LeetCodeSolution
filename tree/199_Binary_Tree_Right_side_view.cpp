4dbfe10221b95a2491912571b3a4f31518489578
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-16 20:54:35 +0800

    Update 199_Binary_Tree_Right_side_view.cpp

--- tree/199_Binary_Tree_Right_side_view.cpp
+++ tree/199_Binary_Tree_Right_side_view.cpp
@@ -83,3 +83,32 @@ vector<int> rightSideView(TreeNode* root)
     }
     return res;
 }
+
+// 2022.03.16
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        queue<TreeNode*> que;
+        if(root != nullptr){
+            que.push(root);
+        }
+        while(!que.empty()){
+            int size = que.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* node = que.front();
+                que.pop();
+                if(i == size - 1){
+                    res.push_back(node->val);
+                }
+                if(node->left != nullptr){
+                    que.push(node->left);
+                }
+                if(node->right != nullptr){
+                    que.push(node->right);
+                }
+            }
+        }
+        return res;
+    }
+};
