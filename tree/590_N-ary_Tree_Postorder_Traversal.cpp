ff0ca7fa907692bc37ca731fb865cd5aa80e8e90
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-17 19:04:21 +0800

    Create 590_N-ary_Tree_Postorder_Traversal.cpp

---
+++ tree/590_N-ary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,73 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+// recursion
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        postOrder(root, res);
+        return res;
+    }
+    void postOrder(Node* root, vector<int>& res) {
+        if(root == nullptr){
+            return ;
+        }
+        for(auto child : root->children){
+            if(child != nullptr){
+                postOrder(child, res);
+            }
+        }
+        res.push_back(root->val);
+    }
+};
+// non recursion
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        stack<Node*> st;
+        if(root != nullptr){
+            st.push(root);
+        }
+        while(!st.empty()){
+            Node* node = st.top();
+            if(node != nullptr){
+                st.pop();
+                //postorder;
+                st.push(node);
+                st.push(nullptr);
+                reverse(node->children.begin(), node->children.end());
+                for(auto child : node->children){
+                    if(child != nullptr){
+                        st.push(child);
+                    }
+                }
+                reverse(node->children.begin(), node->children.end());
+            }
+            else{
+                st.pop();
+                node = st.top();
+                st.pop();
+                res.push_back(node->val);
+            }
+        }
+        return res;
+    }
+};
