66ec5d39925c8632cb0f73aeefda4da04243dd98
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-19 09:46:00 +0800

    Update 222_Count_Complete_Tree_Nodes.cpp

--- tree/222_Count_Complete_Tree_Nodes.cpp
+++ tree/222_Count_Complete_Tree_Nodes.cpp
@@ -55,3 +55,29 @@ bool exists(TreeNode* root, int level, int mid)
     }
     return node != nullptr;
 }
+
+// 2022.03.18
+// utilize the feature of CBT
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr) return 0;
+        int leftHeight = 0;
+        int rightHeight = 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        while(left){
+            left = left->left;
+            leftHeight++;
+        }
+        while(right){
+            right = right->right;
+            rightHeight++;
+        }
+        // judge if a CBT is a FBT
+        if(leftHeight == rightHeight){
+            return (2 << leftHeight) - 1;
+        }
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
