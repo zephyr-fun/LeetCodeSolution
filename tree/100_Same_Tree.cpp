46c5fb704b60b48c046bdaa64fba470c55cebed4
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-19 09:35:20 +0800

    Update 100_Same_Tree.cpp

--- tree/100_Same_Tree.cpp
+++ tree/100_Same_Tree.cpp
@@ -67,3 +67,26 @@ bool isSameTree(TreeNode* p, TreeNode* q)
     }
     return queue1.empty() && queue2.empty();
 }
+
+// 2022.03.18
+// recursion
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr){
+            return true;
+        }
+        if(p != nullptr && q == nullptr){
+            return false;
+        }
+        if(p == nullptr && q!= nullptr){
+            return false;
+        }
+        if(p->val != q->val){
+            return false;
+        }
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        return left && right;
+    }
+};
