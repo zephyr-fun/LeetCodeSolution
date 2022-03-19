fc694e7b7f27b923a446cb4894790c27f036155b
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-19 09:43:16 +0800

    Create 559_Maximum_Depth_of_N-ary_Tree.cpp

---
+++ tree/559_Maximum_Depth_of_N-ary_Tree.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+// 2022.03.18
+//recursion
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == nullptr) return 0;
+        int res = 0;
+        for(auto child : root->children){
+            res = max(res, maxDepth(child));
+        }
+        // postorder traversal
+        return res + 1;
+    }
+};
+
+// backtrack
+class Solution {
+public:
+    int res;
+    int maxDepth(Node* root) {
+        res = 0;
+        if(root == nullptr) return res;
+        getDepth(root, 1);
+        return res;
+    }
+    void getDepth(Node* root, int depth){
+        // preorder traversal
+        res = depth > res ? depth : res;
+        if(root == nullptr) return;
+        for(auto child : root->children){
+            if(child != nullptr){
+                depth++;
+                getDepth(child, depth);
+                depth--;
+            }
+        }
+    }
+};
