cc6d949d5f7fc5ea8998c239a95a51de4219da36
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-21 22:21:27 +0800

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- tree/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ tree/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -72,3 +72,31 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     }
     return nullptr;
 }
+
+// 2022.03.21
+// recursion
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// postorder traversal == backtrack
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == p || root == q || root == nullptr){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left != nullptr && right != nullptr) return root;
+        
+        if(left != nullptr && right == nullptr) return left;
+        else if(right != nullptr && left == nullptr) return right;
+        else return nullptr; // right == nullptr && left == nullptr
+    }
+};
