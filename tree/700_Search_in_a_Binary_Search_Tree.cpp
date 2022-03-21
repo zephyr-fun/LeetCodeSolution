b7c2f32cf29e76fc79b0399c15f47ee606118b93
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-21 22:21:41 +0800

    Create 700_Search_in_a_Binary_Search_Tree.cpp

---
+++ tree/700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 2022.03.21
+// for any tree, recursion, did not utilize BST's feature
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root->val == val){
+            return root;
+        }
+        TreeNode* left = searchBST(root->left, val);
+        TreeNode* right = searchBST(root->right, val);
+        if(left != nullptr){
+            return left;
+        }
+        if(right != nullptr){
+            return right;
+        }
+        return nullptr;
+    }
+};
+// for BST, non recursion
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        TreeNode* node = root;
+        while(node != nullptr){
+            if(node->val == val){
+                return node;
+            }
+            else if(node->val > val){
+                node = node->left;
+            }
+            else if(node->val < val){
+                node = node->right;
+            }
+        }
+        return nullptr;
+    }
+};
+// for BST, recursion
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root->val == val){
+            return root;
+        }
+        if(root->val < val){
+            TreeNode* right = searchBST(root->right, val);
+            if(right != nullptr){
+                return right;
+            }
+        }
+        if(root->val > val){
+            TreeNode* left = searchBST(root->left, val);
+            if(left != nullptr){
+                return left;
+            }
+        }
+        return nullptr;
+    }
+};
