2660552a1699515b90f9b9f18ec867a6b10632f7
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-21 22:21:45 +0800

    Create 701_Insert_into_a_Binary_Search_Tree.cpp

---
+++ tree/701_Insert_into_a_Binary_Search_Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 2022.03.21
+// recursion
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        if(val < root->val){
+            root->left = insertIntoBST(root->left, val);
+        }
+        if(val > root->val){
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
