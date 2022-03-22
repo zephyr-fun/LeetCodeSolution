ca35d4e72253da818a19b65d37e66f34c806f303
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-22 21:58:03 +0800

    Create 669_Trim_a_Binary_Search_Tree.cpp

---
+++ tree/669_Trim_a_Binary_Search_Tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 2022.03.22
+// recursion
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root->val > high){
+            TreeNode* left = trimBST(root->left, low, high);
+            return left;
+        }
+        if(root->val < low){
+            TreeNode* right = trimBST(root->right, low, high);
+            return right;
+        }
+        root->left = trimBST(root->left, low, high); // reconstruct tree structure
+        root->right = trimBST(root->right, low, high); // reconstruct tree structure
+        return root;
+    }
+};
+
+// non recursion
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        while(root != nullptr && (root->val < low || root->val >high)){
+            if(root->val < low){
+                root = root->right;
+            }
+            else{
+                root = root->left;
+            }
+        }
+        // now root->val in [low, high]
+        // trim left sub tree
+        TreeNode* cur = root;
+        while(cur != nullptr){
+            while(cur->left != null && cur->left->val < low){
+                cur->left = cur->left->right;
+            }
+            cur = cur->left;
+        }
+        cur = root;
+        // trim right sub tree
+        while(cur != nullptr){
+            while(cur->right != nullptr && cur->right->val > high){
+                cur->right = cur->right->left;
+            }
+            cur = cur->right;
+        }
+        return root;
+    }
+};
