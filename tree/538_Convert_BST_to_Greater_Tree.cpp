ed4c54203329090de25d9af0a69ca10c0c641a94
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-22 21:54:02 +0800

    Create 538_Convert_BST_to_Greater_Tree.cpp

---
+++ tree/538_Convert_BST_to_Greater_Tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 2022.03.22
+// recursion, reverse inorder traversal
+class Solution {
+public:
+    TreeNode* pre;
+    TreeNode* convertBST(TreeNode* root) {
+        traversalSum(root);
+        return root;
+    }
+    void traversalSum(TreeNode* node){
+        if(node == nullptr){
+            return ;
+        }
+        traversalSum(node->right);
+        if(pre != nullptr){
+            node->val = node->val + pre->val;
+        }
+        pre = node;
+        //cout << "node value:" << node->val;
+        traversalSum(node->left);
+    }
+};
+// non recursion, reverse inorder traversal
+class Solution {
+public:
+    
+    TreeNode* convertBST(TreeNode* root) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        TreeNode* pre;
+        while(cur != nullptr || !st.empty()){
+            if(cur != nullptr){
+                st.push(cur);
+                cur = cur->right;
+            }
+            else{
+                cur = st.top();
+                st.pop();
+                if(pre != nullptr){
+                    cur->val = cur->val + pre->val;
+                }
+                pre = cur;
+                cur = cur->left;
+            }
+        }
+        return root;
+    }
+};
