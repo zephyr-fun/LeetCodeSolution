10a2b01c8c02a6f03d10ad6a3868d9be0fce3364
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-22 21:59:40 +0800

    Update 701_Insert_into_a_Binary_Search_Tree.cpp

--- tree/701_Insert_into_a_Binary_Search_Tree.cpp
+++ tree/701_Insert_into_a_Binary_Search_Tree.cpp
@@ -21,9 +21,38 @@ public:
         if(val < root->val){
             root->left = insertIntoBST(root->left, val);
         }
-        if(val > root->val){
+        if(val > root->val){ // else also works
             root->right = insertIntoBST(root->right, val);
         }
         return root;
+    }
+};
+
+// non recursion
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr){
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        TreeNode* head = root;
+        TreeNode* pre = nullptr;
+        while(root != nullptr){
+                pre = root;
+            if(root->val > val){
+                root = root->left;
+            }
+            else{
+                root = root->right;
+            }
+        }
+        if(val < pre->val){
+            pre->left = new TreeNode(val);
+        }
+        else{
+            pre->right = new TreeNode(val);
+        }
+        return head;
     }
 };
