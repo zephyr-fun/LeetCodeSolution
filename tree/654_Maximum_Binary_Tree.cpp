73546d9eab3d8e4c2159cd68cc040038d47625af
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-23 21:06:46 +0800

    Update 654_Maximum_Binary_Tree.cpp

--- tree/654_Maximum_Binary_Tree.cpp
+++ tree/654_Maximum_Binary_Tree.cpp
@@ -35,3 +35,31 @@ public:
         return node;
     }
 };
+
+// 2022.03.23
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        if(nums.size() == 0){
+            return nullptr;
+        }
+        TreeNode* root = build(nums, 0, nums.size() - 1);
+        return root;
+    }
+    TreeNode* build(vector<int>& nums, int left, int right){
+        if(left > right){
+            return nullptr;
+        }
+        int maxValue = INT_MIN;
+        int maxIndex = INT_MIN;
+        for(int i = left; i <= right; i++){
+            // first give value to index, or the maxValue will change
+            maxIndex = nums[i] > maxValue ? i : maxIndex;
+            maxValue = nums[i] > maxValue ? nums[i] : maxValue;
+        }
+        TreeNode* node = new TreeNode(maxValue);
+        node->left = build(nums, left, maxIndex - 1);
+        node->right = build(nums, maxIndex + 1, right);
+        return node;
+    }
+};
