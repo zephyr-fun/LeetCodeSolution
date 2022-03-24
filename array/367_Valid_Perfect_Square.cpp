bc5740156f11d5fdf55b907e3f8b3ddca7947b7a
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-24 21:14:24 +0800

    Create 367_Valid_Perfect_Square.cpp

---
+++ array/367_Valid_Perfect_Square.cpp
@@ -0,0 +1,22 @@
+// 2022.03.24
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 0;
+        int right = num;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if((long long)mid * mid > num){
+                right = mid - 1;
+            }
+            else if((long long)mid *mid < num){
+                left = mid + 1;
+            }
+            else{
+                // mid*mid == num
+                return true;
+            }
+        }
+        return false;
+    }
+};
