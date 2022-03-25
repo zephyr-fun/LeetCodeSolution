d45d1f96991ebe598d9918f270281a34c0345503
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-25 22:26:22 +0800

    Create 904_Fruit_Into_Baskets.cpp

---
+++ array/904_Fruit_Into_Baskets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left = 0;
+        int right = 0;
+        int leftKind = fruits[left];
+        int rightKind = fruits[right];
+        int res = 0;
+        while(right < fruits.size()){
+            if(fruits[right] == rightKind || fruits[right] == leftKind){
+                res = res > (right - left + 1) ? res : (right - left + 1);
+                right++;
+            }
+            else{
+                left = right - 1;
+                leftKind = fruits[left];
+                while(left > 0 && fruits[left - 1] == leftKind){// order matters "fruits[left - 1] == leftKind && left > 0" bound overflow
+                    left--;
+                }
+                rightKind = fruits[right];
+                res = res > (right - left + 1) ? res : (right - left + 1);
+            }
+        }
+        return res;
+    }
+};
