715a5707ef619ba4040b9148fc52e8619507124d
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-25 22:26:12 +0800

    Create 27_Remove_Element.cpp

---
+++ array/27_Remove_Element.cpp
@@ -0,0 +1,34 @@
+// 2022.03.25
+// fast slow pointer
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slowIndex = 0;
+        for(int fastIndex = 0; fastIndex < nums.size(); fastIndex++){
+            if(val != nums[fastIndex]){
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+};
+// Directional Pointer
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int leftIndex = 0;
+        int rightIndex = nums.size() - 1;
+        while(leftIndex <= rightIndex){
+            while(leftIndex <= rightIndex && nums[leftIndex] != val){
+                leftIndex++;
+            }
+            while(leftIndex <= rightIndex && nums[rightIndex] == val){
+                rightIndex--;
+            }
+            if(leftIndex < rightIndex){
+                nums[leftIndex++] = nums[rightIndex--];
+            }
+        }
+        return leftIndex;
+    }
+};
