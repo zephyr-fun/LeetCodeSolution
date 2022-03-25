269bd073b31f5e1d6b21b0ab154f86c26653315b
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-25 22:26:06 +0800

    Create 209_Minimum_Size_Subarray_Sum.cpp

---
+++ array/209_Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,20 @@
+// 2022.03.25
+// Sliding Window
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum = 0;
+        int res = INT_MAX;
+        int i = 0;
+        int subLength = 0;
+        for(int j = 0; j < nums.size(); j++){
+            sum += nums[j];
+            while(sum >= target){
+                subLength = j - i + 1;
+                res = res < subLength ? res : subLength;
+                sum -= nums[i++];
+            }
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
