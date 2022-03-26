4538a879e75b29e90fccf8953d9d57a34c26c622
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-26 22:15:49 +0800

    Create 142_Linked_List_Cycle_II.cpp

---
+++ linklist/142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){ // cause fast goes two steps one time step, you have to make sure fast and fast->next != nullptr, so that fast->next->next won't raise error
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                ListNode* index1 = slow;
+                ListNode* index2 = head;
+                while(index1 != index2){
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
+    }
+};
