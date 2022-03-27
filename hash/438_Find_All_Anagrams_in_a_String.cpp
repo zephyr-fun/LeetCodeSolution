3eb725bf84c570dbf8a295a80ce38a80799d3776
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-27 21:50:05 +0800

    Create 438_Find_All_Anagrams_in_a_String.cpp

---
+++ hash/438_Find_All_Anagrams_in_a_String.cpp
@@ -0,0 +1,54 @@
+// 2022.03.27
+// over time
+// class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         vector<int> res;
+//         if(s.size() < p.size())
+//             return res;
+//         string pattern = p;
+//         sort(pattern.begin(), pattern.end());
+//         for(int i = 0; i < s.size() - p.size() + 1; i++){
+//             string sub = s.substr(i, p.size());
+//             sort(sub.begin(), sub.end());
+//             if(sub == pattern){
+//                 res.push_back(i);
+//             }
+//         }
+//         return res;
+//     }
+// };
+// sliding window
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res;
+        if(s.size() < p.size())
+            return res;
+        int sizeS = s.size();
+        int sizeP = p.size();
+        int pCount[26] = {0};
+        for(int i = 0; i < sizeP; i++){
+            pCount[p[i] - 'a']++;
+        }
+        int pIden = 0;
+        for(int i = 0; i < 26; i++){
+            if(pCount[i] != 0){
+                pIden++;
+            }
+        }
+        for(int left = 0, right = 0, sIden = 0; right < sizeS; right++){
+            if(--pCount[s[right] - 'a'] == 0){
+                sIden++;
+            }
+            if(right - left + 1 > sizeP && ++pCount[s[left++] - 'a'] == 1){
+                sIden--;
+            }
+            if(pIden == sIden){
+                res.push_back(left);
+            }
+            //cout << "piden" << pIden << "siden" << sIden;
+        }
+        return res;
+    }
+};
