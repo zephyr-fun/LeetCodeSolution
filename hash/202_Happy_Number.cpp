2245ce1d33a7c6c59aeaf17006ebbf0b69a901b9
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-27 21:53:21 +0800

    Create 202_Happy_Number.cpp

---
+++ hash/202_Happy_Number.cpp
@@ -0,0 +1,27 @@
+// 2022.03.27
+class Solution {
+public:
+    int getSum(int n){
+        int sum = 0;
+        while(n != 0){
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(1){
+            int sum = getSum(n);
+            if(sum == 1){
+                return true;
+            }
+            if(set.find(sum) != set.end()){
+                return false;
+            }
+            set.insert(sum);
+            n = sum;
+        }
+        
+    }
+};
