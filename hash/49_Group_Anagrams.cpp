cf306836a71af2173a432220174e4b044c4ac230
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-27 21:48:49 +0800

    Create 49_Group_Anagrams.cpp

---
+++ hash/49_Group_Anagrams.cpp
@@ -0,0 +1,17 @@
+// 2022.03.27
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> map;
+        for(int i = 0; i < strs.size(); i++){
+            string key = strs[i];
+            sort(key.begin(), key.end());// find hash map, from any word to uni representation
+            map[key].push_back(strs[i]);
+        }
+        for(auto it = map.begin(); it != map.end(); it++){
+            res.push_back(it->second);
+        }
+        return res;
+    }
+};
