0511b8a876157da10cc78df950e2786934713723
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-28 22:27:41 +0800

    Update 383_Ransom_Note.cpp

--- hash/383_Ransom_Note.cpp
+++ hash/383_Ransom_Note.cpp
@@ -17,3 +17,22 @@ public:
         return true;
     }
 };
+// 2022.03.28
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int record[26] = {0};
+        for(int i = 0; i < ransomNote.size(); i++){
+            record[ransomNote[i] - 'a']++;
+        }
+        for(int i = 0; i < magazine.size(); i++){
+            record[magazine[i] - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(record[i] > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
