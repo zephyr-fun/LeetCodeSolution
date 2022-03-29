0f933dd2f51e9f8f120b743512b84eb0bbad8022
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-29 20:02:39 +0800

    Update 454_4Sum_II.cpp

--- hash/454_4Sum_II.cpp
+++ hash/454_4Sum_II.cpp
@@ -19,3 +19,24 @@ public:
         return res;
     }
 };
+// 2022.03.29
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> map; // a + b, count;
+        int res = 0;
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                map[nums1[i] + nums2[j]]++;
+            }
+        }
+        for(int i = 0; i < nums3.size(); i++){
+            for(int j = 0; j < nums4.size(); j++){
+                if(map.find(0 - nums3[i] - nums4[j]) != map.end()){
+                    res += map[0 - nums3[i] - nums4[j]];
+                }
+            }
+        }
+        return res;
+    }
+};
