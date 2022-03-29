0038f8b3e881812839c9fa18c6a79f9189a8bf97
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-29 20:02:57 +0800

    Update 15_3Sum.cpp

--- hash/15_3Sum.cpp
+++ hash/15_3Sum.cpp
@@ -46,3 +46,51 @@ public:
         return res;
     }
 };
+// 2022.03.29
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() == 0){
+            return res;
+        }
+        sort(nums.begin(), nums.end());
+        if(nums[0] > 0){
+            return res;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            int temp = 0 - nums[i];
+            while(left < right){
+                if(nums[left] + nums[right] > temp){
+                    right--;
+                    while(right > left && nums[right] == nums[right + 1]){
+                        right--;
+                    }
+                }
+                else if(nums[left] + nums[right] < temp){
+                    left++;
+                    while(right > left && nums[left] == nums[left - 1]){
+                        left++;
+                    }
+                }
+                else{
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while(right > left && nums[left] == nums[left - 1]){
+                        left++;
+                    }
+                    while(right > left && nums[right] == nums[right + 1]){
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
