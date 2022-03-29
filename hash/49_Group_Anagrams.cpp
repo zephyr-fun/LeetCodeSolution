d6952d3d45eddd81c680b04a5bcc4564b8f8b27f
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-29 20:02:36 +0800

    Update 49_Group_Anagrams.cpp

--- hash/49_Group_Anagrams.cpp
+++ hash/49_Group_Anagrams.cpp
@@ -15,3 +15,20 @@ public:
         return res;
     }
 };
+// 2022.03.29
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> map;
+        for(int i = 0; i < strs.size(); i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            map[temp].push_back(strs[i]);
+        }
+        for(auto it = map.begin(); it != map.end(); it++){
+            res.push_back(it->second);
+        }
+        return res;
+    }
+};
