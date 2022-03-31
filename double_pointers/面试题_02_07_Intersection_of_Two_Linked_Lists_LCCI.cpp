c5ac8f24404b4c7c610d56563c58a6bfac2a1c61
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-31 21:52:17 +0800

    Create 面试题_02_07_Intersection_of_Two_Linked_Lists_LCCI.cpp

---
+++ double_pointers/面试题_02_07_Intersection_of_Two_Linked_Lists_LCCI.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// 2022.03.31
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA = 0;
+        int lenB = 0;
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        while(curA != nullptr){
+            lenA++;
+            curA = curA->next;
+        }
+        while(curB != nullptr){
+            lenB++;
+            curB = curB->next;
+        }
+        curA = headA;
+        curB = headB;
+        if(lenA < lenB){
+            swap(curA, curB);
+            swap(lenA, lenB);
+        }
+        int gap = lenA - lenB;
+        while(gap--){
+            curA = curA->next;
+        }
+        while(curA != nullptr){
+            if(curA == curB){
+                return curA;
+            }
+            else{
+                curA = curA->next;
+                curB = curB->next;
+            }
+        }
+        return nullptr;
+    }
+};
