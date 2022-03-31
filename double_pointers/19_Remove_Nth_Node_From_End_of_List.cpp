656f85a76f20c511258dd754dabdc711bbdef748
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-31 21:51:34 +0800

    Create 19_Remove_Nth_Node_From_End_of_List.cpp

---
+++ double_pointers/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+        while(n-- && fast != nullptr){
+            fast = fast->next;
+        }
+        fast = fast->next;
+        while(fast != nullptr){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+        return dummy->next;
+    }
+};
