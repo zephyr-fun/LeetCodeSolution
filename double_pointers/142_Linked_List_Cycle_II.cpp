ee78fc2fa60bfe93d5bb43fdb8babfc10c3d88ea
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-03-31 21:52:56 +0800

    Create 142_Linked_List_Cycle_II.cpp

---
+++ double_pointers/142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// 2022.03.31
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                ListNode* index1 = slow;
+                ListNode* index2 = head;
+                while(index1 != index2){
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index1;
+            }
+        }
+        return nullptr;
+    }
+};
