4a78745995b8cfd4701fbcd625f49383333d62ce
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-04-02 11:35:23 +0800

    Create 17_Letter_Combinations_of_a_Phone_Number.cpp

---
+++ backtrack/17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -0,0 +1,39 @@
+// 2022.04.01
+class Solution {
+private: 
+    const string letterMap[10] = {
+        "",     //0
+        "",     //1
+        "abc",  //2
+        "def",  //3
+        "ghi",  //4
+        "jkl",  //5
+        "mno",  //6
+        "pqrs", //7
+        "tuv",  //8
+        "wxyz"  //9
+    };
+    vector<string> res;
+    string path;
+public:
+    void backtracking(string digits, int index){
+        if(index == digits.size()){
+            res.push_back(path);
+            return ;
+        }
+        int digit = digits[index] - '0';
+        string letters = letterMap[digit];
+        for(int i = 0; i < letters.size(); i++){
+            path.push_back(letters[i]);
+            backtracking(digits, index + 1);
+            path.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0){
+            return res;
+        }
+        backtracking(digits, 0);
+        return res;
+    }
+};
