3ca9d9c9a22ff67a0f343996d47e99b92e45a2ac
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-04-02 09:50:39 +0800

    Create 20_Valid_Parentheses.cpp

---
+++ stack&queue/20_Valid_Parentheses.cpp
@@ -0,0 +1,25 @@
+// 2022.04.01
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(')');
+            }
+            else if(s[i] == '['){
+                st.push(']');
+            }
+            else if(s[i] == '{'){
+                st.push('}');
+            }
+            else if(st.empty() || st.top() != s[i]){
+                return false;
+            }
+            else{
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
