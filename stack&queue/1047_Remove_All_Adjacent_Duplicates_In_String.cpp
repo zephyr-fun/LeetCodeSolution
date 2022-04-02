4eab0f5c5eca31523c138741f23294765c9ee586
fan-xuan-zhe <fanxuanzhe0106@gmail.com>
2022-04-02 11:26:35 +0800

    Create 1047_Remove_All_Adjacent_Duplicates_In_String.cpp

---
+++ stack&queue/1047_Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,36 @@
+// 2022.04.01
+class Solution {
+public:
+    void subReverse(string& s, int start, int end){
+        int left = start;
+        int right = end;
+        while(left < right){
+            s[left] ^= s[right];
+            s[right] ^= s[left];
+            s[left] ^= s[right];
+            left++;
+            right--;
+        }
+    }
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else if(st.top() == s[i]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        string res;
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        subReverse(res, 0, res.size() - 1);
+        return res;
+    }
+};
