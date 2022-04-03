e71eceb5434eb6d00c96a54a66d49e33f48df4b1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-03 21:20:32 +0800

    Update 491_Increasing_Subsequences.cpp

--- backtrack/491_Increasing_Subsequences.cpp
+++ backtrack/491_Increasing_Subsequences.cpp
@@ -23,3 +23,29 @@ public:
         return res;
     }
 };
+
+// optim space usage
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex){
+        if(path.size() > 1){
+            res.push_back(path);
+        }
+        int uset[201] = {0};
+        for(int i = startIndex; i < nums.size(); i++){
+            if((!path.empty() && nums[i] < path.back()) || uset[nums[i] + 100] != 0){
+                continue;
+            }
+            uset[nums[i] + 100] = 1;
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+};
