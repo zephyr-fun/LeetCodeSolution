c163b1311d25b358e54afb4ce31a21059b015fee
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:14:34 +0800

    Update 78_Subsets.cpp

--- backtrack/78_Subsets.cpp
+++ backtrack/78_Subsets.cpp
@@ -19,3 +19,27 @@ public:
         return res;
     }
 };
+
+// 2022.04.04
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex){
+        res.push_back(path);
+        if(startIndex >= nums.size()){ // avoid infinite recursion
+            return ;
+        }
+        for(int i = startIndex; i < nums.size(); i++){
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+};
+
+// bit operation
