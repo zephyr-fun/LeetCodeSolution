1289a135cb7f41fee7aff7e0ca19d461b091787e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:14:40 +0800

    Update 46_Permutations.cpp

--- backtrack/46_Permutations.cpp
+++ backtrack/46_Permutations.cpp
@@ -27,3 +27,31 @@ public:
         return res;
     }
 };
+
+// 2022.04.04
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& nums, vector<bool>& used){
+        if(path.size() == nums.size()){
+            res.push_back(path);
+            return ;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(used[i] == true){
+                continue;
+            }
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtracking(nums, used);
+            used[i] = false;
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        backtracking(nums, used);
+        return res;
+    }
+};
