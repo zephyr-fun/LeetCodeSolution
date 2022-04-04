be40d1d702413284128a52cc4f1cf687084903b2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:13:41 +0800

    Update 77_Combinations.cpp

--- backtrack/77_Combinations.cpp
+++ backtrack/77_Combinations.cpp
@@ -19,3 +19,25 @@ public:
         }
     }
 };
+
+// 2022.04.04
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(int n, int k, int startIndex){
+        if(path.size() == k){
+            res.push_back(path);
+            return ;
+        }
+        for(int i = startIndex; i <= n - (k - path.size()) + 1; i++){
+            path.push_back(i);
+            backtracking(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return res;
+    }
+};
