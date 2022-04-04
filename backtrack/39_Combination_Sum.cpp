f6f2b33b5ef0927f424e16d5292ce22deb2623ee
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:13:59 +0800

    Update 39_Combination_Sum.cpp

--- backtrack/39_Combination_Sum.cpp
+++ backtrack/39_Combination_Sum.cpp
@@ -59,3 +59,61 @@ public:
         return res;
     }
 };
+
+// 2022.04.04
+// original version
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int startIndex, int sum){
+        if(sum > target){
+            return ;
+        }
+        if(sum == target){
+            res.push_back(path);
+            return ;
+        }
+        for(int i = startIndex; i < candidates.size(); i++){
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, i, sum);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        if(candidates.size() == 0){
+            return res;
+        }
+        backtracking(candidates, target, 0, 0);
+        return res;
+    }
+};
+// optim
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int startIndex, int sum){
+        if(sum == target){
+            res.push_back(path);
+            return ;
+        }
+        for(int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++){
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, i, sum);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        if(candidates.size() == 0){
+            return res;
+        }
+        sort(candidates.begin(), candidates.end()); // along with '&& sum + candidates[i] <= target', only when the vector is ordered, the before code works
+        backtracking(candidates, target, 0, 0);
+        return res;
+    }
+};
