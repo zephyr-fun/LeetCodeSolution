99d0e51b66a6ff90c6ce5dc913c872861bbf24b3
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:13:47 +0800

    Update 216_Combination_Sum_III.cpp

--- backtrack/216_Combination_Sum_III.cpp
+++ backtrack/216_Combination_Sum_III.cpp
@@ -25,3 +25,32 @@ public:
         }
     }
 };
+
+// 2022.04.04
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(int k, int n, int startIndex, int sum){
+        if(sum > n){
+            return ;
+        }
+        if(k == path.size()){
+            if(sum == n){
+                res.push_back(path);
+            }
+            return ;
+        }
+        for(int i = startIndex; i <= 9 - (k - path.size()) + 1; i++){ // notice '<='
+            sum += i;
+            path.push_back(i);
+            backtracking(k, n, i + 1, sum);
+            sum -= i;
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtracking(k, n, 1, 0);
+        return res;
+    }
+};
