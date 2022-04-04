fbd5113f18f9f13a523b38b84bd313da479c6d3a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:14:04 +0800

    Update 40_Combination_Sum_II.cpp

--- backtrack/40_Combination_Sum_II.cpp
+++ backtrack/40_Combination_Sum_II.cpp
@@ -31,8 +31,38 @@ public:
         }
         vector<bool> used(candidates.size(), false);
         sort(candidates.begin(), candidates.end());
-        backtracking(candidates, target, 0, 0, used);
-        
+        backtracking(candidates, target, 0, 0, used);     
+        return res;
+    }
+};
+
+// 2022.04.04
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex){
+        if(sum == target){
+            res.push_back(path);
+            return ;
+        }
+        for(int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++){
+            if(i > startIndex && candidates[i] == candidates[i - 1]){// same layer cut
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        if(candidates.size() == 0){
+            return res;
+        }
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0);
         return res;
     }
 };
