e7b4cecda252d75ee8d955b53161425862e3806f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-04 21:13:52 +0800

    Update 17_Letter_Combinations_of_a_Phone_Number.cpp

--- backtrack/17_Letter_Combinations_of_a_Phone_Number.cpp
+++ backtrack/17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -37,3 +37,42 @@ public:
         return res;
     }
 };
+
+// 2022.04.04
+class Solution {
+public:
+    const string letterMap[10] = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+    vector<string> res;
+    string path;
+    void backtracking(string& digits, int index){
+        if(index == digits.size()){
+            res.push_back(path);
+            return ;
+        }
+        int digit = digits[index] - '0';
+        string letters = letterMap[digit];
+        for(int i = 0; i < letters.size(); i++){
+            path.push_back(letters[i]);
+            backtracking(digits, index + 1);
+            path.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0){
+            return res;
+        }
+        backtracking(digits, 0);
+        return res;
+    }
+};
