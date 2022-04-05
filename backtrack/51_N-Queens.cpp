cff7591d3532ebe84e1f04d873c5fdf6d94d38b9
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-05 21:29:39 +0800

    Update 51_N-Queens.cpp

--- backtrack/51_N-Queens.cpp
+++ backtrack/51_N-Queens.cpp
@@ -42,3 +42,45 @@ public:
         return res;
     }
 };
+
+// 2022.04.05
+class Solution {
+public:
+    vector<vector<string>> res;
+    bool isValid(int row, int col, vector<string>& path, int n){
+        for(int i = 0; i < row; i++){
+            if(path[i][col] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row - 1, j = col - 1; i >= 0 && j >=0; i--, j--){
+            if(path[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i = row - 1, j = col + 1; i >=0 && j < n; i--, j++){
+            if(path[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void backtracking(int n, int row, vector<string>& path){
+        if(row == n){
+            res.push_back(path);
+            return ;
+        }
+        for(int col = 0; col < n; col++){
+            if(isValid(row, col, path, n)){
+                path[row][col] = 'Q';
+                backtracking(n, row + 1, path);
+                path[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> path(n, string(n, '.'));
+        backtracking(n, 0, path);
+        return res;
+    }
+};
