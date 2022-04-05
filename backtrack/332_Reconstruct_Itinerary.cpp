b951b1b812c056768e05b38d4b044695ad8f1f84
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-05 21:29:31 +0800

    Update 332_Reconstruct_Itinerary.cpp

--- backtrack/332_Reconstruct_Itinerary.cpp
+++ backtrack/332_Reconstruct_Itinerary.cpp
@@ -30,3 +30,36 @@ public:
         return path;
     }
 };
+
+// 2022.04.05
+class Solution {
+private:
+    unordered_map<string, map<string, int>> targets;
+    vector<string> path;
+    bool backtracking(int ticketNum, vector<string>& path){
+        if(path.size() == ticketNum + 1){
+            return true;
+        }
+        for(pair<const string, int>& target : targets[path[path.size() - 1]]){
+            if(target.second > 0){
+                path.push_back(target.first);
+                target.second--;
+                if(backtracking(ticketNum, path)){
+                    return true;
+                }
+                path.pop_back();
+                target.second++;
+            }
+        }
+        return false;
+    }
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(const vector<string>& vec : tickets){
+            targets[vec[0]][vec[1]]++;
+        }
+        path.push_back("JFK");
+        backtracking(tickets.size(), path);
+        return path;
+    }
+};
