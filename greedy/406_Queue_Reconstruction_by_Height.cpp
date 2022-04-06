7f1644443dfe05affd0acc1610adf481f6b4af33
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-06 21:35:06 +0800

    Create 406_Queue_Reconstruction_by_Height.cpp

---
+++ greedy/406_Queue_Reconstruction_by_Height.cpp
@@ -0,0 +1,44 @@
+// 2022.04.06
+// version 1 with vector, insert operation cost time
+class Solution {
+public:
+    static bool cmp(const vector<int>& a, const vector<int>& b){
+        if(a[0] == b[0]){
+            return a[1] < b[1];
+        }
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> queue;
+        for(int i = 0; i < people.size(); i++){
+            int position = people[i][1];
+            queue.insert(queue.begin() + position, people[i]);
+        }
+        return queue;
+    }
+};
+
+// version 2 with listnode
+class Solution {
+public:
+    static bool cmp(const vector<int>& a, const vector<int>& b){
+        if(a[0] == b[0]){
+            return a[1] < b[1];
+        }
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        list<vector<int>> queue; // stl
+        for(int i = 0; i < people.size(); i++){
+            int position = people[i][1];
+            list<vector<int>>::iterator it = queue.begin(); // iterator it
+            while(position--){
+                it++;
+            }
+            queue.insert(it, people[i]); // insert of list
+        }
+        return vector<vector<int>> (queue.begin(), queue.end());
+    }
+};
