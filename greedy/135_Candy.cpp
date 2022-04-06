7048faf94c3583b6c0d32ad1e9d3875e7081c7e5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-06 21:34:58 +0800

    Update 135_Candy.cpp

--- greedy/135_Candy.cpp
+++ greedy/135_Candy.cpp
@@ -34,3 +34,26 @@ int candy(vector<int>& ratings) {
     }
     return accumulate(num.begin(), num.end(), 0);
 }
+
+// 2022.04.06
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candyVec(ratings.size(), 1);
+        for(int i = 1; i < ratings.size(); i++){
+            if(ratings[i] > ratings[i - 1]){
+                candyVec[i] = candyVec[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.size() - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                candyVec[i] = max(candyVec[i], candyVec[i + 1] + 1); // max(candyVec[i], candyVec[i + 1] + 1)
+            }
+        }
+        int res = 0;
+        for(int i = 0; i < ratings.size(); i++){
+            res += candyVec[i];
+        }
+        return res;
+    }
+};
