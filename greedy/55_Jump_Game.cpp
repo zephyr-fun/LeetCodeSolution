3baf4b13c0ad38bf4a1623a47be90e1107d5d820
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-06 21:34:28 +0800

    Create 55_Jump_Game.cpp

---
+++ greedy/55_Jump_Game.cpp
@@ -0,0 +1,17 @@
+// 2022.04.06
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int cover = 0;
+        if(nums.size() == 1){
+            return true;
+        }
+        for(int i = 0; i <= cover; i++){
+            cover = max(i + nums[i], cover);
+            if(cover >= nums.size() - 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
