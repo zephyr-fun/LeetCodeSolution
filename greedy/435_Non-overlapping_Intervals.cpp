37735d0327b28c24abcd8831a575c2a744550fc8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-07 15:10:47 +0800

    Update 435_Non-overlapping_Intervals.cpp

--- greedy/435_Non-overlapping_Intervals.cpp
+++ greedy/435_Non-overlapping_Intervals.cpp
@@ -33,3 +33,26 @@ int eraseOverlapIntervals(vector<vector<int>>& intervals)
     }
     return total;
 }
+
+// 2022.04.07
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1] < b[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if(intervals.size() == 0){
+            return 0;
+        }
+        sort(intervals.begin(), intervals.end(), cmp);
+        int count = 1;
+        int end = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++){
+            if(end <= intervals[i][0]){
+                end = intervals[i][1]; // right bounder min, cause already sorted
+                count++;
+            }
+        }
+        return intervals.size() - count;
+    }
+};
