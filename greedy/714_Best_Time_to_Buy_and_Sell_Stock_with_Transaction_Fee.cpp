5f35afe3daa006d80dba0135356dc07bc2c91c53
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-07 15:15:48 +0800

    Create 714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp

---
+++ greedy/714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
@@ -0,0 +1,18 @@
+// 2022.04.07
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int res = 0;
+        int minPrice = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            if(prices[i] > minPrice + fee){
+                res += prices[i] - minPrice - fee;
+                minPrice = prices[i] - fee;
+            }
+        }
+        return res;
+    }
+};
