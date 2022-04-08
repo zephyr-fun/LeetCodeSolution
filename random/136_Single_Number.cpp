78a544b131b772788fff1d2c492b096cc679d203
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-08 22:34:17 +0800

    Create 136_Single_Number.cpp

---
+++ random/136_Single_Number.cpp
@@ -0,0 +1,11 @@
+// 2022.04.08
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto num : nums){
+            res ^= num;
+        }
+        return res;
+    }
+};
