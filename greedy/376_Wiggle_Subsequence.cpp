338933afeaff4e1316ee6dc95179f0db9614c23f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-08 10:10:19 +0800

    Update 376_Wiggle_Subsequence.cpp

--- greedy/376_Wiggle_Subsequence.cpp
+++ greedy/376_Wiggle_Subsequence.cpp
@@ -8,7 +8,7 @@ public:
         int curDiff = 0;
         int preDiff = 0;
         int res = 1;
-        for(int i = 0; i <nums.size() - 1; i++){
+        for(int i = 0; i < nums.size() - 1; i++){
             curDiff = nums[i + 1] - nums[i];
             if(preDiff <= 0 && curDiff > 0 || preDiff >= 0 && curDiff < 0){
                 res++;
