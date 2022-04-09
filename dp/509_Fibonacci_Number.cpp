b8c40449618afa35677e5bd83047ae3658401432
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-09 22:34:09 +0800

    Create 509_Fibonacci_Number.cpp

---
+++ dp/509_Fibonacci_Number.cpp
@@ -0,0 +1,50 @@
+// 2022.04.09
+// recursion
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+// dp
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+// dp optim space
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        int dp[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            int sum = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = sum;
+        }
+        return dp[1];
+    }
+};
