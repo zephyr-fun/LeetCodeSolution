894e1703cd3e21acad56ec654e8c71f404367ced
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-09 22:35:04 +0800

    Create 62_Unique_Paths.cpp

---
+++ dp/62_Unique_Paths.cpp
@@ -0,0 +1,38 @@
+// 2022.04.09
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+// zuhe
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long numerator = 1; // 分子
+        int denominator = m - 1; // 分母
+        int count = m - 1;
+        int t = m + n - 2;
+        while (count--) {
+            numerator *= (t--);
+            while (denominator != 0 && numerator % denominator == 0) {
+                numerator /= denominator;
+                denominator--;
+            }
+        }
+        return numerator;
+    }
+};
