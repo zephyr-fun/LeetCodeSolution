bb2bd20dc8d4f95a2c16786c343a33c27e3ed3f1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-09 22:35:11 +0800

    Create 63_Unique_Paths_II.cpp

---
+++ dp/63_Unique_Paths_II.cpp
@@ -0,0 +1,23 @@
+// 2022.04.09
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector(n, 0));
+        for(int i = 0; i < m && obstacleGrid[i][0] ==0; i++){ // same as column
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n && obstacleGrid[0][j] ==0; j++){ // one way column path, if [row-1][0] gotta a obstacle, you cant go to [row][0]
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
