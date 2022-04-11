b73552f3735678dde9855ebc85c62460fc028aac
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-11 22:24:12 +0800

    Create 518_Coin_Change_2.cpp

---
+++ dp/518_Coin_Change_2.cpp
@@ -0,0 +1,18 @@
+// 2022.04.11
+// order matters
+// under the situation of complete bag
+// objects first, bagweight second: combination
+// bagweight first, objects second: arrange
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); i++){
+            for(int j = coins[i]; j <= amount; j++){
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
