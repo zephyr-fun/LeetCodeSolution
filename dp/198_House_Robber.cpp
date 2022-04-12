b8e3898b245b76bfa961fbd5295739beb85e0cef
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-12 21:08:34 +0800

    Update 198_House_Robber.cpp

--- dp/198_House_Robber.cpp
+++ dp/198_House_Robber.cpp
@@ -50,3 +50,20 @@ int rob(vector<int>& nums)
     }
     return second;
 }
+
+// 2022.04.12
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() < 2){
+            return nums[0];
+        }
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.size() - 1];
+    }
+};
