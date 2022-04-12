b89d5d530d4cf405ad7a9a99cca6a6439c46cfc0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-12 21:09:04 +0800

    Update 213_House_Robber_II.cpp

--- dp/213_House_Robber_II.cpp
+++ dp/213_House_Robber_II.cpp
@@ -51,3 +51,31 @@ int dynamic(vector<int>& nums, int start, int end)
     }
     return second;
 }
+
+// 2022.04.12
+class Solution {
+public:
+    int rob(vector<int>& nums){
+        if(nums.size() == 0){
+            return 0;
+        }
+        if(nums.size() == 1){
+            return nums[0];
+        }
+        int situation1 = robRange(nums, 0, nums.size() - 2);
+        int situation2 = robRange(nums, 1, nums.size() - 1);
+        return max(situation1, situation2);
+    }
+    int robRange(vector<int>& nums, int start, int end){
+        if(start == end){
+            return nums[start];
+        }
+        vector<int> dp(nums.size(), 0);
+        dp[start] = nums[start];
+        dp[start + 1] = max(nums[start], nums[start + 1]);
+        for(int i = start + 2; i <= end; i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[end];
+    }
+};
