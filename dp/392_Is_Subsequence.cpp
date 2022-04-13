ecd29ee081cba49fb5b49866560b829ef1fcd49b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-13 21:51:48 +0800

    Create 392_Is_Subsequence.cpp

---
+++ dp/392_Is_Subsequence.cpp
@@ -0,0 +1,21 @@
+// 2022.04.13
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 1; j <= t.size(); j++){
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else{
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        if(dp[s.size()][t.size()] == s.size()){
+            return true;
+        }
+        return false;
+    }
+};
