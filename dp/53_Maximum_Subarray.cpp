5c123e9d214ed7a52ac5d21876d824824f13ed27
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-13 21:51:51 +0800

    Create 53_Maximum_Subarray.cpp

---
+++ dp/53_Maximum_Subarray.cpp
@@ -0,0 +1,36 @@
+// 2022.04.06
+// greedy
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN;
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            count += nums[i];
+            res = count > res ? count : res;
+            if(count <= 0){
+                count = 0;
+            }
+        }
+        return res;
+    }
+};
+
+// 2022.04.13
+// dp
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        int res = dp[0];
+        for(int i = 1; i < nums.size(); i++){
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
