1d6f5b9bfae946da22504d1e1b3070a963ffc14d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-14 22:23:40 +0800

    Create 583_Delete_Operation_for_Two_Strings.cpp

---
+++ dp/583_Delete_Operation_for_Two_Strings.cpp
@@ -0,0 +1,44 @@
+// 2022.04.14
+// dp 1
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for(int i = 1; i <= word1.size(); i++){
+            for(int j = 1; j <= word2.size(); j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
+    }
+};
+
+// dp 2
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0)); // dp means the min op num to make the 2 substr the same
+        for(int i = 0; i <= word1.size(); i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0; j <= word2.size(); j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1; i <= word1.size(); i++){
+            for(int j = 1; j <= word2.size(); j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = min(dp[i - 1][j - 1] + 2, min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+};
