dfbe8717f200245a4559690a5a0ed858835f0e6d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-14 22:23:32 +0800

    Update 115_Distinct_Subsequences.cpp

--- dp/115_Distinct_Subsequences.cpp
+++ dp/115_Distinct_Subsequences.cpp
@@ -1,3 +1,4 @@
+// 2022.04.14
 class Solution {
 public:
     int numDistinct(string s, string t) {
@@ -11,7 +12,7 @@ public:
         for(int i = 1; i <= s.size(); i++){
             for(int j = 1; j <= t.size(); j++){
                 if(s[i - 1] == t[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]; // i - 1 of dp[i - 1][j] means delete (i - 1)th char, then calc again
                 }
                 else{
                     dp[i][j] = dp[i - 1][j];
