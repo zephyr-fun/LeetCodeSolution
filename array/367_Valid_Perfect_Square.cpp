07001ca471ab6b08fb2f947cf49d83bbb451930f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:38:03 +0800

    Update 367_Valid_Perfect_Square.cpp

--- array/367_Valid_Perfect_Square.cpp
+++ array/367_Valid_Perfect_Square.cpp
@@ -20,3 +20,25 @@ public:
         return false;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if((long long)mid *mid < num) {
+                left = mid + 1;
+            }
+            else if((long long)mid * mid > num) {
+                right = mid - 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
