b025567a8f32453a85c575c509590bc4ae9794a5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:37:12 +0800

    Update 704_Binary_Search.cpp

--- array/704_Binary_Search.cpp
+++ array/704_Binary_Search.cpp
@@ -52,3 +52,29 @@ public:
         return res;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // insure
+        if(nums.size() == 0){
+            return -1;
+        }
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left <= right) { // <=
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
