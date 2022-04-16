42f879322763bd699dca708673eaa6efaee8f995
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:40:42 +0800

    Update 904_Fruit_Into_Baskets.cpp

--- array/904_Fruit_Into_Baskets.cpp
+++ array/904_Fruit_Into_Baskets.cpp
@@ -24,3 +24,31 @@ public:
         return res;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left = 0;
+        int right = 0;
+        int leftKind = fruits[left];
+        int rightKind = fruits[right];
+        int res = INT_MIN;
+        while(right < fruits.size()) {
+            if(fruits[right] == leftKind || fruits[right] == rightKind) {
+                res = res > right - left + 1 ? res : right - left + 1;
+                right++;
+            }
+            else {
+                left = right - 1;
+                leftKind = fruits[left];
+                while(left > 0 && fruits[left - 1] == leftKind){
+                    left--;
+                }
+                rightKind = fruits[right];
+                res = res > right - left + 1 ? res : right - left + 1;
+            }
+        }
+        return res;
+    }
+};
