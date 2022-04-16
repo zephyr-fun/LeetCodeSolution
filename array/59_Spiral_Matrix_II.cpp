4d79465a4596e56a27ea8a18ea09cfd1401fe5d2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:43:13 +0800

    Update 59_Spiral_Matrix_II.cpp

--- array/59_Spiral_Matrix_II.cpp
+++ array/59_Spiral_Matrix_II.cpp
@@ -39,3 +39,44 @@ public:
         return res;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int startX = 0;
+        int startY = 0;
+        int loop = n / 2;
+        int mid = n / 2;
+        int offset = 1;
+        int count = 1;
+        vector<vector<int>> res(n, vector<int>(n));
+        while(loop--){
+            int i = startX;
+            int j = startY;
+            for(; j < startY + n - offset; j++){
+                res[i][j] = count;
+                count++;
+            }
+            for(; i < startX + n - offset; i++){
+                res[i][j] = count;
+                count++;
+            }
+            for(; j > startY; j--){
+                res[i][j] = count;
+                count++;
+            }
+            for(; i > startX; i--){
+                res[i][j] = count;
+                count++;
+            }
+            startX++;
+            startY++;
+            offset += 2;
+        }
+        if(n % 2){
+            res[mid][mid] = count;
+        }
+        return res;
+    }
+};
