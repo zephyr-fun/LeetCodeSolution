b87c2ca66b133db6da3951e7e4e7c86a802abba3
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:37:25 +0800

    Update 35_Search_Insert_Position.cpp

--- array/35_Search_Insert_Position.cpp
+++ array/35_Search_Insert_Position.cpp
@@ -23,3 +23,28 @@ public:
         return left;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                return mid; // mid
+            }
+        }
+        return left; // return left
+    }
+};
