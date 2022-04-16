992169e003d359f79e0a75b468cd850510a3324a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-16 16:39:46 +0800

    Update 209_Minimum_Size_Subarray_Sum.cpp

--- array/209_Minimum_Size_Subarray_Sum.cpp
+++ array/209_Minimum_Size_Subarray_Sum.cpp
@@ -19,3 +19,24 @@ public:
         return res == INT_MAX ? 0 : res;
     }
 };
+
+// 2022.04.16
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int res = INT_MAX;
+        while(right < nums.size()){
+            sum += nums[right];
+            while(sum >= target){
+                res = res < right - left + 1 ? res : right - left + 1;
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
