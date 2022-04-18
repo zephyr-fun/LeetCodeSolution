dff03d8bfba36af56ebc86ef20cb5d5001c27686
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-18 22:31:05 +0800

    Update 349_Intersection_of_Two_Arrays.cpp

--- hash/349_Intersection_of_Two_Arrays.cpp
+++ hash/349_Intersection_of_Two_Arrays.cpp
@@ -29,3 +29,21 @@ public:
         return vector<int>(res_set.begin(), res_set.end());
     }
 };
+
+// 2022.04.18
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1;
+        unordered_set<int> res;
+        for(int i = 0; i < nums1.size(); i++) {
+            set1.insert(nums1[i]);
+        }
+        for(int i = 0; i < nums2.size(); i++) {
+            if(set1.find(nums2[i]) != set1.end()) {
+                res.insert(nums2[i]);
+            }
+        }
+        return vector<int> (res.begin(), res.end());
+    }
+};
