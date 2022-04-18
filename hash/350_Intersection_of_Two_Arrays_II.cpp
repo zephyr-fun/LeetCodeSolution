3c3a218918994161d3378152dc2da59c053af1a7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-18 22:31:11 +0800

    Update 350_Intersection_of_Two_Arrays_II.cpp

--- hash/350_Intersection_of_Two_Arrays_II.cpp
+++ hash/350_Intersection_of_Two_Arrays_II.cpp
@@ -104,3 +104,50 @@ public:
         return res;
     }
 };
+
+// 2022.04.18
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> map;
+        vector<int> res;
+        for(int i = 0; i < nums1.size(); i++) {
+            map[nums1[i]]++;
+        }
+        for(int i = 0; i < nums2.size(); i++) {
+            if(map[nums2[i]] > 0) {
+                res.push_back(nums2[i]);
+                map[nums2[i]]--;
+            }
+        }
+        return res;
+    }
+};
+
+// double pointers
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int index1 = 0;
+        int index2 = 0;
+        vector<int> res;
+        while(index1 < len1 && index2 < len2) {
+            if(nums1[index1] < nums2[index2]) {
+                index1++;
+            }
+            else if(nums1[index1] > nums2[index2]) {
+                index2++;
+            }
+            else {
+                res.push_back(nums1[index1]);
+                index1++;
+                index2++;
+            }
+        }
+        return res;
+    }
+};
