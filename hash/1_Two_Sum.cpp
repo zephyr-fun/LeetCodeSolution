993cb564baf63676988b304ec8f0e2bd25589e8e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-18 22:31:21 +0800

    Update 1_Two_Sum.cpp

--- hash/1_Two_Sum.cpp
+++ hash/1_Two_Sum.cpp
@@ -28,3 +28,20 @@ public:
         return {};
     }
 };
+
+// 2022.04.18
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            if(map.find(target - nums[i]) != map.end()) {
+                return {map[target - nums[i]], i};
+            }
+            else {
+                map[nums[i]] = i;
+            }
+        }
+        return {};
+    }
+};
