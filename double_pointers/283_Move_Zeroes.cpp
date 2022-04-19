3b90fa718d5353225aa39f2c4c1b6ad922a6542a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 20:21:42 +0800

    Update 283_Move_Zeroes.cpp

--- double_pointers/283_Move_Zeroes.cpp
+++ double_pointers/283_Move_Zeroes.cpp
@@ -13,3 +13,25 @@ public:
         }
     }
 };
+
+// 2022.04.19
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while(slow <= fast && fast < nums.size()) {
+            if(nums[fast] == 0) {
+                fast++;
+            }
+            else {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            }
+        }
+        for(int i = slow; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
