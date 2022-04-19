682377a147b46798d7b61c16c4c211a7fa278d39
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 11:48:32 +0800

    Update 459_Repeated_Substring_Pattern.cpp

--- string/459_Repeated_Substring_Pattern.cpp
+++ string/459_Repeated_Substring_Pattern.cpp
@@ -28,3 +28,30 @@ public:
         return false;
     }
 };
+
+// 2022.04.19
+class Solution {
+public:
+    void getNext(string& s, int* next) {
+        next[0] = 0;
+        int left = 0;
+        for(int right = 1; right < s.size(); right++) {
+            while(left > 0 && s[left] != s[right]) {
+                left = next[left - 1];
+            }
+            if(s[left] == s[right]) {
+                left++;
+            }
+            next[right] = left;
+        }
+    }
+    bool repeatedSubstringPattern(string s) {
+        int next[s.size()];
+        getNext(s, next);
+        int len = s.size();
+        if(next[len - 1] != 0 && len % (len - next[len - 1]) == 0) {
+            return true;
+        }
+        return false;
+    }
+};
