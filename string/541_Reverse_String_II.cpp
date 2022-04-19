ad581f2e87c25f7ceb4b0f96971d5d2b20de3e31
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 11:48:03 +0800

    Update 541_Reverse_String_II.cpp

--- string/541_Reverse_String_II.cpp
+++ string/541_Reverse_String_II.cpp
@@ -21,3 +21,27 @@ public:
         return s;
     }
 };
+
+// 2022.04.19
+class Solution {
+public:
+    void subReverse(string& s, int start, int end) {
+        while(start < end) {
+            s[start] ^= s[end];
+            s[end] ^= s[start];
+            s[start] ^= s[end];
+            start++;
+            end--;
+        }
+    }
+    string reverseStr(string s, int k) {
+        for(int i = 0; i < s.size(); i += (2 * k)) {
+            if(i + k < s.size()) {
+                subReverse(s, i, i + k - 1);
+                continue;
+            }
+            subReverse(s, i, s.size() - 1);
+        }
+        return s;
+    }
+};
