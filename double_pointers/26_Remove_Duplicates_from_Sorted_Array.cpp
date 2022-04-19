8ac63123f907da2a30d6498df82be299c1aaa7fc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 20:21:38 +0800

    Update 26_Remove_Duplicates_from_Sorted_Array.cpp

--- double_pointers/26_Remove_Duplicates_from_Sorted_Array.cpp
+++ double_pointers/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -19,3 +19,23 @@ public:
         return slow + 1;
     }
 };
+
+// 2022.04.19
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while(slow <= fast && fast < nums.size()) {
+            if(fast > 0 && nums[fast] == nums[fast - 1]) {
+                fast++;
+            }
+            else {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            }
+        }
+        return slow;
+    }
+};
