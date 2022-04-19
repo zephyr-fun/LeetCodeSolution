0eebdd3b1a64d860273c1e684e51ecb068ed552e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 20:21:52 +0800

    Update 977_Squares_of_a_Sorted_Array.cpp

--- double_pointers/977_Squares_of_a_Sorted_Array.cpp
+++ double_pointers/977_Squares_of_a_Sorted_Array.cpp
@@ -19,3 +19,26 @@ public:
         return res;
     }
 };
+
+// 2022.04.19
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size(), 0);
+        int left = 0;
+        int right = nums.size() - 1;
+        int count = nums.size() - 1;
+        while(left <= right) {
+            if(abs(nums[left]) > abs(nums[right])) {
+                res[count] = nums[left] * nums[left];
+                left++;
+            }
+            else {
+                res[count] = nums[right] * nums[right];
+                right--;
+            }
+            count--;
+        }
+        return res;
+    }
+};
