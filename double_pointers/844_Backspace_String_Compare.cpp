035d7c1f058b2e23e9acce66faf62a5170a39329
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-19 20:21:48 +0800

    Update 844_Backspace_String_Compare.cpp

--- double_pointers/844_Backspace_String_Compare.cpp
+++ double_pointers/844_Backspace_String_Compare.cpp
@@ -77,3 +77,91 @@ public:
         return false;
     }
 };
+
+// 2022.04.19
+// simulate stack
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string sStack;
+        string tStack;
+        int sIndex = 0;
+        int tIndex = 0;
+        while(sIndex < s.size()) {
+            if(s[sIndex] != '#') {
+                sStack += s[sIndex];
+            }
+            else {
+                if(!sStack.empty()) {
+                    sStack.pop_back();
+                }
+            }
+            sIndex++;
+        }
+        while(tIndex < t.size()) {
+            if(t[tIndex] != '#') {
+                tStack += t[tIndex];
+            }
+            else {
+                if(!tStack.empty()) {
+                    tStack.pop_back();
+                }
+            }
+            tIndex++;
+        }
+        return sStack == tStack;
+    }
+};
+
+// O(1) space
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int sSharp = 0;
+        int tSharp = 0;
+        int sIndex = s.size() - 1;
+        int tIndex = t.size() - 1;
+        while(true) {
+            while(sIndex >= 0) {
+                if(s[sIndex] == '#') {
+                    sSharp++;
+                }
+                else {
+                    if(sSharp > 0) {
+                        sSharp--;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                sIndex--;
+            }
+            while(tIndex >= 0) {
+                if(t[tIndex] == '#') {
+                    tSharp++;
+                }
+                else {
+                    if(tSharp > 0) {
+                        tSharp--;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                tIndex--;
+            }
+            if(sIndex < 0 || tIndex < 0) {
+                break;
+            }
+            if(s[sIndex] != t[tIndex]) {
+                return false;
+            }
+            sIndex--;
+            tIndex--;
+        }
+        if(sIndex == -1 && tIndex == -1) {
+            return true;
+        }
+        return false;
+    }
+};
