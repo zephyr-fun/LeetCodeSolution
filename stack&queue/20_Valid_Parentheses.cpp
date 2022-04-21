f3582a9a9760fd64cc6503d58a40b2e5c3229db2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-21 22:15:52 +0800

    Update 20_Valid_Parentheses.cpp

--- stack&queue/20_Valid_Parentheses.cpp
+++ stack&queue/20_Valid_Parentheses.cpp
@@ -23,3 +23,31 @@ public:
         return st.empty();
     }
 };
+
+// 2022.04.20
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                st.push(')');
+            }
+            else if(s[i] == '[') {
+                st.push(']');
+            }
+            else if(s[i] == '{') {
+                st.push('}');
+            }
+            else {
+                if(st.empty() || st.top() != s[i]) {
+                    return false;
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
