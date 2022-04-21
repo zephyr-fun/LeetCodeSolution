71ca9aa44e42193b46df618288f3df2dac567052
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-21 22:15:40 +0800

    Update 232_Implement_Queue_using_Stacks.cpp

--- stack&queue/232_Implement_Queue_using_Stacks.cpp
+++ stack&queue/232_Implement_Queue_using_Stacks.cpp
@@ -62,3 +62,64 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+
+// 2022.04.20
+class MyQueue {
+public:
+    stack<int> st1;
+    stack<int> st2;
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        if(!st2.empty()) {
+            int temp = st2.top();
+            st2.pop();
+            return temp;
+        }
+        else {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+            int temp = st2.top();
+            st2.pop();
+            return temp;
+        }
+    }
+    
+    int peek() {
+        if(!st2.empty()) {
+            return st2.top();
+        }
+        else {
+            while(!st1.empty()) {
+                st2.push(st1.top());
+                st1.pop();
+            }
+            return st2.top();
+        }
+    }
+    
+    bool empty() {
+        if(st1.empty() && st2.empty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
