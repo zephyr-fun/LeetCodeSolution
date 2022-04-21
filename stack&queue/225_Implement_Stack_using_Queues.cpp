dc9edd62306ad52a02e8ff862e0abe70acb81dc7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-21 22:15:47 +0800

    Update 225_Implement_Stack_using_Queues.cpp

--- stack&queue/225_Implement_Stack_using_Queues.cpp
+++ stack&queue/225_Implement_Stack_using_Queues.cpp
@@ -85,3 +85,56 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+// 2022.04.20
+class MyStack {
+public:
+    queue<int> que;
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        que.push(x);
+    }
+    
+    int pop() {
+        int size = que.size() - 1;
+        while(size--) {
+            int front = que.front();
+            que.pop();
+            que.push(front);
+        }
+        int temp = que.front();
+        que.pop();
+        return temp;
+    }
+    
+    int top() {
+        int size = que.size() - 1;
+        while(size--) {
+            int front = que.front();
+            que.pop();
+            que.push(front);
+        }
+        int temp = que.front();
+        que.pop();
+        que.push(temp);
+        return temp;
+        // or just use
+        // return que.back();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
