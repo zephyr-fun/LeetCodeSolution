0b528824a7c5a2f330c53aaaf84b024d73de76af
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-21 22:16:01 +0800

    Update 150_Evaluate_Reverse_Polish_Notation.cpp

--- stack&queue/150_Evaluate_Reverse_Polish_Notation.cpp
+++ stack&queue/150_Evaluate_Reverse_Polish_Notation.cpp
@@ -29,3 +29,44 @@ public:
         return st.top();
     }
 };
+
+// 2022.04.20
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i = 0; i < tokens.size(); i++) {
+            if(tokens[i] == "+") { // a little bit redundent
+                int temp2 = st.top();
+                st.pop();
+                int temp1 = st.top();
+                st.pop();
+                st.push(temp1 + temp2);
+            }
+            else if(tokens[i] == "-") {
+                int temp2 = st.top();
+                st.pop();
+                int temp1 = st.top();
+                st.pop();
+                st.push(temp1 - temp2);
+            }
+            else if(tokens[i] == "*") {
+                int temp2 = st.top();
+                st.pop();
+                int temp1 = st.top();
+                st.pop();
+                st.push(temp1 * temp2);
+            }else if(tokens[i] == "/") {
+                int temp2 = st.top();
+                st.pop();
+                int temp1 = st.top();
+                st.pop();
+                st.push(temp1 / temp2);
+            }
+            else {
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
