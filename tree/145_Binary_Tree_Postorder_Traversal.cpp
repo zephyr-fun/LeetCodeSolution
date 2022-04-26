09daf7bc16aaed4e3a1d37d416145f9837bd8fc7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-26 22:25:24 +0800

    Update 145_Binary_Tree_Postorder_Traversal.cpp

--- tree/145_Binary_Tree_Postorder_Traversal.cpp
+++ tree/145_Binary_Tree_Postorder_Traversal.cpp
@@ -172,3 +172,116 @@ public:
         return pre;
     }
 };
+
+// 2022.04.26
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// recursion
+class Solution {
+public:
+    void traversal(TreeNode* root, vector<int>& res) {
+        if(root == nullptr) {
+            return ;
+        }
+        traversal(root->left, res);
+        traversal(root->right, res);
+        res.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+};
+
+// non recursion
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(root == nullptr) {
+            return res;
+        }
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()) {
+            TreeNode* node = st.top();
+            if(node != nullptr) {
+                st.pop();
+                st.push(node);
+                st.push(nullptr);
+                if(node->right != nullptr) {
+                    st.push(node->right);
+                }
+                if(node->left != nullptr) {
+                    st.push(node->left);
+                }
+            }
+            else {
+                st.pop();
+                node = st.top();
+                st.pop();
+                res.push_back(node->val);
+            }
+        }
+        return res;
+    }
+};
+
+// Morris
+class Solution {
+public:
+    TreeNode* reverse(TreeNode* root) {
+        TreeNode* pre;
+        TreeNode* next;
+        while(root != nullptr) {
+            next = root->right;
+            root->right = pre;
+            pre = root;
+            root = next;
+        }
+        return pre;
+    }
+    void printNode(TreeNode* root, vector<int>& res) {
+        TreeNode* tail = reverse(root);
+        TreeNode* cur = tail;
+        while(cur != nullptr) {
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        reverse(tail);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* head = root;
+        while(root != nullptr) {
+            if(root->left != nullptr) {
+                TreeNode* temp = root->left;
+                while(temp->right != nullptr && temp->right != root) {
+                    temp = temp->right;
+                }
+                if(temp->right == nullptr) {
+                    temp->right = root;
+                    root = root->left;
+                }
+                else {
+                    temp->right = nullptr;
+                    printNode(root->left, res); // root->left
+                    root = root->right;
+                }
+            }
+            else {
+                root = root->right;
+            }
+        }
+        printNode(head, res);
+        return res;
+    }
+};
