62203b49a00c73069ac337a19478e3058d2f95e2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-26 22:25:56 +0800

    Update 637_Average_of_Levels_in_Binary_Tree.cpp

--- tree/637_Average_of_Levels_in_Binary_Tree.cpp
+++ tree/637_Average_of_Levels_in_Binary_Tree.cpp
@@ -37,3 +37,30 @@ public:
         return res;
     }
 };
+
+// 2022.04.26
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> res;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            double sum = 0;
+            for(int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                sum += node->val;
+                if(node->left != nullptr) {
+                    que.push(node->left);
+                }
+                if(node->right != nullptr) {
+                    que.push(node->right);
+                }
+            }
+            res.push_back(sum / size);
+        }
+        return res;
+    }
+};
