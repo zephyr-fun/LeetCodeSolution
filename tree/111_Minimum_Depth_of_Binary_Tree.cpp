b66f52d3b3abb25dd462e3cc63f2ee5d368adcf3
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-26 22:26:36 +0800

    Update 111_Minimum_Depth_of_Binary_Tree.cpp

--- tree/111_Minimum_Depth_of_Binary_Tree.cpp
+++ tree/111_Minimum_Depth_of_Binary_Tree.cpp
@@ -113,3 +113,16 @@ public:
         return (left == 0 || right == 0) ? left + right + 1 : min(left, right) + 1;
     }
 };
+
+// 2022.04.26
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        return (left == 0 || right == 0) ? left + right + 1 : min(left, right) + 1;
+    }
+};
