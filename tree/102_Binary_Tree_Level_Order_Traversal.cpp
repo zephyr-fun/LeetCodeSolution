7ac396b74fe0e7d2f547adfc62870ae6345adc27
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-26 22:25:41 +0800

    Update 102_Binary_Tree_Level_Order_Traversal.cpp

--- tree/102_Binary_Tree_Level_Order_Traversal.cpp
+++ tree/102_Binary_Tree_Level_Order_Traversal.cpp
@@ -107,3 +107,33 @@ public:
         return res;
     }
 };
+
+// 2022.04.26
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == nullptr) {
+            return res;
+        }
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            vector<int> temp;
+            for(int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                temp.push_back(node->val);
+                if(node->left != nullptr) {
+                    que.push(node->left);
+                }
+                if(node->right != nullptr) {
+                    que.push(node->right);
+                }
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
