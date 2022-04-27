50c50720c766c25f63020d83ac37506db073e29e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-27 23:02:31 +0800

    Update 100_Same_Tree.cpp

--- tree/100_Same_Tree.cpp
+++ tree/100_Same_Tree.cpp
@@ -90,3 +90,17 @@ public:
         return left && right;
     }
 };
+
+// 2022.04.27
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) {
+            return true;
+        }
+        if(p == nullptr || q == nullptr || p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
