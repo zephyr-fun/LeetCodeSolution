f80ed9e4e7fe260a28845a8a260685ca40cca263
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-28 21:09:09 +0800

    Update 1595_Minimum_Cost_to_Connect_Two_Groups_of_Points.cpp

--- dp/1595_Minimum_Cost_to_Connect_Two_Groups_of_Points.cpp
+++ dp/1595_Minimum_Cost_to_Connect_Two_Groups_of_Points.cpp
@@ -46,3 +46,25 @@ public:
         return dp[s1][ALLs2];
     }
 };
+
+
+// 2022.04.28
+// kind of hard to understand
+class Solution {
+public:
+    int connectTwoGroups(vector<vector<int>>& cost) {
+        constexpr int inf = 0x3f3f3f3f;
+        const int m = cost.size();
+        const int n = cost[0].size();
+        vector<vector<int>> dp(m + 1, vector<int>(1 << n, inf));
+        dp[0][0] = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int s = 0; s < 1 << n; s++) {
+                    dp[i + 1][s | (1 << j)] = min({dp[i + 1][s | (1 << j)], dp[i + 1][s] + cost[i][j], dp[i][s] + cost[i][j]});
+                }
+            }
+        }
+        return dp[m].back();
+    }
+};
