e75b0eb0f41e174ba7cd6bbedc80db8b5870f11d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-29 22:13:53 +0800

    Update 110_Balanced_Binary_Tree.cpp

--- tree/110_Balanced_Binary_Tree.cpp
+++ tree/110_Balanced_Binary_Tree.cpp
@@ -88,7 +88,27 @@ public:
         if(node == nullptr) return 0;
         int leftHeight = getDepth(node->left);
         int rightHeight = getDepth(node->right);
-        if(leftHeight ==-1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) return -1;
+        if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) return -1;
         return max(leftHeight, rightHeight) + 1;
+    }
+};
+
+// 2022.04.29
+class Solution {
+public:
+    int getHeight(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int left = getHeight(root->left);
+        int right = getHeight(root->right);
+        if(left == -1 || right == -1 || abs(left - right) > 1) {
+            return -1;
+        }
+        return max(left, right) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        int res = getHeight(root);
+        return res == -1 ? false : true;
     }
 };
