89f805311326a3073f0c161546ed10c1e519cfc4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-29 22:13:48 +0800

    Update 222_Count_Complete_Tree_Nodes.cpp

--- tree/222_Count_Complete_Tree_Nodes.cpp
+++ tree/222_Count_Complete_Tree_Nodes.cpp
@@ -81,3 +81,29 @@ public:
         return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
+
+// 2022.04.29
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int leftHeight = 0;
+        int rightHeight = 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        while(left != nullptr) {
+            left = left->left;
+            leftHeight++;
+        }
+        while(right != nullptr) {
+            right = right->right;
+            rightHeight++;
+        }
+        if(leftHeight == rightHeight) {
+            return (2 << leftHeight) - 1;
+        }
+        return countNodes(root->left) + countNodes(root->right) + 1; // notice that left now equals nullptr
+    }
+};
