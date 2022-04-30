64f65da7a1ec76c904e6a1bd185f49a84af1c1cc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-30 22:51:43 +0800

    Update 669_Trim_a_Binary_Search_Tree.cpp

--- tree/669_Trim_a_Binary_Search_Tree.cpp
+++ tree/669_Trim_a_Binary_Search_Tree.cpp
@@ -66,3 +66,24 @@ public:
         return root;
     }
 };
+
+// 2022.04.30
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == nullptr) {
+            return nullptr;
+        }
+        if(root->val < low) {
+            TreeNode* right = trimBST(root->right, low, high);
+            return right;
+        }
+        if(root->val > high) {
+            TreeNode* left = trimBST(root->left, low, high);
+            return left;
+        }
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        return root;
+    }
+};
