b4fe0d93431717813ffcf0a3e8d8937b00bf0f12
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-04-30 22:51:16 +0800

    Update 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp

--- tree/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
+++ tree/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -140,3 +140,22 @@ public:
         return nullptr;
     }
 };
+
+// 2022.04.30
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root != nullptr) {
+            if(root->val > p->val && root->val > q->val) {
+                root = root->left;
+            }
+            else if(root->val < p->val && root->val < q->val) {
+                root = root->right;
+            }
+            else {
+                return root;
+            }
+        }
+        return nullptr;
+    }
+};
