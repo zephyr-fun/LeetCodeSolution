fef9d64356b186094384ce9c959b94b59cb77a1c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-02 18:21:51 +0800

    Update 416_Partition_Equal_Subset_Sum.cpp

--- dp/416_Partition_Equal_Subset_Sum.cpp
+++ dp/416_Partition_Equal_Subset_Sum.cpp
@@ -29,3 +29,28 @@ public:
         return false;
     }
 };
+
+// 2022.05.02 retroactive 2022.05.01
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+        }
+        if(sum % 2 == 1) {
+            return false;
+        }
+        int target = sum / 2;
+        vector<int> dp(target + 1, 0);
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = target; j >= nums[i]; j--) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        if(dp[target] == target) {
+            return true;
+        }
+        return false;
+    }
+};
