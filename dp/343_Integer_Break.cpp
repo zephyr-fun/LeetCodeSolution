a4da637ead6b687b2ad3ccc3dfbf13083b7c588f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-03 20:58:58 +0800

    Update 343_Integer_Break.cpp

--- dp/343_Integer_Break.cpp
+++ dp/343_Integer_Break.cpp
@@ -13,16 +13,19 @@ public:
     }
 };
 
-// class Solution {
-// public:
-//     int integerBreak(int n) {
-//         int base = 0;
-//         for(int i = 2; i < n; i++){
-//             if(n >= i *i){
-//                 base = i;
-//             }
-//         }
-//         int res = n - i * i;
-//         return pow(i, i - res) * pow(i + 1, res);
-//     }
-// };
+// 2022.05.03
+class Solution {
+public:
+    int integerBreak(int n) {
+        // dp[i] refers to the maximum of time result of breaking i
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            for(int j = 1; j < i; j++) {
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+};
