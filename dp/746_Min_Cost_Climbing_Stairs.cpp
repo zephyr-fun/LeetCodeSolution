e0a89f380160380af61263f770432e1cd4da5d84
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-03 20:58:38 +0800

    Update 746_Min_Cost_Climbing_Stairs.cpp

--- dp/746_Min_Cost_Climbing_Stairs.cpp
+++ dp/746_Min_Cost_Climbing_Stairs.cpp
@@ -39,3 +39,40 @@ public:
         return min(dp[0], dp[1]);
     }
 };
+
+// 2022.05.03
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size(), 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        if(cost.size() == 2) {
+            return min(dp[0], dp[1]);
+        }
+        for(int i = 2; i < cost.size(); i++) {
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return min(dp[cost.size() - 1], dp[cost.size() - 2]);
+    }
+};
+
+// optim space
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(2, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        if(cost.size() == 2) {
+            return min(dp[0], dp[1]);
+        }
+        for(int i = 2; i < cost.size(); i++) {
+            dp[0] = min(dp[0], dp[1]) + cost[i];
+            dp[0] ^= dp[1];
+            dp[1] ^= dp[0];
+            dp[0] ^= dp[1];
+        }
+        return min(dp[0], dp[1]);
+    }
+};
