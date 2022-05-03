764885028fdb5da8716e4e9bddb5b44c557fefb4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-03 20:57:31 +0800

    Update 279_Perfect_Squares.cpp

--- dp/279_Perfect_Squares.cpp
+++ dp/279_Perfect_Squares.cpp
@@ -29,3 +29,40 @@ public:
         return dp[n];
     }
 };
+
+// 2022.05.03
+// bag first
+// item second
+class Solution {
+public:
+    int numSquares(int n) {
+        // dp[i] refers to the minimum of breaking i in to psn
+        vector<int> dp(n + 1, 0x3f3f3f3f);
+        dp[0] = 0;
+        for(int i = 0; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
+
+// item first
+// bag second
+class Solution {
+public:
+    int numSquares(int n) {
+        // dp[i] refers to the minimum of breaking i in to psn
+        vector<int> dp(n + 1, 0x3f3f3f3f);
+        dp[0] = 0;
+        for(int j = 1; j * j <= n; j++) {
+            for(int i = 0; i <= n; i++) {
+                if(i >= j * j){
+                    dp[i] = min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
+};
