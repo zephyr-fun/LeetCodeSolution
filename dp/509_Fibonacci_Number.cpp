15ab6230633b8d074efebc290eb576c337b34ac7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-03 20:58:27 +0800

    Update 509_Fibonacci_Number.cpp

--- dp/509_Fibonacci_Number.cpp
+++ dp/509_Fibonacci_Number.cpp
@@ -48,3 +48,40 @@ public:
         return dp[1];
     }
 };
+
+// 2022.05.03
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
+// optim space
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        vector<int> dp(2, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[0] = dp[0] + dp[1];
+            dp[0] ^= dp[1];
+            dp[1] ^= dp[0];
+            dp[0] ^= dp[1];
+        }
+        return dp[1];
+    }
+};
