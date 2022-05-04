08bb1285a9b962fea149ff1e5b14f8e9a5534bf7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-04 22:19:36 +0800

    Update 96_Unique_Binary_Search_Trees.cpp

--- dp/96_Unique_Binary_Search_Trees.cpp
+++ dp/96_Unique_Binary_Search_Trees.cpp
@@ -12,3 +12,19 @@ public:
         return dp[n];
     }
 };
+
+// 2022.05.04
+class Solution {
+public:
+    int numTrees(int n) {
+        // dp[i] refers to total number of BST when set root value to 1 to i
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
