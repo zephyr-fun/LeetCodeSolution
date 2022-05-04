19d372ee9849f42e7401a0868bc302e2315558b0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-04 22:19:13 +0800

    Update 1035_Uncrossed_Lines.cpp

--- dp/1035_Uncrossed_Lines.cpp
+++ dp/1035_Uncrossed_Lines.cpp
@@ -16,3 +16,23 @@ public:
         return dp[nums1.size()][nums2.size()];
     }
 };
+
+// 2022.05.04
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        // dp[i][j] refers to the LCS between front i int in nums1 and front j int in nums2
+        vector<vector<int>> dp (nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+        for(int i = 1; i <= nums1.size(); i++) {
+            for(int j = 1; j <= nums2.size(); j++) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[nums1.size()][nums2.size()];
+    }
+};
