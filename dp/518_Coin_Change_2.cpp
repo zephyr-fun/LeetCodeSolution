5f064f34e193f0fb61fa5daf193c559a98573a90
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-05 19:08:33 +0800

    Update 518_Coin_Change_2.cpp

--- dp/518_Coin_Change_2.cpp
+++ dp/518_Coin_Change_2.cpp
@@ -16,3 +16,19 @@ public:
         return dp[amount];
     }
 };
+
+// 2022.05.05
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        // dp[j] refers to the total number of plans to consisting amount j
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); i++) {
+            for(int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
