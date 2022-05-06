dcbf95046ae362afa3685958fe4882e13145f59e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-06 20:10:17 +0800

    Update 1575_Count_All_Possible_Routes.cpp

--- dp/1575_Count_All_Possible_Routes.cpp
+++ dp/1575_Count_All_Possible_Routes.cpp
@@ -49,4 +49,33 @@ public:
     }
 };
 
-// dp
+class Solution {
+public:
+    int mod = 1000000007;
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        int size = locations.size();
+        // dp[i][j] refers to the maximum path to get to the finish point at pos i, with rest j fuel
+        vector<vector<int>> dp(size, vector<int>(fuel + 1, 0));
+        for(int j = 0; j <= fuel; j++) {
+            dp[finish][j] = 1;
+        }
+        // 从状态转移方程可以发现 f[i][fuel]=f[i][fuel]+f[k][fuel-need]
+        // 在计算 f[i][fuel] 的时候依赖于 f[k][fuel-need]
+        // 其中 i 和 k 并无严格的大小关系
+        // 而 fuel 和 fuel-need 具有严格大小关系：fuel >= fuel-need
+        // 因此需要先从小到大枚举油量
+        for(int j = 0; j <= fuel; j++) {
+            for(int i = 0; i < size; i++) {
+                for(int k = 0; k < size; k++) {
+                    if(i != k) {
+                        if(abs(locations[i] - locations[k]) <= j) {
+                            dp[i][j] += dp[k][j - abs(locations[i] - locations[k])];
+                            dp[i][j] %= mod;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[start][fuel];
+    }
+};
