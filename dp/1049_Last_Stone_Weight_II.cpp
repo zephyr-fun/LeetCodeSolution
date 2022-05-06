9f4b4aa74b00c61871737f555abf41e9c09fed81
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-06 20:10:00 +0800

    Update 1049_Last_Stone_Weight_II.cpp

--- dp/1049_Last_Stone_Weight_II.cpp
+++ dp/1049_Last_Stone_Weight_II.cpp
@@ -35,3 +35,23 @@ public:
         return sum - dp[target] - dp[target];
     }
 };
+
+// 2022.05.06
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for(int i = 0; i < stones.size(); i++) {
+            sum += stones[i];
+        }
+        int target = sum / 2;
+        vector<int> dp(target + 1, 0);
+        dp[0] = 0;
+        for(int i = 0; i < stones.size(); i++) {
+            for(int j = target; j >= stones[i]; j--) {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return sum - dp[target] - dp[target];
+    }
+};
