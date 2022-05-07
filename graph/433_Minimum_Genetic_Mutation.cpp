6bd4a08722346014ac184bab690fd9f408f32791
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-07 17:18:33 +0800

    Create 433_Minimum_Genetic_Mutation.cpp

---
+++ graph/433_Minimum_Genetic_Mutation.cpp
@@ -0,0 +1,72 @@
+// 2022.05.07
+// dual bfs
+class Solution {
+public:
+    string beginWord;
+    string endWord;
+    static constexpr char CH[4] = {'A', 'C', 'G', 'T'};
+    unordered_set<string> set;
+    int minMutation(string start, string end, vector<string>& bank) {
+        beginWord = start;
+        endWord = end;
+        for(auto bankWord : bank) {
+            set.insert(bankWord);
+        }
+        if(set.find(end) == set.end()) {
+            return -1;
+        }
+        int res = bfs();
+        return res;
+    }
+    int bfs() {
+        queue<string> que1;
+        queue<string> que2;
+        unordered_map<string, int> map1;
+        unordered_map<string, int> map2;
+        que1.push(beginWord);
+        map1[beginWord] = 0;
+        que2.push(endWord);
+        map2[endWord] = 0;
+        while(!que1.empty() && !que2.empty()) {
+            int res = -1;
+            if(que1.size() <= que2.size()) {
+                res = update(que1, map1, map2);
+            }
+            else {
+                res = update(que2, map2, map1);
+            }
+            if(res != -1) {
+                return res;
+            }
+        }
+        return -1;
+    }
+    int update(queue<string>& que, unordered_map<string, int>& map, unordered_map<string, int>& viceMap) {
+        int size = que.size();
+        while(size--) {
+            string mutate = que.front();
+            que.pop();
+            int length = mutate.size();
+            string sub;
+            for(int i = 0; i < length; i++) {
+                sub = mutate;
+                for(int j = 0; j < 4; j++) {
+                    sub[i] = CH[j];
+                    if(set.count(sub)) {
+                        if(map.count(sub)) {
+                            continue;
+                        }
+                        if(viceMap.count(sub)) {
+                            return map[mutate] + 1 + viceMap[sub];
+                        }
+                        else {
+                            que.push(sub);
+                            map[sub] = map[mutate] + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
