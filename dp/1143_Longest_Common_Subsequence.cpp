3d2b83548400b04c9a523df22399e19c32d29e7e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-08 19:56:06 +0800

    Update 1143_Longest_Common_Subsequence.cpp

--- dp/1143_Longest_Common_Subsequence.cpp
+++ dp/1143_Longest_Common_Subsequence.cpp
@@ -35,3 +35,23 @@ public:
         return dp[text1.size()][text2.size()];
     }
 };
+
+// 2022.05.08
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        // dp[i][j] refers to the maximum subsequence length between front i chars from text1 and front j chars from text2
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+        for(int i = 1; i <= text1.size(); i++) {
+            for(int j = 1; j <= text2.size(); j++) {
+                if(text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
