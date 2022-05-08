eaa97dc0ef22a9e4f5ce41c0169d79e659dcde7d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-08 19:56:02 +0800

    Update 474_Ones_and_Zeroes.cpp

--- dp/474_Ones_and_Zeroes.cpp
+++ dp/474_Ones_and_Zeroes.cpp
@@ -23,3 +23,30 @@ public:
         return dp[m][n];
     }
 };
+
+// 2022.05.08
+// 2-dim 0-1 bag
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(auto str : strs) {
+            int zeroNum = 0;
+            int oneNum = 0;
+            for(int i = 0; i < str.size(); i++) {
+                if(str[i] == '0') {
+                    zeroNum++;
+                }
+                else {
+                    oneNum++;
+                }
+            }
+            for(int i = m; i >= zeroNum; i--) {
+                for(int j = n; j >= oneNum; j--) {
+                    dp[i][j] = max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
