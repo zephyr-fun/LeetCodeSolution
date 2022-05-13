13c2799624aa7c12db0afd4c3e69101f26fe94b6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-13 19:09:49 +0800

    Update 518_Coin_Change_2.cpp

--- dp/518_Coin_Change_2.cpp
+++ dp/518_Coin_Change_2.cpp
@@ -32,3 +32,21 @@ public:
         return dp[amount];
     }
 };
+
+// 2022.05.13
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        // dp[j] refers to number of ways to consist amount j
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size(); i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(j >= coins[i]) {
+                    dp[j] += dp[j - coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
