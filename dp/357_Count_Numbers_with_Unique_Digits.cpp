37c3c7eb75b9b63aaff57f338a9e52dda0e4184a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-13 19:10:02 +0800

    Create 357_Count_Numbers_with_Unique_Digits.cpp

---
+++ dp/357_Count_Numbers_with_Unique_Digits.cpp
@@ -0,0 +1,16 @@
+// 2022.05.13
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int res = 10;
+        for(int i = 2, last = 9; i <= n; i++) { // init last can not be 0(high bit), so it equals to 9
+            int cur = last * (10 - i + 1); // 9 9 8 7 6 5 4 3 2 1.........
+            res += cur;
+            last = cur;
+        }
+        return res;
+    }
+};
