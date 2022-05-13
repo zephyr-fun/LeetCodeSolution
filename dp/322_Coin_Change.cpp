876eda11f43929534dab20cef23b1cd171350084
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-13 19:09:44 +0800

    Update 322_Coin_Change.cpp

--- dp/322_Coin_Change.cpp
+++ dp/322_Coin_Change.cpp
@@ -33,3 +33,21 @@ public:
         return dp[amount] == 0x3f3f3f3f ? -1 : dp[amount];
     }
 };
+
+// 2022.05.13
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // dp[j] refers to the minimum number of coins to consisting amount j
+        vector<int> dp(amount + 1, 0x3f3f3f3f);
+        dp[0] = 0;
+        for(int i = 0; i < coins.size(); i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(j >= coins[i]) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount] == 0x3f3f3f3f ? -1 : dp[amount];
+    }
+};
