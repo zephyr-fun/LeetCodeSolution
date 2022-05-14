3eeb4756524888b36f98bb075c51c73c71e335bb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-14 22:31:00 +0800

    Update 1588_Sum_of_All_Odd_Length_Subarrays.cpp

--- binary_index_tree/1588_Sum_of_All_Odd_Length_Subarrays.cpp
+++ binary_index_tree/1588_Sum_of_All_Odd_Length_Subarrays.cpp
@@ -33,15 +33,15 @@ public:
 class Solution {
 public:
     int sumOddLengthSubarrays(vector<int>& arr) {
-        vector<int> prefix = {0};
-        for(int i = 0; i < arr.size(); i++) {
-            prefix.push_back(prefix.back() + arr[i]);
-        }
         int res = 0;
         for(int i = 0; i < arr.size(); i++) {
-            for(int size = 1; i + size - 1 < arr.size(); size += 2) {
-                res += prefix[i + size] - prefix[i];
-            }
+            int left = i + 1;
+            int right = arr.size() - i;
+            int left_even = (left + 1) / 2;
+            int right_even = (right + 1) / 2;
+            int left_odd = left / 2;
+            int right_odd = right / 2;
+            res += (left_even * right_even + left_odd * right_odd) * arr[i];
         }
         return res;
     }
