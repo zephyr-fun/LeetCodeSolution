7d20e45a99205bdd3ed2b95cf9d720ce3a108414
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-15 19:59:53 +0800

    Update 307_Range_Sum_Query-Mutable.cpp

--- binary_index_tree/307_Range_Sum_Query-Mutable.cpp
+++ binary_index_tree/307_Range_Sum_Query-Mutable.cpp
@@ -54,4 +54,50 @@ private:
  */
 
 
-// 
+// 2022.05.15
+class FenwickTree {
+public:
+    FenwickTree(int n): sums_(n + 1, 0) {}
+
+    void update(int i, int delta) { // update delta
+        while(i < sums_.size()) {
+            sums_[i] += delta;
+            i += lowbit(i);
+        }
+    }
+
+    int query(int i) {
+        int sum = 0;
+        while(i > 0) {
+            sum += sums_[i];
+            i -= lowbit(i);
+        }
+        return sum;
+    }
+private:
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+    vector<int> sums_;
+};
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums): nums_(std::move(nums)), tree_(nums_.size()) {
+        for(int i = 0; i < nums_.size(); i++) {
+            tree_.update(i + 1, nums_[i]);
+        } // nlogn
+    }
+    
+    void update(int index, int val) {
+        tree_.update(index + 1, val - nums_[index]);
+        nums_[index] = val;
+    }
+    
+    int sumRange(int left, int right) {
+        return tree_.query(right + 1) - tree_.query(left);
+    }
+private:
+    vector<int> nums_;
+    FenwickTree tree_;
+};
