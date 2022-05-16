383569f7e283500e035f8a2417cb6d8086a41e9d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-16 16:28:32 +0800

    Update 583_Delete_Operation_for_Two_Strings.cpp

--- dp/583_Delete_Operation_for_Two_Strings.cpp
+++ dp/583_Delete_Operation_for_Two_Strings.cpp
@@ -42,3 +42,25 @@ public:
         return dp[word1.size()][word2.size()];
     }
 };
+
+// 2022.05.16
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // dp[i][j] refers to the LCS between i char in word1 and j char in word2
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for(int i = 1; i <= word1.size(); i++) {
+            for(int j = 1; j <= word2.size(); j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
+    }
+};
+
+// LCS -> LIS, cant now
