6c8b43f8340f162a4f34a441d450056d67d78656
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-17 19:52:50 +0800

    Create 1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp

---
+++ binary_index_tree/1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp
@@ -0,0 +1,129 @@
+// 2022.05.17
+// diff array
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        vector<int> diff(52, 0);
+        for(int i = 0; i < ranges.size(); i++) {
+            diff[ranges[i][0]]++;
+            diff[ranges[i][1] + 1]--;
+        }
+        int cur = 0;
+        for(int i = 0; i < 52; i++) {
+            cur += diff[i];
+            if(i >= left && i <= right && cur <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// plain BIT
+class FenwickTree {
+public:
+    FenwickTree(int n): sums_(n + 1, 0) {}
+
+    void update(int i, int delta) {
+        while(i < sums_.size()) {
+            sums_[i] += delta;
+            i += lowbit(i);
+        }
+    }
+
+    int query(int i) {
+        if(i >= sums_.size()) {
+            i = sums_.size() - 1;
+        }
+        int sum = 0;
+        while(i > 0) {
+            sum += sums_[i];
+            i -= lowbit(i);
+        }
+        return sum;
+    }
+
+private:
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+    vector<int> sums_;
+};
+
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        sort(ranges.begin(), ranges.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
+        });
+        FenwickTree diffTree(max(ranges[ranges.size() - 1][1], right) + 1);
+        for(int i = 0; i < ranges.size(); i++) {
+            vector<int> range = ranges[i];
+            for(int j = ranges[i][0]; j <= ranges[i][1]; j++) {
+                diffTree.update(j, 1);
+            }
+        }
+        for(int i = left; i <= right; i++) {
+            int cur = diffTree.query(i) - diffTree.query(i - 1);
+            if(cur <= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// drop repeat
+class FenwickTree {
+public:
+    FenwickTree(int n): sums_(n + 1, 0) {}
+
+    void update(int i, int delta) {
+        while(i < sums_.size()) {
+            sums_[i] += delta;
+            i += lowbit(i);
+        }
+    }
+
+    int query(int i) {
+        if(i >= sums_.size()) {
+            i = sums_.size() - 1;
+        }
+        int sum = 0;
+        while(i > 0) {
+            sum += sums_[i];
+            i -= lowbit(i);
+        }
+        return sum;
+    }
+
+private:
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+    vector<int> sums_;
+};
+
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        sort(ranges.begin(), ranges.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
+        });
+        unordered_set<int> set;
+        FenwickTree diffTree(max(ranges[ranges.size() - 1][1], right) + 1);
+        for(int i = 0; i < ranges.size(); i++) {
+            vector<int> range = ranges[i];
+            for(int j = ranges[i][0]; j <= ranges[i][1]; j++) {
+                if(set.count(j) == 0) {
+                    diffTree.update(j, 1);
+                    set.insert(j);
+                }
+            }
+        }
+        if((diffTree.query(right) - diffTree.query(left - 1)) != (right - left + 1)) {
+            return false;
+        }
+        return true;
+    }
+};
