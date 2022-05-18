a314ec01ecf85c0be2c8127d66315f35ee59927a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-18 22:36:43 +0800

    Update 69_Sqrt(x).cpp

--- array/69_Sqrt(x).cpp
+++ array/69_Sqrt(x).cpp
@@ -73,3 +73,24 @@ public:
         return (long long)left * left <= x ? left : left - 1;
     }
 };
+
+// 2022.05.18
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+        int res = 0;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if((long long) mid * mid <= x) {
+                left = mid + 1;
+                res = mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return res;
+    }
+};
