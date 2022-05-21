2fa65cb1467997f896c715e61b6726c7f2699a04
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-21 09:56:12 +0800

    Update 494_Target_Sum.cpp

--- dp/494_Target_Sum.cpp
+++ dp/494_Target_Sum.cpp
@@ -55,3 +55,70 @@ public:
         return dp[aTarget];
     }
 };
+
+// 2022.05.21
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // set a, b
+        // a + b = sum(nums)
+        // a - b = target
+        // a = (sum + target) / 2
+        int sum = 0;
+        for(auto num : nums) {
+            sum += num;
+        }
+        if(abs(target) > sum) {
+            return 0;
+        }
+        if((sum + target) % 2 != 0) {
+            return 0;
+        }
+        int val = (sum + target) / 2;
+        // dp[i][j] refers to considering first i nums, get sum = j, total nums
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(val + 1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= nums.size(); i++) {
+            for(int j = 0; j <= val; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[nums.size()][val];
+    }
+};
+
+// optim i dim
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // set a, b
+        // a + b = sum(nums)
+        // a - b = target
+        // a = (sum + target) / 2
+        int sum = 0;
+        for(auto num : nums) {
+            sum += num;
+        }
+        if(abs(target) > sum) {
+            return 0;
+        }
+        if((sum + target) % 2 != 0) {
+            return 0;
+        }
+        int val = (sum + target) / 2;
+        // dp[j] refers to considering first i nums, get sum = j, total nums
+        vector<int> dp(val + 1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= nums.size(); i++) {
+            for(int j = val; j >= 0; j--) {
+                if(j >= nums[i - 1]) {
+                    dp[j] += dp[j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[val];
+    }
+};
