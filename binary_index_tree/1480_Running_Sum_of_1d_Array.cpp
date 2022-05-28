d0ff5d3298e57c44e7d9776ae6984e67c1e72e38
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-28 15:02:33 +0800

    Update 1480_Running_Sum_of_1d_Array.cpp

--- binary_index_tree/1480_Running_Sum_of_1d_Array.cpp
+++ binary_index_tree/1480_Running_Sum_of_1d_Array.cpp
@@ -36,3 +36,58 @@ public:
         return res;
     }
 };
+
+// 2022.05.28
+// plain version
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum = 0;
+        vector<int> res;
+        for(int num : nums) {
+            sum += num;
+            res.push_back(sum);
+        }
+        return res;
+    }
+};
+
+// funny version
+class FenwickTree {
+public:
+    FenwickTree(int n): sums_(n + 1, 0) {}
+
+    void update(int index, int delta) {
+        while(index < sums_.size()) {
+            sums_[index] += delta;
+            index += lowbit(index);
+        }
+    }
+
+    int query(int index) {
+        int sum = 0;
+        while(index > 0) {
+            sum += sums_[index];
+            index -= lowbit(index);
+        }
+        return sum;
+    }
+
+private:
+    vector<int> sums_;
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+};
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        FenwickTree tree(nums.size());
+        vector<int> res(nums.size(), 0);
+        for(int i = 0; i < nums.size(); i++) {
+            tree.update(i + 1, nums[i]);
+            res[i] = tree.query(i + 1);
+        }
+        return res;
+    }
+};
