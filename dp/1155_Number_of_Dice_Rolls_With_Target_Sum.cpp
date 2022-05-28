0d865f6acf506a8cc1a04a5bc3efba6448e0539d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-28 15:07:30 +0800

    Update 1155_Number_of_Dice_Rolls_With_Target_Sum.cpp

--- dp/1155_Number_of_Dice_Rolls_With_Target_Sum.cpp
+++ dp/1155_Number_of_Dice_Rolls_With_Target_Sum.cpp
@@ -45,3 +45,47 @@ public:
         return dp[target];
     }
 };
+
+// 2022.05.28
+// 2 dim
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        int mod = 1e9 + 7;
+        // dp[i][j] refers to total plans of getting j sum with considering first i dice
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= target; j++) {
+                for(int t = 1; t <= k; t++) {
+                    if(j - t >= 0) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - t]) % mod;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
+
+// optim i dim
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        int mod = 1e9 + 7;
+        // dp[j] refers to total plans of getting j sum with considering first i dice
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = target; j >= 0; j--) {
+                dp[j] = 0;
+                for(int t = 1; t <= k; t++) {
+                    if(j - t >= 0) {
+                        dp[j] = (dp[j] + dp[j - t]) % mod;
+                    }
+                }
+            }
+        }
+        return dp[target];
+    }
+};
