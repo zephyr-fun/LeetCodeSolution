00dff4082ed94c752d46ca9f870c41c1f361c01b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-28 15:02:51 +0800

    Update 583_Delete_Operation_for_Two_Strings.cpp

--- dp/583_Delete_Operation_for_Two_Strings.cpp
+++ dp/583_Delete_Operation_for_Two_Strings.cpp
@@ -63,4 +63,23 @@ public:
     }
 };
 
-// LCS -> LIS, cant now
+// LCS -> LIS, cant now, cause there are repeated numbers
+
+// 2022.05.28
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for(int i = 1; i <= word1.size(); i++) {
+            for(int j = 1; j <= word2.size(); j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
+    }
+};
