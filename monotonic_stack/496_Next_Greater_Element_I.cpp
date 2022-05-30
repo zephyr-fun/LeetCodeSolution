8224484af6a6d81f1c0b6617002eafa899ca94c5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-30 14:51:26 +0800

    Update 496_Next_Greater_Element_I.cpp

--- monotonic_stack/496_Next_Greater_Element_I.cpp
+++ monotonic_stack/496_Next_Greater_Element_I.cpp
@@ -32,3 +32,32 @@ public:
         return res;
     }
 };
+
+// 2022.05.30
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> map;
+        for(int i = 0; i < nums1.size(); i++) {
+            map[nums1[i]] = i;
+        }
+        stack<int> st;
+        st.push(0);
+        vector<int> res(nums1.size(), -1);
+        for(int i = 1; i < nums2.size(); i++) {
+            if(nums2[i] <= nums2[st.top()]) {
+                st.push(i);
+            }
+            else {
+                while(!st.empty() && nums2[i] > nums2[st.top()]) {
+                    if(map.find(nums2[st.top()]) != map.end()) {
+                        res[map[nums2[st.top()]]] = nums2[i];
+                    }
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return res;
+    }
+};
