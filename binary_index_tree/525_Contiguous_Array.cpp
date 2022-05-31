627e4d474e62081a68e0186da879785db4988d93
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-05-31 20:21:04 +0800

    Update 525_Contiguous_Array.cpp

--- binary_index_tree/525_Contiguous_Array.cpp
+++ binary_index_tree/525_Contiguous_Array.cpp
@@ -41,3 +41,27 @@ public:
         return res;
     }
 };
+
+// 2022.05.31
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        // hash_map records <state, index>, when state appear first time, so that keep the index minimum
+        int map[200007];
+        memset(map, 0x3f, sizeof(map));
+        int M = 200007 / 2;
+        map[0 + M] = -1;
+        int curState = 0;
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] == 0 ? curState-- : curState++;
+            if(map[curState + M] != 0x3f3f3f3f) {
+                res = max(res, i - map[curState + M]);
+            }
+            else {
+                map[curState + M] = i;
+            }
+        }
+        return res;
+    }
+};
