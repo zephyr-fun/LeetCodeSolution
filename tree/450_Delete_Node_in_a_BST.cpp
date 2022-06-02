f6a142c47bfaca00aaf927ed62060b604bc2d633
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-02 22:16:11 +0800

    Update 450_Delete_Node_in_a_BST.cpp

--- tree/450_Delete_Node_in_a_BST.cpp
+++ tree/450_Delete_Node_in_a_BST.cpp
@@ -141,3 +141,57 @@ public:
         return root;
     }
 };
+
+// 2022.06.02
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == nullptr) {
+            return nullptr;
+        }
+        if(root->val == key) {
+            if(root->left == nullptr && root->right == nullptr) {
+                delete root;
+                return nullptr;
+            }
+            else if(root->left == nullptr) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if(root->right == nullptr) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            else {
+                TreeNode* temp = root->right;
+                TreeNode* leftMost = temp;
+                while(leftMost->left != nullptr) {
+                    leftMost = leftMost->left;
+                }
+                leftMost->left = root->left;
+                delete root;
+                return temp;
+            }
+        }
+        else if(root->val > key) {
+            root->left = deleteNode(root->left, key);
+        }
+        else {
+            root->right = deleteNode(root->right, key);
+        }
+        return root;
+    }
+};
