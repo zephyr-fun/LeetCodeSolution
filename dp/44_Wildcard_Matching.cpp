791536408db0b58e93a16ed992dc1b9a0a5b6d14
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-03 09:23:02 +0800

    Update 44_Wildcard_Matching.cpp

--- dp/44_Wildcard_Matching.cpp
+++ dp/44_Wildcard_Matching.cpp
@@ -1,4 +1,10 @@
 // 2022.06.02
+// \textit{dp}[0][0] = \text{True}dp[0][0]=True，即当字符串 ss 和模式 pp 均为空时，匹配成功；
+
+// \textit{dp}[i][0] = \text{False}dp[i][0]=False，即空模式无法匹配非空字符串；
+
+// \textit{dp}[0][j]dp[0][j] 需要分情况讨论：因为星号才能匹配空字符串，所以只有当模式 pp 的前 jj 个字符均为星号时，\textit{dp}[0][j]dp[0][j] 才为真。
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
