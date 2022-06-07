0712f4750f9ced2bbf231677e91ec3e4de2dd0d6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-07 11:53:20 +0800

    Update 307_Range_Sum_Query-Mutable.cpp

--- binary_index_tree/307_Range_Sum_Query-Mutable.cpp
+++ binary_index_tree/307_Range_Sum_Query-Mutable.cpp
@@ -101,3 +101,59 @@ private:
     vector<int> nums_;
     FenwickTree tree_;
 };
+
+// 2022.06.07
+class FenWickTree {
+public:
+    FenWickTree(int n): sums_(n + 1, 0) {}
+
+    void update(int idx, int delta) {
+        while(idx < sums_.size()) {
+            sums_[idx] += delta;
+            idx += lowbit(idx);
+        }
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        while(idx > 0) {
+            sum += sums_[idx];
+            idx -= lowbit(idx);
+        }
+        return sum;
+    }
+private:
+    vector<int> sums_;
+    int lowbit(int x) {
+        return x & (-x);
+    }
+};
+class NumArray {
+public:
+    NumArray(vector<int>& nums): tree(nums.size()) {
+        int n = nums.size();
+        nums_ = nums;
+        for(int i = 0; i < n; i++) {
+            tree.update(i + 1, nums[i]);
+        }
+    }
+    
+    void update(int index, int val) {
+        tree.update(index + 1, val - nums_[index]);
+        nums_[index] = val;
+    }
+    
+    int sumRange(int left, int right) {
+        return tree.query(right + 1) - tree.query(left);
+    }
+private:
+    FenWickTree tree;
+    vector<int> nums_;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
