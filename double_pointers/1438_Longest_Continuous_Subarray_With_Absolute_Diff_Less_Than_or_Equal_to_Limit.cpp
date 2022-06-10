709f80b4bfbd8d944cc9efec0303058636b75207
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-10 16:35:37 +0800

    Update 1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp

--- double_pointers/1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
+++ double_pointers/1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
@@ -29,3 +29,59 @@ public:
         return res;
     }
 };
+
+// 2022.06.10
+// maxQue && minQue && order
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        deque<int> maxQue;
+        deque<int> minQue;
+        int n = nums.size();
+        int res = 0;
+        for(int left = 0, right = 0; right < n; right++) {
+            while(!maxQue.empty() && nums[right] > maxQue.back()) {
+                maxQue.pop_back();
+            }
+            maxQue.push_back(nums[right]);
+            while(!minQue.empty() && nums[right] < minQue.back()) {
+                minQue.pop_back();
+            }
+            minQue.push_back(nums[right]);
+            while(left <= right && maxQue.front() - minQue.front() > limit) {
+                if(maxQue.front() == nums[left]) {
+                    maxQue.pop_front();
+                }
+                if(minQue.front() == nums[left]) {
+                    minQue.pop_front();
+                }
+                left++;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
+
+// multiset, already ordered
+// diff between set and multiset
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        multiset<int> set;
+        int n = nums.size();
+        int res = 0;
+        int left = 0;
+        int right = 0;
+        while(right < n) {
+            set.insert(nums[right]);
+            while(left <= right && *set.rbegin() - *set.begin() > limit) {//*set.rbegin() - *set.begin()
+                set.erase(set.find(nums[left]));
+                left++;
+            }
+            res = max(res, right - left + 1);
+            right++;
+        }
+        return res;
+    }
+};
