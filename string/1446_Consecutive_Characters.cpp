0b6d5914aed50540e208221302f2b5c427597917
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-10 16:35:34 +0800

    Update 1446_Consecutive_Characters.cpp

--- string/1446_Consecutive_Characters.cpp
+++ string/1446_Consecutive_Characters.cpp
@@ -16,3 +16,23 @@ public:
         return res;
     }
 };
+
+// 2022.06.10
+class Solution {
+public:
+    int maxPower(string s) {
+        int res = 1;
+        int cur = 1;
+        int n = s.size();
+        for(int i = 1; i < n; i++) {
+            if(s[i] == s[i - 1]) {
+                cur++;
+            }
+            else {
+                cur = 1;
+            }
+            res = max(res, cur);
+        }
+        return res;
+    }
+};
