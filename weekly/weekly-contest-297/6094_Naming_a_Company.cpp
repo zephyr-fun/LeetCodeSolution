cd55a3f2923975674c3a144ece7f807ba9f09aff
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-13 16:49:45 +0800

    Create 6094_Naming_a_Company.cpp

---
+++ weekly/weekly-contest-297/6094_Naming_a_Company.cpp
@@ -0,0 +1,32 @@
+// 2022.06.13
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        long long res = 0;
+        unordered_map<string, int> group;
+        for(auto& idea : ideas) {
+            group[idea.substr(1)] |= 1 << (idea[0] - 'a');
+        }
+        int cnt[26][26]; // refers to total nums of group with i and without j
+        memset(cnt, 0, sizeof(cnt)); // matters when using []
+        for(auto& [_, mask] : group) {
+            for(int i = 0; i < 26; i++) {
+                if((mask >> i & 1) == 0) {
+                    for(int j = 0; j < 26; j++) {
+                        if(mask >> j & 1) {
+                            cnt[i][j]++;
+                        }
+                    }
+                }
+                else {
+                    for(int j = 0; j < 26; j++) {
+                        if((mask >> j & 1) == 0) {
+                            res += cnt[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        return res * 2;
+    }
+};
