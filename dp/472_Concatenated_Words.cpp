4d58d2146b9fdb98eced24aab6561f087a4002ed
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-14 19:59:34 +0800

    Update 472_Concatenated_Words.cpp

--- dp/472_Concatenated_Words.cpp
+++ dp/472_Concatenated_Words.cpp
@@ -107,3 +107,54 @@ public:
 // 0,-1,-1,1,-1,-1,2,-1,-1,3,-1,-1,1,
 // i:9
 // 0,-1,-1,1,-1,-1,2,-1,-1,3,-1,-1,4,
+
+// 2022.06.14
+// hash + dp
+class Solution {
+public:
+    // 13, 131, 1313, 13131......
+    int P = 131, OFFSET = 128;
+    unordered_set<unsigned long long> set;
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        int size = words.size();
+        // preprocess string to a hash set
+        for(int i = 0; i < size; i++) {
+            unsigned long long hash = 0;
+            for(auto& letter : words[i]) {
+                hash = hash * P + (letter - 'a') + OFFSET;
+            }
+            set.insert(hash);
+        }
+        vector<string> res;
+        // for each word, use dp to process it
+        for(auto word : words) {
+            if(check(word)) {
+                res.push_back(word);
+            }
+        }
+        return res;
+    }
+    bool check(string word) {
+        int size = word.size();
+        // dp[j], considering the first j chars, the maximum ways to get item
+        vector<int> dp(size + 1, -1);
+        dp[0] = 0;
+        for(int i = 0; i <= size; i++) {
+            if(dp[i] == -1) {
+                continue;
+            }
+            unsigned long long cur = 0;
+            // i->j form a word in set
+            for(int j = i + 1; j <= size; j++) {
+                cur = cur * P + (word[j - 1] - 'a') + OFFSET;
+                if(set.count(cur)) {
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+            if(dp[size] > 1) {
+                    return true;
+            }
+        }
+        return false;
+    }
+};
