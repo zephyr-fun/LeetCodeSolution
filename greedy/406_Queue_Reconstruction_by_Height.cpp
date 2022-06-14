497bb97451c5ec5f71a24303b46ec75f47bfca98
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-14 16:58:29 +0800

    Update 406_Queue_Reconstruction_by_Height.cpp

--- greedy/406_Queue_Reconstruction_by_Height.cpp
+++ greedy/406_Queue_Reconstruction_by_Height.cpp
@@ -62,3 +62,21 @@ public:
 };
 
 // binary search + BIT
+
+// 2022.06.14
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b) {
+            if(a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            return a[0] > b[0];
+        });
+        vector<vector<int>> res;
+        for(auto& person : people) {
+            res.insert(res.begin() + person[1], person);
+        }
+        return res;
+    }
+};
