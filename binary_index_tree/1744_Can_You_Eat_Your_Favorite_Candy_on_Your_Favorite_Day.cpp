2bb59b0cf75e5825a8f76a18288dfbea020b3136
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-16 11:21:57 +0800

    Update 1744_Can_You_Eat_Your_Favorite_Candy_on_Your_Favorite_Day.cpp

--- binary_index_tree/1744_Can_You_Eat_Your_Favorite_Candy_on_Your_Favorite_Day.cpp
+++ binary_index_tree/1744_Can_You_Eat_Your_Favorite_Candy_on_Your_Favorite_Day.cpp
@@ -20,3 +20,28 @@ public:
         return res;
     }
 };
+
+// 2022.06.16
+class Solution {
+public:
+    vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries) {
+        int n = candiesCount.size();
+        int m = queries.size();
+        vector<long> sum(n + 1, 0); // note type long
+        for(int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + candiesCount[i - 1];
+        }
+        vector<bool> res(m, false);
+        for(int i = 0; i < m; i++) {
+            int type = queries[i][0];
+            int day = queries[i][1] + 1;
+            int cap = queries[i][2];
+            long early = sum[type] / cap + 1; // note type long
+            long later = sum[type + 1]; // note type long
+            if(day >= early && day <= later) {
+                res[i] = true;
+            }
+        }
+        return res;
+    }
+};
