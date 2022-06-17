7c0cd658780b5608170d14e2624cfcd162114fd6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-17 10:00:48 +0800

    Update 1089_Duplicate_Zeros.cpp

--- double_pointers/1089_Duplicate_Zeros.cpp
+++ double_pointers/1089_Duplicate_Zeros.cpp
@@ -1,4 +1,5 @@
 // 2022.06.17
+// plain solution
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
@@ -26,5 +27,39 @@ public:
             oldIdx--;
         }
         arr.resize(n);
+    }
+};
+
+// simulate stack
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int top = 0; // refers to the top of a stack
+        int i = -1; // refers to current arr index
+        int n = arr.size();
+        while(top < n) {
+            i++;
+            if(arr[i] == 0) {
+                top += 2;
+            }
+            else {
+                top++;
+            }
+        }
+        int j = n - 1;
+        if(top == n + 1) {
+            arr[j] = 0;
+            i--;
+            j--;
+        }
+        while(i >= 0) {
+            arr[j] = arr[i];
+            j--;
+            if(arr[i] == 0) {
+                arr[j] = arr[i];
+                j--;
+            }
+            i--;
+        }
     }
 };
