230c9f233e79282c7a3a768ca01dd4b5b71b09c0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-19 10:28:28 +0800

    Update 508_Most_Frequent_Subtree_Sum.cpp

--- tree/508_Most_Frequent_Subtree_Sum.cpp
+++ tree/508_Most_Frequent_Subtree_Sum.cpp
@@ -48,3 +48,54 @@ vector<int> findFrequentTreeSum(TreeNode* root) {
     }
     return res;
 }
+
+// 2022.06.19
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> map;
+    vector<int> res;
+    void traversalSum(TreeNode* root) {
+        if(root->left == nullptr && root->right == nullptr) {
+            map[root->val]++;
+            return ;
+        }
+        int left = 0;
+        int right = 0;
+        if(root->left) {
+            traversalSum(root->left);
+            left = root->left->val;
+        }
+        if(root->right) {
+            traversalSum(root->right);
+            right = root->right->val;
+        }
+        root->val = left + right + root->val;
+        map[root->val]++;
+    }
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        traversalSum(root);
+        int maxCnt = 1;
+        for(auto it : map) {
+            if(it.second > maxCnt) {
+                res.clear();
+                maxCnt = it.second;
+                res.emplace_back(it.first);
+            }
+            else if(it.second == maxCnt) {
+                res.emplace_back(it.first);
+            }
+        }
+        return res;
+    }
+};
