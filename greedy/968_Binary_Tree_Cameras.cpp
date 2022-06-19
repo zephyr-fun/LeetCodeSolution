223c4d20d08a06f387933b5d4ff0ea447ce90137
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-19 16:36:40 +0800

    Update 968_Binary_Tree_Cameras.cpp

--- greedy/968_Binary_Tree_Cameras.cpp
+++ greedy/968_Binary_Tree_Cameras.cpp
@@ -39,3 +39,46 @@ public:
         return res;
     }
 };
+
+// 2022.06.19
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 0 for no cover, 1 for with cam, 2 for with cover
+class Solution {
+public:
+    int res = 0;
+    int traversal(TreeNode* cur) {
+        if(cur == nullptr) {
+            return 2;
+        }
+        int left = traversal(cur->left);
+        int right = traversal(cur->right);
+        if(left == 2 && right == 2) {
+            return 0;
+        }
+        else if(left == 0 || right == 0) {
+            res++;
+            return 1;
+        }
+        else if(left == 1 || right == 1) {
+            return 2;
+        }
+        return -1;
+    }
+    int minCameraCover(TreeNode* root) {
+        int rootStatus = traversal(root);
+        if(!rootStatus) {
+            res++;
+        }
+        return res;
+    }
+};
