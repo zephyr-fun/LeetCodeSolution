49ad5e4d786a758ca3220781d639567aeda3cdd6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-24 09:39:26 +0800

    Update 515_Find_Largest_Value_in_Each_Tree_Row.cpp

--- tree/515_Find_Largest_Value_in_Each_Tree_Row.cpp
+++ tree/515_Find_Largest_Value_in_Each_Tree_Row.cpp
@@ -69,3 +69,44 @@ public:
         return res;
     }
 };
+
+// 2022.06.24
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> res;
+        if(root == nullptr) {
+            return res;
+        }
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            int maxVal = INT_MIN; // note that -0x3f3f3f3f > some examples
+            for(int i = 0; i < size; i++) {
+                TreeNode* cur = que.front();
+                que.pop();
+                maxVal = max(maxVal, cur->val);
+                if(cur->left) {
+                    que.push(cur->left);
+                }
+                if(cur->right) {
+                    que.push(cur->right);
+                }
+            }
+            res.emplace_back(maxVal);
+        }
+        return res;
+    }
+};
