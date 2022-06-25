436492a88edc1296f32b89b073cf50073543be7b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-25 15:15:00 +0800

    Create 413_Arithmetic_Slices.cpp

---
+++ dp/413_Arithmetic_Slices.cpp
@@ -0,0 +1,40 @@
+// 2022.06.25
+// ingenious, actually this is dp too
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 2) {
+            return 0;
+        }
+        int tot = 0;
+        int diff = nums[0] - nums[1];
+        int res = 0;
+        for(int i = 2; i < n; i++) { // find out if nums[i] can concate to the prev subarray
+            if(nums[i - 1] - nums[i] == diff) {
+                tot++;
+            }
+            else {
+                tot = 0;
+                diff = nums[i - 1] - nums[i];
+            }
+            res += tot;
+        }
+        return res;
+    }
+};
+
+// dp
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        int dp = 0;
+        int res = 0;
+        for(int i = 2; i < n; i++) {
+            dp = nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2] ? dp + 1 : 0;
+            res += dp;
+        }
+        return res;
+    }
+};
