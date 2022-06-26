01f389273f662b180ba3ab5b363fd7588ecf29e5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-26 09:33:13 +0800

    Create 729_My_Calendar_I.cpp

---
+++ segment_tree/729_My_Calendar_I.cpp
@@ -0,0 +1,36 @@
+// 2022.06.26
+// pair<int, int> store segment
+class MyCalendar {
+public:
+    set<pair<int, int>> set;
+    const int INF = 2e9;
+    MyCalendar() {
+        set.insert({-INF, -INF});
+        set.insert({INF, INF});
+    }
+    
+    bool check(pair<int, int> a, pair<int, int> b) {
+        if(a.second <= b.first || b.second <= a.first) {
+            return false;
+        }
+        return true;
+    }
+
+    bool book(int start, int end) {
+        auto i = set.lower_bound({start, INF}); // first number less or equal to {key}
+        auto j = i;
+        j--;
+        pair<int, int> t(start, end);
+        if(check(*i, t) || check(*j, t)) {
+            return false;
+        }
+        set.insert(t);
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
