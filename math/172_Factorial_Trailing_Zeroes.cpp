ca05165a0acb2b9fab3a61c7e47728cc8827c7ed
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-27 09:29:24 +0800

    Create 172_Factorial_Trailing_Zeroes.cpp

---
+++ math/172_Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,8 @@
+// 2022.06.27
+// https://leetcode.cn/problems/factorial-trailing-zeroes/solution/by-ac_oier-1y6w/
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+};
