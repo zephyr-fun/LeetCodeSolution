1e94d53a796626d31ad966f55a3e4eb56424ac51
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-28 11:40:59 +0800

    Update 686_Repeated_String_Match.cpp

--- binary_search/686_Repeated_String_Match.cpp
+++ binary_search/686_Repeated_String_Match.cpp
@@ -27,3 +27,62 @@ public:
         return res;
     }
 };
+
+// 2022.06.28
+// brute force, no analyze
+class Solution {
+public:
+    bool check(string& a, string& b) {
+        if(a.size() < b.size() || a.find(b) == string::npos) {
+            return false;
+        }
+        return true;
+    }
+    int repeatedStringMatch(string a, string b) {
+        int n = a.size();
+        int m = b.size();
+        int left = 0;
+        int right = m / n + 2;
+        int res = -1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            string temp = "";
+            for(int i = 0; i < mid; i++) {
+                temp += a;
+            }
+            if(check(temp, b)) {
+                res = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+};
+
+// https://leetcode.cn/problems/repeated-string-match/solution/gong-shui-san-xie-yi-ti-san-jie-qia-chan-3hbr/
+// analyze upper bound and lower bound
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int lower = b.size() / a.size();
+        if(b.size() % a.size() != 0) {
+            lower += 1;
+        }
+        int upper = lower + 1;
+        string low = "";
+        for(int i = 0; i < lower; i++) {
+            low += a;
+        }
+        string up = low + a;
+        if(low.find(b) != string::npos) {
+            return lower;
+        }
+        if(up.find(b) != string::npos) {
+            return upper;
+        }
+        return -1;
+    }
+};
