1616af4ba86c68e3f7b404366657c412ad8fa66a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-28 09:53:42 +0800

    Update 172_Factorial_Trailing_Zeroes.cpp

--- math/172_Factorial_Trailing_Zeroes.cpp
+++ math/172_Factorial_Trailing_Zeroes.cpp
@@ -6,3 +6,11 @@ public:
         return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
     }
 };
+
+// 2022.06.28
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+    }
+};
