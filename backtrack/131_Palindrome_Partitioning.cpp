dcf42208f5449f558a5a244d048f38241dcc9cd6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-29 10:54:39 +0800

    Update 131_Palindrome_Partitioning.cpp

--- backtrack/131_Palindrome_Partitioning.cpp
+++ backtrack/131_Palindrome_Partitioning.cpp
@@ -144,3 +144,42 @@ public:
         }
     }
 };
+
+// 2022.06.29
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int j = 0; j < n; j++) {
+            for(int i = j; i >= 0; i--) {
+                if(i == j) {
+                    dp[i][j] = true;
+                }
+                else if(j - i + 1 == 2) {
+                    dp[i][j] = s[i] == s[j];
+                }
+                else {
+                    dp[i][j] = dp[i + 1][j - 1] && s[i] == s[j];
+                }
+            }
+        }
+        vector<vector<string>> res;
+        vector<string> path;
+        dfs(s, 0, res, path, dp);
+        return res;
+    }
+    void dfs(string& s, int startIdx, vector<vector<string>>& res, vector<string>& path, vector<vector<bool>>& dp) {
+        if(startIdx == s.size()) {
+            res.emplace_back(path);
+            return ;
+        }
+        for(int i = startIdx; i < s.size(); i++) {
+            if(dp[startIdx][i]) {
+                path.emplace_back(s.substr(startIdx, i - startIdx + 1));
+                dfs(s, i + 1, res, path, dp);
+                path.pop_back();
+            }
+        }
+    }
+};
