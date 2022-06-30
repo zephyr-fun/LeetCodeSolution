de2da66cb416acc7fa259a6807801fda2a36b905
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-30 16:20:53 +0800

    Update 729_My_Calendar_I.cpp

--- segment_tree/729_My_Calendar_I.cpp
+++ segment_tree/729_My_Calendar_I.cpp
@@ -34,3 +34,33 @@ public:
  * MyCalendar* obj = new MyCalendar();
  * bool param_1 = obj->book(start,end);
  */
+
+// 2022.06.30
+class MyCalendar {
+public:
+    set<pair<int, int>> set;
+    const int INF = 2e9;
+    MyCalendar() {
+        set.insert({INF, INF});
+        set.insert({-INF, -INF});
+    }
+    
+    bool check(pair<int, int> a, pair<int, int> b) {
+        if(a.second <= b.first || a.first >= b.second) {
+            return false;
+        }
+        return true;
+    }
+
+    bool book(int start, int end) {
+        auto i = set.lower_bound({start, end});
+        auto j = i;
+        j--;
+        pair<int, int> t(start, end);
+        if(check(*i, t) || check(*j, t)) {
+            return false;
+        }
+        set.insert(t);
+        return true;
+    }
+};
