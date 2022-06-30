3cbe94071d24989480d0160aade668004f77a536
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-30 21:35:01 +0800

    Create 23_Merge_k_Sorted_Lists.cpp

---
+++ linklist/23_Merge_k_Sorted_Lists.cpp
@@ -0,0 +1,111 @@
+// 2022.06.30
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// plain merge one by one
+class Solution {
+public:
+    ListNode* mergeTwo(ListNode* a, ListNode* b) {
+        if(a == nullptr || b == nullptr) {
+            return a == nullptr ? b : a;
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* head = dummy;
+        while(a != nullptr && b != nullptr) {
+            if(a->val < b->val) {
+                head->next = a;
+                a = a->next;
+            }
+            else {
+                head->next = b;
+                b = b->next;
+            }
+            head = head->next;
+        }
+        head->next = a == nullptr ? b : a;
+        return dummy->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* res = nullptr;
+        for(auto list : lists) {
+            res = mergeTwo(res, list);
+        }
+        return res;
+    }
+};
+
+// merge sort
+// Divide and Conquer
+class Solution {
+public:
+    ListNode* mergeTwo(ListNode* a, ListNode* b) {
+        if(a == nullptr || b == nullptr) {
+            return a == nullptr ? b : a;
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* head = dummy;
+        while(a != nullptr && b != nullptr) {
+            if(a->val < b->val) {
+                head->next = a;
+                a = a->next;
+            }
+            else {
+                head->next = b;
+                b = b->next;
+            }
+            head = head->next;
+        }
+        head->next = a == nullptr ? b : a;
+        return dummy->next;
+    }
+    ListNode* merge(vector<ListNode*>& lists, int l, int r) {
+        if(l == r) {
+            return lists[l];
+        }
+        if(l > r) {
+            return nullptr;
+        }
+        int mid = l + (r - l) / 2;
+        return mergeTwo(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        return merge(lists, 0, n - 1);
+    }
+};
+
+// heap
+class Solution {
+public:
+    struct Cmp {
+        bool operator() (ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Cmp> heap;
+        for(auto list : lists) {
+            if(list != nullptr) {
+                heap.push(list);
+            }
+        }
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        while(!heap.empty()) {
+            tail->next = heap.top();
+            heap.pop();
+            tail = tail->next;
+            if(tail->next != nullptr) {
+                heap.push(tail->next);
+            }
+        }
+        return dummy->next;
+    }
+};
