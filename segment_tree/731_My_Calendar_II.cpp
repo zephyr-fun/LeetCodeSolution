7dfe3b3da029d28a3b248cd2cc50d2d40e731753
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-06-30 16:34:21 +0800

    Update 731_My_Calendar_II.cpp

--- segment_tree/731_My_Calendar_II.cpp
+++ segment_tree/731_My_Calendar_II.cpp
@@ -28,3 +28,27 @@ public:
  * MyCalendarTwo* obj = new MyCalendarTwo();
  * bool param_1 = obj->book(start,end);
  */
+
+// 2022.06.30
+class MyCalendarTwo {
+public:
+    map<int, int> map;
+    MyCalendarTwo() {
+
+    }
+    
+    bool book(int start, int end) {
+        map[start]++;
+        map[end]--;
+        int cur = 0;
+        for(auto [k, v] : map) {
+            cur += v;
+            if(cur >= 3) {
+                map[start]--;
+                map[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
