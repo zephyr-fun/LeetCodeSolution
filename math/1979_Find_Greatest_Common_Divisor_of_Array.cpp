9ea40c34946fc7431dabbb114e7f296cfe195350
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-01 09:13:12 +0800

    Update 1979_Find_Greatest_Common_Divisor_of_Array.cpp

--- math/1979_Find_Greatest_Common_Divisor_of_Array.cpp
+++ math/1979_Find_Greatest_Common_Divisor_of_Array.cpp
@@ -14,3 +14,20 @@ public:
         return gcd(maxVal, minVal);
     }
 };
+
+// 2022.07.01
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return b ? gcd(b, a % b) : a;
+    }
+    int findGCD(vector<int>& nums) {
+        int maxVal = -0x3f3f3f3f;
+        int minVal = 0x3f3f3f3f;
+        for(auto num : nums) {
+            maxVal = max(maxVal, num);
+            minVal = min(minVal, num);
+        }
+        return gcd(maxVal, minVal);
+    }
+};
