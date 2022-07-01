162502b42ab22b902460a37497768afaaf5927cc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-01 15:57:45 +0800

    Update 1034_Coloring_A_Border.cpp

--- graph/1034_Coloring_A_Border.cpp
+++ graph/1034_Coloring_A_Border.cpp
@@ -99,3 +99,99 @@ public:
         return res;
     }
 };
+
+// bfs repeat
+class Solution {
+public:
+    const int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int n;
+    int m;
+    vector<vector<int>> grid;
+    vector<vector<int>> res;
+    int color;
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid_, int row, int col, int color_) {
+        grid = grid_;
+        color = color_;
+        n = grid.size();
+        m = grid[0].size();
+        res.resize(n, vector<int>(m, 0));
+        queue<pair<int, int>> que;
+        que.push({row, col});
+        while(!que.empty()) {
+            auto [x, y] = que.front();
+            que.pop();
+            int cnt = 0;
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+                if(grid[x][y] != grid[nx][ny]) {
+                    continue;
+                }
+                cnt++;
+                if(res[nx][ny] != 0) {
+                    continue;
+                }
+                que.push({nx, ny});
+            }
+            res[x][y] = cnt == 4 ? grid[x][y] : color;
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(res[i][j] == 0) {
+                    res[i][j] = grid[i][j];
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// dfs repeat
+class Solution {
+public:
+    const int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int n;
+    int m;
+    vector<vector<int>> grid;
+    vector<vector<int>> res;
+    int color;
+    void dfs(int x, int y) {
+        int cnt = 0;
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dirs[i][0];
+            int ny = y + dirs[i][1];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+            if(grid[x][y] != grid[nx][ny]) {
+                continue;
+            }
+            cnt++;
+            if(res[nx][ny] != 0) {
+                continue;
+            }
+            res[nx][ny] = -1;
+            dfs(nx, ny);
+        }
+        res[x][y] = cnt == 4 ? grid[x][y] : color;
+    }
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid_, int row, int col, int color_) {
+        grid = grid_;
+        color = color_;
+        n = grid.size();
+        m = grid[0].size();
+        res.resize(n, vector<int>(m, 0));
+        dfs(row, col);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(res[i][j] == 0) {
+                    res[i][j] = grid[i][j];
+                }
+            }
+        }
+        return res;
+    }
+};
