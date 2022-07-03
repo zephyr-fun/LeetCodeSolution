d010377e2071677555c0f86ad394021ca86add0b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-03 19:45:17 +0800

    Update 6111_Spiral_Matrix_IV.cpp

--- weekly/weekly-contest-300/6111_Spiral_Matrix_IV.cpp
+++ weekly/weekly-contest-300/6111_Spiral_Matrix_IV.cpp
@@ -33,3 +33,29 @@ public:
         return res;
     }
 };
+
+// 2022.07.03
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int n, int m, ListNode* head) {
+        int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int dir = 1;
+        vector<vector<int>> res(n, vector<int>(m, -1));
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < n * m && head != nullptr; i++) {
+            res[x][y] = head->val;
+            int nx = x + dirs[dir][0];
+            int ny = y + dirs[dir][1];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || res[nx][ny] != -1) {
+                dir = (dir + 1) % 4;
+                nx = x + dirs[dir][0];
+                ny = y + dirs[dir][1];
+            }
+            x = nx;
+            y = ny;
+            head = head->next;
+        }
+        return res;
+    }
+};
