6ae570e9dca0af6056289c8ee665a779655221b0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-03 18:37:24 +0800

    Update 556_Next_Greater_Element_III.cpp

--- double_pointers/556_Next_Greater_Element_III.cpp
+++ double_pointers/556_Next_Greater_Element_III.cpp
@@ -34,3 +34,26 @@ public:
         return res > INT_MAX ? -1 : res;
     }
 };
+
+// 2022.07.03
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string nums = to_string(n);
+        int i = nums.size() - 2;
+        int j = nums.size() - 1;
+        while(i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if(i < 0) {
+            return -1;
+        }
+        while(j >= 0 && nums[j] <= nums[i]) {
+            j--;
+        }
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
+        long res = stol(nums);
+        return res > INT_MAX ? -1 : res;
+    }
+};
