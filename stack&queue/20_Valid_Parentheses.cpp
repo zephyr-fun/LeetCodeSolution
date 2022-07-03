95c028102d3da7dc68f4d4071881d4de13cc15f1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-03 10:22:33 +0800

    Update 20_Valid_Parentheses.cpp

--- stack&queue/20_Valid_Parentheses.cpp
+++ stack&queue/20_Valid_Parentheses.cpp
@@ -51,3 +51,70 @@ public:
         return st.empty();
     }
 };
+
+// 2022.07.03
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        bool flag = false;
+        for(auto c : s) {
+            if(c == '(') {
+                st.push(')');
+            }
+            else if(c == '[') {
+                st.push(']');
+            }
+            else if(c == '{') {
+                st.push('}');
+            }
+            else {
+                if(!st.empty()) {
+                    if(st.top() == c) {
+                        st.pop();
+                    }
+                    else {
+                        flag = true;
+                        break;
+                    }
+                }
+                else {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if(flag || !st.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
+
+// optim
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c : s) {
+            if(c == '(') {
+                st.push(')');
+            }
+            else if(c == '[') {
+                st.push(']');
+            }
+            else if(c == '{') {
+                st.push('}');
+            }
+            else {
+                if(st.empty() || st.top() != c) {
+                    return false;
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
