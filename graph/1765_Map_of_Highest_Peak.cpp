897b049f3056c3f5a9ede708010601e3bb7a039c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-05 17:24:14 +0800

    Create 1765_Map_of_Highest_Peak.cpp

---
+++ graph/1765_Map_of_Highest_Peak.cpp
@@ -0,0 +1,46 @@
+// 2022.07.05
+// multi source bfs
+class Solution {
+public:
+    const int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int getIdx(int x, int y, int n) {
+        return x * n + y;
+    }
+    pair<int, int> getPos(int cur, int n) {
+        return {cur / n, cur % n};
+    }
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int n = isWater.size();
+        int m = isWater[0].size();
+        queue<int> que;
+        unordered_map<int, int> map;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(isWater[i][j]) {
+                    que.push(getIdx(i, j, m));
+                    map[getIdx(i, j, m)] = 0;
+                    isWater[i][j] = 0;
+                }
+            }
+        }
+        while(!que.empty()) {
+            auto [x, y] = getPos(que.front(), m);
+            int step = map[que.front()];
+            que.pop();
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+                if(map.find(getIdx(nx, ny, m)) != map.end()) {
+                    continue;
+                }
+                isWater[nx][ny] = step + 1;
+                que.push(getIdx(nx, ny, m));
+                map[getIdx(nx, ny, m)] = step + 1;
+            }
+        }
+        return isWater;
+    }
+};
