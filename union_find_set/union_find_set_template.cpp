b31ea338fd3470fd52ca9eb8d95498e8a8e8b0b1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-05 10:01:01 +0800

    Update union_find_set_template.cpp

--- union_find_set/union_find_set_template.cpp
+++ union_find_set/union_find_set_template.cpp
@@ -37,3 +37,59 @@ private:
     vector<int> ranks_;
     vector<int> parent_;
 };
+
+// with size
+// grid is a spatial constraint
+class UnionFindSet {
+public:
+    UnionFindSet(int n, vector<vector<int>>& grid) : rank(n + 1, 0), parent(n + 1, 0), size(n + 1, 0) {
+        for(int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) {
+                    size[i * grid.size() + j] = 1;
+                }
+            }
+        }
+    }
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+            size[pu] += size[pv];
+        }
+        return true;
+    }
+
+    int getSize(int u) {
+        int pu = Find(u);
+        return size[pu];
+    }
+private:
+    vector<int> rank;
+    vector<int> parent;
+    vector<int> size;
+};
