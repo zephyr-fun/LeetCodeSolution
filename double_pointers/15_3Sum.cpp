7a5f239042895eb675b0d0d3e4accc4a3178537b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-06 11:08:27 +0800

    Update 15_3Sum.cpp

--- double_pointers/15_3Sum.cpp
+++ double_pointers/15_3Sum.cpp
@@ -47,3 +47,62 @@ public:
         return res;
     }
 };
+
+// 2022.07.06
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        if(nums.size() == 0) {
+            return res;
+        }
+        sort(nums.begin(), nums.end());
+        if(nums[0] > 0) {
+            return res;
+        }
+        for(int i = 0; i < n; i++) {
+            // remove repeat
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int temp = 0 - nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                if(nums[left] + nums[right] < temp) {
+                    left++;
+                    // remove repeat
+                    while(nums[left] == nums[left - 1] && left < right) {
+                        left++;
+                    }
+                }
+                else if(nums[left] + nums[right] > temp) {
+                    right--;
+                    // remove repeat
+                    while(nums[right] == nums[right + 1] && left < right) {
+                        right--;
+                    }
+                }
+                else {
+                    vector<int> path;
+                    path.emplace_back(nums[i]);
+                    path.emplace_back(nums[left]);
+                    path.emplace_back(nums[right]);
+                    res.emplace_back(path);
+                    left++;
+                    // remove repeat
+                    while(nums[left] == nums[left - 1] && left < right) {
+                        left++;
+                    }
+                    right--;
+                    // remove repeat
+                    while(nums[right] == nums[right + 1] && left < right) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
