8dabf2b19ba418ea062f10f8dc791abcb40ff9c0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-07 19:59:37 +0800

    Update 1036_Escape_a_Large_Maze.cpp

--- graph/1036_Escape_a_Large_Maze.cpp
+++ graph/1036_Escape_a_Large_Maze.cpp
@@ -60,3 +60,69 @@ public:
         return false;
     }
 };
+
+// 2022.07.07
+// bfs
+class Solution {
+public:
+    vector<int> source;
+    vector<int> target;
+    vector<vector<int>> blocked;
+    int maxVis;
+    const int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    unordered_set<long long> set;
+    bool isArrive(bool isSource, long long x, long long y) {
+        if(isSource) {
+            if(x == target[0] && y == target[1]) {
+                return true;
+            }
+        }
+        else {
+            if(x == source[0] && y == source[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool bfs(int startX, int startY, bool isSource) {
+        unordered_set<long long> vis;
+        queue<long long> que;
+        vis.insert(startX * 1e6 + startY);
+        que.push(startX * 1e6 + startY);
+        while(!que.empty() && vis.size() <= maxVis) {
+            long long x = que.front() / 1e6;
+            long long y = que.front() % (long long)(1e6);
+            que.pop();
+            for(int i = 0; i < 4; i++) {
+                long long nx = x + dirs[i][0];
+                long long ny = y + dirs[i][1];
+                if(nx < 0 || nx >= 1e6 || ny < 0 || ny >= 1e6) {
+                    continue;
+                }
+                if(vis.count(nx * 1e6 + ny)) {
+                    continue;
+                }
+                if(set.count(nx * 1e6 + ny)) {
+                    continue;
+                }
+                if(isArrive(isSource, nx, ny)) {
+                    return true;
+                }
+                vis.insert(nx * 1e6 + ny);
+                que.push(nx * 1e6 + ny);
+            }
+        }
+        return vis.size() > maxVis;
+    }
+    bool isEscapePossible(vector<vector<int>>& blocked_, vector<int>& source_, vector<int>& target_) {
+        source = source_;
+        target = target_;
+        blocked = blocked_;
+        int n = blocked.size();
+        maxVis = (n + 2) * (n + 1) / 2;
+        for(int i = 0; i < n; i++) {
+            set.insert(blocked[i][0] * 1e6 + blocked[i][1]);
+        }
+        return bfs(source[0], source[1], true) && bfs(target[0], target[1], false);
+    }
+};
