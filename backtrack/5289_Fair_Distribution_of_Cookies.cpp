07d2f5608d6209855c3d726213e2226621118d57
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-07 16:51:20 +0800

    Update 5289_Fair_Distribution_of_Cookies.cpp

--- backtrack/5289_Fair_Distribution_of_Cookies.cpp
+++ backtrack/5289_Fair_Distribution_of_Cookies.cpp
@@ -61,3 +61,76 @@ public:
         return dp[k - 1][(1 << n) - 1];
     }
 };
+
+// 2022.07.07
+// brute force
+class Solution {
+public:
+    int n;
+    int k;
+    vector<int> cookies;
+    vector<int> kids;
+    int minVal;
+    void dfs(int cur) {
+        if(cur == n) {
+            int temp = 0;
+            for(int i = 0; i < k; i++) {
+                temp = max(temp, kids[i]);
+            }
+            minVal = min(minVal, temp);
+            return ;
+        }
+        for(int i = 0; i < k; i++) {
+            if(kids[i] + cookies[cur] > minVal) {
+                continue;
+            }
+            kids[i] += cookies[cur];
+            dfs(cur + 1);
+            kids[i] -= cookies[cur];
+        }
+    }
+    int distributeCookies(vector<int>& cookies_, int k_) {
+        k = k_;
+        cookies = cookies_;
+        n = cookies.size();
+        kids.resize(k, 0);
+        minVal = 0x3f3f3f3f;
+        dfs(0);
+        return minVal;
+    }
+};
+
+// dp
+class Solution {
+public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n = cookies.size();
+        vector<int> sum(1 << n, 0);
+        vector<vector<int>> dp(k, vector<int>(1 << n, -1));
+        for(int i = 1; i < (1 << n); i++) {
+            for(int j = 0; j < n; j++) {
+                if((i & (1 << j)) == 0) {
+                    continue;
+                }
+                int left = i - (1 << j);
+                sum[i] = cookies[j] + sum[left];
+                break;
+            }
+        }
+        for(int i = 0; i < (1 << n); i++) {
+            dp[0][i] = sum[i];
+        }
+        for(int i = 1; i < k; i++) {
+            for(int j = 0; j < (1 << n); j++) {
+                int minVal = 0x3f3f3f3f;
+                for(int st = j; st; st = (st - 1) & j) {
+                    int left = j - st;
+                    int val = max(dp[i - 1][left], sum[st]);
+                    minVal = min(minVal, val);
+                }
+                dp[i][j] = minVal;
+            }
+        }
+        return dp[k - 1][(1 << n) - 1];
+    }
+};
