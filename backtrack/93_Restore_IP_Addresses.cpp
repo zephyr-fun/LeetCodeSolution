776351c2befdd7a174c6164030a57ceb5efce0dd
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-09 11:16:10 +0800

    Update 93_Restore_IP_Addresses.cpp

--- backtrack/93_Restore_IP_Addresses.cpp
+++ backtrack/93_Restore_IP_Addresses.cpp
@@ -154,3 +154,53 @@ public:
         return res;
     }
 };
+
+// 2022.07.09
+class Solution {
+public:
+    vector<string> res;
+    bool isValid(string s, int start, int end) {
+        int cur = 0;
+        if(start > end) {
+            return false;
+        }
+        if(s[start] == '0' && start != end) {
+            return false;
+        }
+        for(int i = start; i <= end; i++) {
+            if(s[i] > '9' || s[i] < '0') {
+                return false;
+            }
+            cur = cur * 10 + s[i] - '0';
+            if(cur > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+    void dfs(string s, int cur, int cnt) {
+        if(cnt == 3) {
+            if(isValid(s, cur, s.size() - 1)) {
+                res.emplace_back(s);
+            }
+            return ;
+        }
+        for(int i = cur; i < s.size(); i++) {
+            if(isValid(s, cur, i)) {
+                s.insert(s.begin() + i + 1, '.');
+                dfs(s, i + 2, cnt + 1);
+                s.erase(s.begin() + i + 1);
+            }
+            else {
+                break;
+            }
+        }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        if(s.size() > 12) {
+            return res;
+        }
+        dfs(s, 0, 0);
+        return res;
+    }
+};
