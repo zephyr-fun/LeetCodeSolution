b31931084e08e36d8501fb5be87c306481cfa340
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-09 16:44:28 +0800

    Update 873_Length_of_Longest_Fibonacci_Subsequence.cpp

--- dp/873_Length_of_Longest_Fibonacci_Subsequence.cpp
+++ dp/873_Length_of_Longest_Fibonacci_Subsequence.cpp
@@ -60,3 +60,35 @@ public:
         return res;
     }
 };
+
+// repeat
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, int> map;
+        for(int i = 0; i < n; i++) {
+            map[arr[i]] = i;
+        }
+        int res = 0;
+        // dp[i][j] refers to max length of t, j, i
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++) {
+            for(int j = i - 1; j >= 0 && j + 2 > res; j--) {
+                if(arr[i] - arr[j] >= arr[j]) {
+                    break;
+                }
+                int t = -1;
+                if(map.count(arr[i] - arr[j])) {
+                    t = map[arr[i] - arr[j]];
+                }
+                if(t == -1) {
+                    continue;
+                }
+                dp[i][j] = max(3, dp[j][t] + 1);
+                res = max(res, dp[i][j]);
+            }
+        }
+        return res;
+    }
+};
