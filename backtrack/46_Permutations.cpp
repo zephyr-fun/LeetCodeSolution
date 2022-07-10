e4f3490c00afcd58b9484ccf0f87d7e6c87f58ac
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-10 09:49:55 +0800

    Update 46_Permutations.cpp

--- backtrack/46_Permutations.cpp
+++ backtrack/46_Permutations.cpp
@@ -55,3 +55,31 @@ public:
         return res;
     }
 };
+
+// 2022.07.10
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void dfs(vector<int>& nums, vector<bool>& vis, int cur) {
+        if(cur == nums.size()) {
+            res.emplace_back(path);
+            return ;
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(!vis[i]) {
+                path.emplace_back(nums[i]);
+                vis[i] = true;
+                dfs(nums, vis, cur + 1);
+                vis[i] = false;
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> vis(n, false);
+        dfs(nums, vis, 0);
+        return res;
+    }
+};
