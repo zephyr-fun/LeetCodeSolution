90403ba7dec8b9c69963786efdc0ef7bbd231e85
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-11 18:35:15 +0800

    Update 239_Sliding_Window_Maximum.cpp

--- stack&queue/239_Sliding_Window_Maximum.cpp
+++ stack&queue/239_Sliding_Window_Maximum.cpp
@@ -74,3 +74,46 @@ public:
         return res;
     }
 };
+
+// 2022.07.11
+class MyQueue {
+public:
+    deque<int> dq;
+
+    void pop(int val) {
+        if(!dq.empty() && dq.front() == val) {
+            dq.pop_front();
+        }
+    }
+
+    void push(int val) {
+        while(!dq.empty() && dq.back() < val) {
+            dq.pop_back();
+        }
+        dq.push_back(val);
+    }
+
+    int front() {
+        return dq.front();
+    }
+};
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        MyQueue que;
+        vector<int> res;
+        if(nums.size() == 0) {
+            return res;
+        }
+        for(int i = 0; i < k; i++) {
+            que.push(nums[i]);
+        }
+        res.emplace_back(que.front());
+        for(int i = k; i < nums.size(); i++) {
+            que.pop(nums[i - k]);
+            que.push(nums[i]);
+            res.emplace_back(que.front());
+        }
+        return res;
+    }
+};
