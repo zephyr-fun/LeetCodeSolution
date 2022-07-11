76d76c1c6d5b248b7b6a0c1aac4b31bb40def19f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-11 20:44:42 +0800

    Update 739_Daily_Temperatures.cpp

--- monotonic_stack/739_Daily_Temperatures.cpp
+++ monotonic_stack/739_Daily_Temperatures.cpp
@@ -23,3 +23,21 @@ public:
         return res;
     }
 };
+
+// 2022.07.11
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                res[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
