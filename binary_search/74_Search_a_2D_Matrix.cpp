003e8cdb73332b659eb1a7cd790ea146e2e517d6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-12 20:58:52 +0800

    Update 74_Search_a_2D_Matrix.cpp

--- binary_search/74_Search_a_2D_Matrix.cpp
+++ binary_search/74_Search_a_2D_Matrix.cpp
@@ -27,3 +27,26 @@ public:
         return false;
     }
 };
+
+// abstract BFS
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0;
+        int col = m - 1;
+        while(row < n && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] > target) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        return false;
+    }
+};
