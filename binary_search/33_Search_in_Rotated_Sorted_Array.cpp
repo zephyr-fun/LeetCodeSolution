4fb2f6a5337bce92f32c60ec2bae8090f5b29999
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-12 19:41:20 +0800

    Create 33_Search_in_Rotated_Sorted_Array.cpp

---
+++ binary_search/33_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,75 @@
+// 2022.07.12
+// change original vector
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(target >= nums[0]) {
+                if(nums[mid] < nums[0]) {
+                    nums[mid] = 0x3f3f3f3f;
+                }
+            }
+            else {
+                if(nums[mid] >= nums[0]) {
+                    nums[mid] = -0x3f3f3f3f;
+                }
+            }
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+// first find out Dividing Point
+// second subarea binary search
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int pos = -1;
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l <= r) {
+            int mid = (l + r) >> 1;
+            if(nums[mid] >= nums[0]) {
+                l = mid + 1;
+                pos = mid;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        if(target >= nums[0]) {
+            l = 0;
+            r = pos;
+        }
+        else {
+            l = pos + 1;
+            r = nums.size() - 1;
+        }
+        while(l <= r) {
+            int mid = (l + r) >> 1;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] > target) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
