84d8845d51cb68fce399419d6dff5e1dc32fa0fe
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-13 16:23:31 +0800

    Update 42_Trapping_Rain_Water.cpp

--- monotonic_stack/42_Trapping_Rain_Water.cpp
+++ monotonic_stack/42_Trapping_Rain_Water.cpp
@@ -141,3 +141,55 @@ public:
         return res;
     }
 };
+
+// 2022.07.13
+// col
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> maxLeft(n, 0);
+        vector<int> maxRight(n, 0);
+        maxLeft[0] = height[0];
+        maxRight[n - 1] = height[n - 1];
+        for(int i = 1; i < n; i++) {
+            maxLeft[i] = max(maxLeft[i - 1], height[i]);
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            maxRight[i] = max(maxRight[i + 1], height[i]);
+        }
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(min(maxLeft[i], maxRight[i]) - height[i] > 0) {
+                res += min(maxLeft[i], maxRight[i]) - height[i];
+            }
+        }
+        return res;
+    }
+};
+
+// layer
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        stack<int> st;
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && height[i] > height[st.top()]) {
+                int mid = st.top();
+                st.pop();
+                while(!st.empty() && height[st.top()] == height[mid]) {
+                    st.pop();
+                }
+                if(!st.empty()) {
+                    int h = min(height[st.top()], height[i]) - height[mid];
+                    int w = i - st.top() - 1;
+                    res += h * w;
+                }
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
