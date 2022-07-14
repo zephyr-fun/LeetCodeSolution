63fcf347a4d774e56fc2c7ec7faf6db8e43230c5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-14 10:25:54 +0800

    Update 543_Diameter_of_Binary_Tree.cpp

--- tree/543_Diameter_of_Binary_Tree.cpp
+++ tree/543_Diameter_of_Binary_Tree.cpp
@@ -33,3 +33,39 @@ int diameterOfBinaryTree(TreeNode* root) {
     oneSideMaxLength(root);
     return max_length;
 }
+
+// 2022.07.14
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    int oneSideMax(TreeNode* root) {
+        if(root->left == nullptr && root->right == nullptr) {
+            return 1;
+        }
+        int left = 0;
+        int right = 0;
+        if(root->left != nullptr) {
+            left = oneSideMax(root->left);
+        }
+        if(root->right != nullptr) {
+            right = oneSideMax(root->right);
+        }
+        res = max(res, left + right + 1);
+        return max(left, right) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        oneSideMax(root);
+        return res == 0 ? 0 : res - 1;
+    }
+};
