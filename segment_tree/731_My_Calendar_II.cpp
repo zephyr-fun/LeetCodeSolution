ef3c2aa36700819baabdd8a16aebb3acb74094be
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-19 11:52:21 +0800

    Update 731_My_Calendar_II.cpp

--- segment_tree/731_My_Calendar_II.cpp
+++ segment_tree/731_My_Calendar_II.cpp
@@ -52,3 +52,33 @@ public:
         return true;
     }
 };
+
+// 2022.07.19
+class MyCalendarTwo {
+public:
+    map<int, int> map;
+    MyCalendarTwo() {
+
+    }
+    
+    bool book(int start, int end) {
+        map[start]++;
+        map[end]--;
+        int cur = 0;
+        for(auto [k, v] : map) {
+            cur += v;
+            if(cur >= 3) {
+                map[start]--;
+                map[end]++;
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
