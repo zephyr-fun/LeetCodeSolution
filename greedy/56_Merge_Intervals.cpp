47fb0437017f8476425a7b6b390b4883f88a62bd
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-19 12:39:15 +0800

    Update 56_Merge_Intervals.cpp

--- greedy/56_Merge_Intervals.cpp
+++ greedy/56_Merge_Intervals.cpp
@@ -22,3 +22,28 @@ public:
         return res;
     }
 };
+
+// 2022.07.19
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            if(a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            return a[0] < b[0];
+        });
+        int n = intervals.size();
+        for(int i = 0; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            while(i + 1 < n && intervals[i + 1][0] <= end) {
+                i++;
+                end = max(end, intervals[i][1]);
+            }
+            res.emplace_back(initializer_list<int>{start, end});
+        }
+        return res;
+    }
+};
