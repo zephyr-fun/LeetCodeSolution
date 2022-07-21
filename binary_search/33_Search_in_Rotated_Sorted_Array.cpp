d8869cdf851f072f88744911d204cdd61a58d3e5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-21 16:04:20 +0800

    Update 33_Search_in_Rotated_Sorted_Array.cpp

--- binary_search/33_Search_in_Rotated_Sorted_Array.cpp
+++ binary_search/33_Search_in_Rotated_Sorted_Array.cpp
@@ -73,3 +73,45 @@ public:
         return -1;
     }
 };
+
+// 2022.07.21
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        int pos = -1;
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] >= nums[0]) {
+                left = mid + 1;
+                pos = mid;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if(target >= nums[0]) {
+            left = 0;
+            right = pos;
+        }
+        else {
+            left = pos + 1;
+            right = n - 1;
+        }
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
