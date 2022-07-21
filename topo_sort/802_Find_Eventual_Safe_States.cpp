01ac6d4716b8822c3638e50ce5c592b65262bd13
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-21 16:04:23 +0800

    Update 802_Find_Eventual_Safe_States.cpp

--- topo_sort/802_Find_Eventual_Safe_States.cpp
+++ topo_sort/802_Find_Eventual_Safe_States.cpp
@@ -36,3 +36,42 @@ public:
         return res;
     }
 };
+
+// 2022.07.21
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> reverseg(n);
+        vector<int> d(n);
+        for(int i = 0; i < n; i++) {
+            for(auto u : graph[i]) {
+                reverseg[u].emplace_back(i);
+                d[i]++;
+            }
+        }
+        queue<int> que;
+        for(int i = 0; i < n; i++) {
+            if(d[i] == 0) {
+                que.push(i);
+            }
+        }
+        while(!que.empty()) {
+            int cur = que.front();
+            que.pop();
+            for(int i = 0; i < reverseg[cur].size(); i++) {
+                d[reverseg[cur][i]]--;
+                if(d[reverseg[cur][i]] == 0) {
+                    que.push(reverseg[cur][i]);
+                }
+            }
+        }
+        vector<int> res;
+        for(int i = 0; i < n; i++) {
+            if(d[i] == 0) {
+                res.emplace_back(i);
+            }
+        }
+        return res;
+    }
+};
