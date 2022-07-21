76fc172ec63298ab4e15074d3d1120f404343fc5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-21 17:23:06 +0800

    Update 518_Coin_Change_2.cpp

--- dp/518_Coin_Change_2.cpp
+++ dp/518_Coin_Change_2.cpp
@@ -50,3 +50,19 @@ public:
         return dp[amount];
     }
 };
+
+// 2022.07.21
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        // int mod = 1e9 + 7;
+        for(auto coin : coins) {
+            for(int j = coin; j <= amount; j++) {
+                dp[j] = (dp[j] + dp[j - coin]);
+            }
+        }
+        return dp[amount];
+    }
+};
