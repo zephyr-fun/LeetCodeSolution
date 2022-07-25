9df659f5c9acc83fdd055841388d38e528e2ea4a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-25 11:20:46 +0800

    Update 797_All_Paths_From_Source_to_Target.cpp

--- graph/797_All_Paths_From_Source_to_Target.cpp
+++ graph/797_All_Paths_From_Source_to_Target.cpp
@@ -24,3 +24,30 @@ public:
         return res;
     }
 };
+
+// 2022.07.25
+class Solution {
+public:
+    int n;
+    vector<vector<int>> g;
+    vector<int> path;
+    vector<vector<int>> res;
+    void dfs(int u) {
+        if(u == n - 1) {
+            res.emplace_back(path);
+            return ;
+        }
+        for(int i = 0; i < g[u].size(); i++) {
+            path.emplace_back(g[u][i]);
+            dfs(g[u][i]);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        g = graph;
+        n = g.size();
+        path.emplace_back(0);
+        dfs(0);
+        return res;
+    }
+};
