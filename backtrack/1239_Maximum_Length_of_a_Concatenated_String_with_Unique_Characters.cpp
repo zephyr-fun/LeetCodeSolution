375f49f138cdfa528f96587be61593946ecd3faa
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-26 20:33:19 +0800

    Update 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.cpp

--- backtrack/1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.cpp
+++ backtrack/1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.cpp
@@ -1,45 +1,48 @@
 // 2022.07.26
-// doesn't work
-// class Solution {
-// public:
-//     int res = 0;
-//     vector<string> arr;
-//     bool check(string a, string b) {
-//         int i = 0;
-//         int j = 0;
-//         while(i < a.size() && j < b.size()) {
-//             if(a[i] < b[j]) {
-//                 i++;
-//             }
-//             else if(a[i] > b[j]) {
-//                 j++;
-//             }
-//             else {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-//     void dfs(string cur) {
-//         res = max(res, (int)cur.size());
-//         sort(cur.begin(), cur.end());
-//         for(int i = 0; i < arr.size(); i++) {
-//             if(check(cur, arr[i])) {
-//                 dfs(cur + arr[i]);
-//             }
-//         }
-//     }
-//     int maxLength(vector<string>& arr_) {
-//         for(auto a : arr_) {
-//             sort(a.begin(), a.end());
-//             if(unique(a.begin(), a.end()) == a.end()) {
-//                 arr.emplace_back(a);
-//             }
-//         }
-//         dfs("");
-//         return res;
-//     }
-// };
+// doesn't work, work now
+// key point lies in 27-29
+// start index
+class Solution {
+public:
+    int res = 0;
+    vector<string> arr;
+    bool check(string a, string b) {
+        int i = 0;
+        int j = 0;
+        while(i < a.size() && j < b.size()) {
+            if(a[i] < b[j]) {
+                i++;
+            }
+            else if(a[i] > b[j]) {
+                j++;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    void dfs(int u, string cur) {
+        res = max(res, (int)cur.size());
+        sort(cur.begin(), cur.end());
+        for(int i = u; i < arr.size(); i++) { // key point
+            if(check(cur, arr[i])) {
+                dfs(i + 1, cur + arr[i]);
+            }
+        }
+    }
+    int maxLength(vector<string>& arr_) {
+        for(auto a : arr_) {
+            sort(a.begin(), a.end());
+            if(unique(a.begin(), a.end()) == a.end()) {
+                arr.emplace_back(a);
+            }
+        }
+        dfs(0, "");
+        return res;
+    }
+};
+
 // whole 
 // __builtin_popcount
 class Solution {
