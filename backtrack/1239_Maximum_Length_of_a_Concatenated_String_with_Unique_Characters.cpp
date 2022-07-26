9de21e73ceb9cebf21d68e0193a88906951cd0dc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-26 18:51:53 +0800

    Create 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.cpp

---
+++ backtrack/1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters.cpp
@@ -0,0 +1,70 @@
+// 2022.07.26
+// doesn't work
+// class Solution {
+// public:
+//     int res = 0;
+//     vector<string> arr;
+//     bool check(string a, string b) {
+//         int i = 0;
+//         int j = 0;
+//         while(i < a.size() && j < b.size()) {
+//             if(a[i] < b[j]) {
+//                 i++;
+//             }
+//             else if(a[i] > b[j]) {
+//                 j++;
+//             }
+//             else {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     void dfs(string cur) {
+//         res = max(res, (int)cur.size());
+//         sort(cur.begin(), cur.end());
+//         for(int i = 0; i < arr.size(); i++) {
+//             if(check(cur, arr[i])) {
+//                 dfs(cur + arr[i]);
+//             }
+//         }
+//     }
+//     int maxLength(vector<string>& arr_) {
+//         for(auto a : arr_) {
+//             sort(a.begin(), a.end());
+//             if(unique(a.begin(), a.end()) == a.end()) {
+//                 arr.emplace_back(a);
+//             }
+//         }
+//         dfs("");
+//         return res;
+//     }
+// };
+// whole 
+// __builtin_popcount
+class Solution {
+public:
+    vector<int> a;
+    int res = 0;
+    void dfs(int s, int mask) {
+        res = max(res, __builtin_popcount(mask));
+        for(int i = s; i < a.size(); i++) {
+            if((mask & a[i]) == 0) {
+                dfs(i + 1, mask | a[i]);
+            }
+        }
+    }
+    int maxLength(vector<string>& arr) {
+        for(auto x : arr) {
+            int mask = 0;
+            for(auto c : x) {
+                mask |= 1 << (c - 'a');
+            }
+            if(__builtin_popcount(mask) == x.size()) {
+                a.emplace_back(mask);
+            }
+        }
+        dfs(0, 0);
+        return res;
+    }
+};
