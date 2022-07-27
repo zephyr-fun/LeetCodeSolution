8dbb674e38b681486a7a800f89ea68146721d324
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-27 22:26:10 +0800

    Update 2049_Count_Nodes_With_the_Highest_Score.cpp

--- tree/2049_Count_Nodes_With_the_Highest_Score.cpp
+++ tree/2049_Count_Nodes_With_the_Highest_Score.cpp
@@ -38,3 +38,44 @@ public:
         return n - tot;
     }
 };
+
+// 2022.07.27
+class Solution {
+public:
+    int n;
+    long long maxVal = -0x3f3f3f3f;
+    int res = 0;
+    vector<vector<int>> children;
+    int dfs(int u) {
+        int tot = n - 1;
+        vector<int> temp;
+        for(int i = 0; i < children[u].size(); i++) {
+            temp.emplace_back(dfs(children[u][i]));
+            tot -= temp.back();
+        }
+        long long sum = 1;
+        if(tot != 0) {
+            sum *= tot;
+        }
+        for(int i = 0; i < temp.size(); i++) {
+            sum *= temp[i];
+        }
+        if(sum > maxVal) {
+            maxVal = sum;
+            res = 1;
+        }
+        else if(sum == maxVal) {
+            res++;
+        }
+        return n - tot;
+    }
+    int countHighestScoreNodes(vector<int>& parents) {
+        n = parents.size();
+        children.resize(n);
+        for(int i = 1; i < parents.size(); i++) {
+            children[parents[i]].emplace_back(i);
+        }
+        dfs(0);
+        return res;
+    }
+};
