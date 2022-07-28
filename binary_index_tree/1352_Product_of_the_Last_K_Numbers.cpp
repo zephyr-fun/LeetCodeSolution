dc6bda904fef63ca6a4002c8047aede924e8ef08
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-28 21:58:36 +0800

    Update 1352_Product_of_the_Last_K_Numbers.cpp

--- binary_index_tree/1352_Product_of_the_Last_K_Numbers.cpp
+++ binary_index_tree/1352_Product_of_the_Last_K_Numbers.cpp
@@ -29,3 +29,35 @@ public:
  * obj->add(num);
  * int param_2 = obj->getProduct(k);
  */
+
+// 2022.07.28
+class ProductOfNumbers {
+public:
+    int prod[40007];
+    int len = 0;
+    ProductOfNumbers() {
+        memset(prod, 0, sizeof(prod));
+         prod[0] = 1;
+    }
+    
+    void add(int num) {
+        if(num == 0) {
+            len = 0;
+        }
+        else {
+            len++;
+            prod[len] = num * prod[len - 1];
+        }
+    }
+    
+    int getProduct(int k) {
+        return len >= k ? prod[len] / prod[len - k] : 0;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
