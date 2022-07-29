d70d616875268e367477e0ce9faeeb45054bdfc5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-29 19:42:55 +0800

    Create 31_Next_Permutation.cpp

---
+++ double_pointers/31_Next_Permutation.cpp
@@ -0,0 +1,19 @@
+// 2022.07.29
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if(i >= 0) {
+            int j = n - 1;
+            while(nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
