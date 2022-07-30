c5551e821aa606974d1680f9db633633f39b0068
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-07-30 09:04:48 +0800

    Create 165_Compare_Version_Numbers.cpp

---
+++ double_pointers/165_Compare_Version_Numbers.cpp
@@ -0,0 +1,31 @@
+// 2022.07.30
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.size();
+        int m = version2.size();
+        int i = 0;
+        int j = 0;
+        while(i < n || j < m) {
+            long long num1 = 0;
+            long long num2 = 0;
+            while(i < n && version1[i] != '.') {
+                num1 = num1 * 10 + version1[i] - '0';
+                i++;
+            }
+            while(j < m && version2[j] != '.') {
+                num2 = num2 * 10 + version2[j] - '0';
+                j++;
+            }
+            if(num1 < num2) {
+                return -1;
+            }
+            else if(num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
