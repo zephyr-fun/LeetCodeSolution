bf563fdcc7a0b831512339efd5c53414e6f17940
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-01 22:37:29 +0800

    Create 剑指_Offer_35_复杂链表的复制.cpp

---
+++ linklist/剑指_Offer_35_复杂链表的复制.cpp
@@ -0,0 +1,42 @@
+// 2022.08.01
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* dummy = new Node(-10007);
+        Node* t = dummy;
+        Node* cur = head;
+        unordered_map<Node*, Node*> map;
+        while(cur != nullptr) {
+            Node* temp = new Node(cur->val);
+            t->next = temp;
+            map[cur] = temp;
+            cur = cur->next;
+            t = t->next;
+        }
+        t = dummy->next;
+        cur = head;
+        while(cur != nullptr) {
+            t->random = map[cur->random];
+            t = t->next;
+            cur = cur->next;
+        }
+        return dummy->next;
+    }
+};
+
+// optim, don't use hash map
