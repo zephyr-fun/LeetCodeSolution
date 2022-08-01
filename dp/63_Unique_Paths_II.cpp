8697827ba1d6b966691adcb45f136def25b0f536
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-01 22:37:34 +0800

    Update 63_Unique_Paths_II.cpp

--- dp/63_Unique_Paths_II.cpp
+++ dp/63_Unique_Paths_II.cpp
@@ -51,3 +51,26 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// 2022.08.01
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int up = (i - 1 >= 0) ? dp[i - 1][j] : 0;
+                int left = (j - 1 >= 0) ? dp[i][j - 1] : 0;
+                dp[i][j] += up + left;
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
