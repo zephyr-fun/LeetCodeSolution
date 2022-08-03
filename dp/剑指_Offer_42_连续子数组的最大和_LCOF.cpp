fa136420e5fda375e76506089188aef3859c8ea6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-03 16:21:16 +0800

    Update 剑指_Offer_42_连续子数组的最大和_LCOF.cpp

--- dp/剑指_Offer_42_连续子数组的最大和_LCOF.cpp
+++ dp/剑指_Offer_42_连续子数组的最大和_LCOF.cpp
@@ -13,3 +13,19 @@ public:
         return res;
     }
 };
+
+// 2022.08.03
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        // dp[i] means the max value we can get end with nums[i]
+        int dp = 0;
+        int res = -0x3f3f3f3f;
+        for(int i = 0; i < n; i++) {
+            dp = max(0, dp) + nums[i];
+            res = max(res, dp);
+        }
+        return res;
+    }
+};
