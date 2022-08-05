b8134ea7b73c92cceb521f08be60e1b782dd03cb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-05 19:55:21 +0800

    Create 剑指_Offer_32-III_从上到下打印二叉树_III.cpp

---
+++ tree/剑指_Offer_32-III_从上到下打印二叉树_III.cpp
@@ -0,0 +1,43 @@
+// 2022.08.05
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == nullptr) {
+            return res;
+        }
+        queue<TreeNode*> que;
+        que.push(root);
+        bool re = false;
+        while(!que.empty()) {
+            int size = que.size();
+            vector<int> temp;
+            for(int i = 0; i < size; i++) {
+                TreeNode* cur = que.front();
+                que.pop();
+                temp.emplace_back(cur->val);
+                if(cur->left) {
+                    que.push(cur->left);
+                }
+                if(cur->right) {
+                    que.push(cur->right);
+                }
+            }
+            if(re) {
+                reverse(temp.begin(), temp.end());
+            }
+            re = !re;
+            res.emplace_back(temp);
+        }
+        return res;
+    }
+};
