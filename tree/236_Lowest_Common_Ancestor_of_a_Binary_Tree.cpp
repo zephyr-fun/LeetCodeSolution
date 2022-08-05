79cf1ce817f5035b42a600cce28ecb354b2e6e7c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-05 19:55:29 +0800

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- tree/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ tree/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -124,3 +124,36 @@ public:
         }
     }
 };
+
+// 2022.08.05
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left != nullptr && right != nullptr) {
+            return root;
+        }
+        else if(left == nullptr && right != nullptr) {
+            return right;
+        }
+        else if(left != nullptr && right == nullptr) {
+            return left;
+        }
+        else {
+            return nullptr;
+        }
+    }
+};
