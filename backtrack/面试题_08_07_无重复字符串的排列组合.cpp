0120ac4ea0bd52c2d8ccfa7ad582e88f6118993e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-06 16:20:55 +0800

    Create 面试题_08_07_无重复字符串的排列组合.cpp

---
+++ backtrack/面试题_08_07_无重复字符串的排列组合.cpp
@@ -0,0 +1,28 @@
+// 2022.08.06
+class Solution {
+public:
+    int n;
+    vector<bool> vis;
+    string s;
+    vector<string> res;
+    void dfs(string cur) {
+        if(cur.size() == n) {
+            res.emplace_back(cur);
+            return ;
+        }
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                vis[i] = true;
+                dfs(cur + s[i]);
+                vis[i] = false;
+            }
+        }
+    }
+    vector<string> permutation(string S) {
+        s = S;
+        n = s.size();
+        vis.resize(n, false);
+        dfs("");
+        return res;
+    }
+};
