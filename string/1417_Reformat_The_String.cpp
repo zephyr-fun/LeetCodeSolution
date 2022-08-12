6ae06dff68e4fb913d886091430f87d891ff7adc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-12 11:13:50 +0800

    Update 1417_Reformat_The_String.cpp

--- string/1417_Reformat_The_String.cpp
+++ string/1417_Reformat_The_String.cpp
@@ -1,3 +1,38 @@
-2022-08-11T18:46:00+08:00
-随机数:55231
-提交次数:1
+// 2022.08.11
+class Solution {
+public:
+    string reformat(string s) {
+        string a = "";
+        string b = "";
+        for(auto c : s) {
+            if(c >= 'a' && c <= 'z') {
+                a += c;
+            }
+            else {
+                b += c;
+            }
+        }
+        if(abs((int)a.size() - (int)b.size()) > 1) {
+            return "";
+        }
+        if(a.size() < b.size()) {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+        string res = "";
+        int i = 0;
+        int j = 0;
+        while(i < a.size() || j < b.size()) {
+            if(i < a.size()) {
+                res += a[i];
+                i++;
+            }
+            if(j < b.size()) {
+                res += b[j];
+                j++;
+            }
+        }
+        return res;
+    }
+};
