425961198699103731d7410c0a13733a2b15a7bc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-16 11:03:05 +0800

    Create 421_Maximum_XOR_of_Two_Numbers_in_an_Array.cpp

---
+++ trie/421_Maximum_XOR_of_Two_Numbers_in_an_Array.cpp
@@ -0,0 +1,44 @@
+// 2022.08.16
+class Solution {
+public:
+    vector<vector<int>> son;
+
+    void insert(int num) {
+        int p = 0;
+        for(int i = 30; i >= 0; i--) {
+            int u = (num >> i) & 1;
+            if(!son[p][u]) {
+                son[p][u] = son.size();
+                son.push_back({0, 0});
+            }
+            p = son[p][u];
+        }
+    }
+
+    int query(int num) {
+        int p = 0;
+        int res = 0;
+        for(int i = 30; i >= 0; i--) {
+            int u = (num >> i) & 1;
+            if(son[p][!u]) {
+                res = res * 2 + !u;
+                p = son[p][!u];
+            }
+            else {
+                res = res * 2 + u;
+                p = son[p][u];
+            }
+        }
+        return res ^ num;
+    }
+
+    int findMaximumXOR(vector<int>& nums) {
+        son.push_back({0, 0});
+        int res = 0;
+        for(auto& num : nums) {
+            res = max(res, query(num));
+            insert(num);
+        }
+        return res;
+    }
+};
