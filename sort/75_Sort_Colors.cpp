615f9871f7e66e808a8c9892277c2a40d064f103
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-17 15:05:51 +0800

    Update 75_Sort_Colors.cpp

--- sort/75_Sort_Colors.cpp
+++ sort/75_Sort_Colors.cpp
@@ -22,3 +22,28 @@ public:
         quick_sort(nums, 0, nums.size() - 1);
     }
 };
+
+// 2022.08.17
+class Solution {
+public:
+    void quick_sort(vector<int>& nums, int left, int right) {
+        if(left >= right) {
+            return ;
+        }
+        int x = nums[(left + right) >> 1];
+        int i = left - 1;
+        int j = right + 1;
+        while(i < j) {
+            while(nums[++i] < x);
+            while(nums[--j] > x);
+            if(i < j) {
+                swap(nums[i], nums[j]);
+            }
+        }
+        quick_sort(nums, left, j);
+        quick_sort(nums, j + 1, right);
+    }
+    void sortColors(vector<int>& nums) {
+        quick_sort(nums, 0, nums.size() - 1);
+    }
+};
