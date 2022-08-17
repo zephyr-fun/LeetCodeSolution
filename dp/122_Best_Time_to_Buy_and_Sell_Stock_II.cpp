9b85725979872de26e5001ad961ec4b223c0af92
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-17 08:56:22 +0800

    Update 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp

--- dp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ dp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -28,3 +28,35 @@ public:
         return dp[prices.size() - 1][1];
     }
 };
+
+// 2022.08.17
+// greedy
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        int res = 0;
+        for(int i = 1; i < len; i++) {
+            if(prices[i] - prices[i - 1] > 0) {
+                res += prices[i] - prices[i - 1];
+            }
+        }
+        return res;
+    }
+};
+
+// dp
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        vector<vector<int>> dp(len, vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < len; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+};
