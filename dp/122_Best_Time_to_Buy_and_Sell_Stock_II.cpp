9968998631678ae85aaac2d4d50dc88b113b03de
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-18 08:43:18 +0800

    Update 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp

--- dp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ dp/122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -60,3 +60,21 @@ public:
         return dp[len - 1][0];
     }
 };
+
+// 2022.08.18
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        // dp[i][0] not hold stock, max interest
+        // dp[i][0] hold stock, max interest
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+};
