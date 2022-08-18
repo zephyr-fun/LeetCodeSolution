41c367ca42c2c137da6563f8da499ae4600da45a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-18 09:10:17 +0800

    Update 137_Single_Number_II.cpp

--- random/137_Single_Number_II.cpp
+++ random/137_Single_Number_II.cpp
@@ -31,3 +31,25 @@ public:
         return b;
     }
 };
+
+// 2022.08.18
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> temp(32, 0);
+        for(auto& num : nums) {
+            for(int i = 31; i >= 0; i--) {
+                if(num & (1 << i)) {
+                    temp[i] = (temp[i] + 1) % 3;
+                }
+            }
+        }
+        int res = 0;
+        for(int i = 0; i <= 31; i++) {
+            if(temp[i]) {
+                res |= 1 << i;
+            }
+        }
+        return res;
+    }
+};
