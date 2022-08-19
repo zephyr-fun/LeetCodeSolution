b5568259a91b36351dc8e249a0cebbad9c9f2f5e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-19 08:41:21 +0800

    Create 1450_Number_of_Students_Doing_Homework_at_a_Given_Time.cpp

---
+++ array/1450_Number_of_Students_Doing_Homework_at_a_Given_Time.cpp
@@ -0,0 +1,14 @@
+// 2022.08.19
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int res = 0;
+        int n = startTime.size();
+        for(int i = 0; i < n; i++) {
+            if(startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
