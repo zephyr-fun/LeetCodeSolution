41ff47ddc0a00ab797c7a19bf776795595ecfeff
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-19 08:50:08 +0800

    Update 1450_Number_of_Students_Doing_Homework_at_a_Given_Time.cpp

--- array/1450_Number_of_Students_Doing_Homework_at_a_Given_Time.cpp
+++ array/1450_Number_of_Students_Doing_Homework_at_a_Given_Time.cpp
@@ -12,3 +12,25 @@ public:
         return res;
     }
 };
+
+// diff array
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        int maxEnd = *max_element(endTime.begin(), endTime.end());
+        if(queryTime > maxEnd) {
+            return 0;
+        }
+        vector<int> diff(maxEnd + 2, 0);
+        for(int i = 0; i < n; i++) {
+            diff[startTime[i]]++;
+            diff[endTime[i] + 1]--;
+        }
+        int res = 0;
+        for(int i = 0; i <= queryTime; i++) {
+            res += diff[i];
+        }
+        return res;
+    }
+};
