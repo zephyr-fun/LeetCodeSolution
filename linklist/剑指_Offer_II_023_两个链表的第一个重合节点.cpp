20cf81fd2a6e3bbaa4ccc48ac7baa3249b7d4058
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-19 09:23:57 +0800

    Create 剑指_Offer_II_023_两个链表的第一个重合节点.cpp

---
+++ linklist/剑指_Offer_II_023_两个链表的第一个重合节点.cpp
@@ -0,0 +1,44 @@
+// 2022.08.19
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int len1 = 0;
+        int len2 = 0;
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        while(curA != nullptr) {
+            len1++;
+            curA = curA->next;
+        }
+        while(curB != nullptr) {
+            len2++;
+            curB = curB->next;
+        }
+        if(len2 > len1) {
+            return getIntersectionNode(headB, headA);
+        }
+        int diff = len1 - len2;
+        curA = headA;
+        curB = headB;
+        while(diff) {
+            curA = curA->next;
+            diff--;
+        }
+        while(curA != nullptr && curB != nullptr) {
+            if(curA == curB) {
+                return curA;
+            }
+            curA = curA->next;
+            curB = curB->next;
+        }
+        return nullptr;
+    }
+};
