30350dc3bd522109f43c2290a7ba4abfd1d1dd7d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-23 09:50:13 +0800

    Create 剑指_Offer_II_008_和大于等于target的最短子数组.cpp

---
+++ binary_index_tree/剑指_Offer_II_008_和大于等于target的最短子数组.cpp
@@ -0,0 +1,33 @@
+// 2022.08.23
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        vector<int> presum(n + 1);
+        presum[0] = 0;
+        int res = 0x3f3f3f3f;
+        for(int i = 0; i < n; i++) {
+            presum[i + 1] = presum[i] + nums[i];
+        }
+        for(int i = 1; i <= n; i++) {
+            int temp = presum[i] - target;
+            int left = 0;
+            int right = i - 1;
+            int pos = -1;
+            while(left <= right) {
+                int mid = (left + right) >> 1;
+                if(presum[mid] <= temp) {
+                    pos = mid;
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            if(pos != -1) {
+                res = min(res, i - pos);
+            }
+        }
+        return res == 0x3f3f3f3f ? 0 : res;
+    }
+};
