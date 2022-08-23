508006e68f5fd7701094c75ece0efe453fb50624
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-23 09:50:11 +0800

    Create 209_Minimum_Size_Subarray_Sum.cpp

---
+++ binary_index_tree/209_Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,160 @@
+// 2022.03.25
+// Sliding Window
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum = 0;
+        int res = INT_MAX;
+        int i = 0;
+        int subLength = 0;
+        for(int j = 0; j < nums.size(); j++){
+            sum += nums[j];
+            while(sum >= target){
+                subLength = j - i + 1;
+                res = res < subLength ? res : subLength;
+                sum -= nums[i];
+                i++;
+            }
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
+
+// 2022.04.16
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int res = INT_MAX;
+        while(right < nums.size()){
+            sum += nums[right];
+            while(sum >= target){
+                res = res < right - left + 1 ? res : right - left + 1;
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
+
+// 2022.06.08
+// sliding window
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        int n = nums.size();
+        int sum = 0;
+        int res = 0x3f3f3f3f;
+        while(right < n) {
+            sum += nums[right];
+            right++;
+            while(sum - nums[left] >= target) {
+                sum -= nums[left];
+                left++;
+            }
+            if(sum >= target) {
+                res = min(res, right - left);
+            }
+            
+        }
+        return res == 0x3f3f3f3f ? 0 : res;
+    }
+};
+
+// O(nlogn) binary search + prefixSum
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int res = 0x3f3f3f3f;
+        int n = nums.size();
+        vector<int> prefix(n + 1, 0);
+        for(int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+        for(int i = 0; i < n; i++) {
+            int prefixSum = prefix[i + 1];
+            if(prefixSum < target) {
+                continue;
+            }
+            int left = 0;
+            int right = i;
+            int pos = -1;
+            while(left <= right) {
+                int mid = left + (right - left) / 2;
+                if(prefixSum - prefix[mid] >= target) {
+                    pos = mid;
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            res = min(res, i - pos + 1);
+        }
+        return res == 0x3f3f3f3f ? 0 : res;
+    }
+};
+
+// 2022.06.24
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int len = 0x3f3f3f3f;
+        int left = 0;
+        int right = 0;
+        int cur = 0;
+        while(right < n) {
+            cur += nums[right];
+            right++;
+            while(cur - nums[left] >= target) {
+                cur -= nums[left];
+                left++;
+            }
+            if(cur >= target) {
+                len = min(len, right - left);
+            }
+        }
+        return len == 0x3f3f3f3f ? 0 : len;
+    }
+};
+
+// prefixSum + binary search
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int res = 0x3f3f3f3f;
+        vector<int> sum(n + 1, 0);
+        for(int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        for(int i = 0; i < n; i++) {
+            int preSum = sum[i + 1];
+            if(preSum < target) {
+                continue;
+            }
+            int left = 0;
+            int right = i;
+            int pos = -1;
+            while(left <= right) {
+                int mid = left + (right - left) / 2;
+                if(preSum - sum[mid] >= target) {
+                    pos = mid;
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            res = min(res, i + 1 - pos);
+        }
+        return res == 0x3f3f3f3f ? 0 : res;
+    }
+};
