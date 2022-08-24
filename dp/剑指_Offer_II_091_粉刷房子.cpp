1622e39984437b0e9d6fc07d73dc166137c729ec
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-24 10:25:39 +0800

    Update 剑指_Offer_II_091_粉刷房子.cpp

--- dp/剑指_Offer_II_091_粉刷房子.cpp
+++ dp/剑指_Offer_II_091_粉刷房子.cpp
@@ -61,3 +61,21 @@ public:
         return res;
     }
 };
+
+// 2022.08.24
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        vector<vector<int>> dp(n, vector<int>(3, 0x3f3f3f3f));
+        for(int i = 0; i < 3; i++) {
+            dp[0][i] = costs[0][i];
+        }
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+        }
+        return min(dp[n - 1][0], min(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
