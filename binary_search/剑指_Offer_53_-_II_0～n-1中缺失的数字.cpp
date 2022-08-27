9bf0541ad2e1a50232aeeac8953de324a9d4b42c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-27 10:01:16 +0800

    Update 剑指_Offer_53_-_II_0～n-1中缺失的数字.cpp

--- binary_search/剑指_Offer_53_-_II_0～n-1中缺失的数字.cpp
+++ binary_search/剑指_Offer_53_-_II_0～n-1中缺失的数字.cpp
@@ -19,3 +19,51 @@ public:
         return pos + 1;
     }
 };
+
+// 2022.08.27
+// inplace hash
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; ) {
+            if(nums[i] >= 0 && nums[i] < n && nums[i] != nums[nums[i]]) {
+                int idx = nums[i];
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+};
+
+// binary search
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        int pos = -1;
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] == mid) {
+                left = mid + 1;
+            }
+            else {
+                pos = mid;
+                right = mid - 1;
+            }
+        }
+        return pos == -1 ? n : pos;
+    }
+};
