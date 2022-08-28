251587682405047319cbfc94f0661b11992379cd
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-28 08:39:31 +0800

    Create 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp

---
+++ binary_search/154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,24 @@
+// 2022.08.28
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        int pos = 0;
+        while(right >= 0 && nums[right] == nums[0]) {
+            right--;
+        }
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] < nums[0]) {
+                pos = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return nums[pos];
+    }
+};
