a204759ea5930e5a7693444da0d4bddeb7ab2180
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-29 10:38:19 +0800

    Update 剑指_Offer_II_005单词长度的最大乘积.cpp

--- hash/剑指_Offer_II_005单词长度的最大乘积.cpp
+++ hash/剑指_Offer_II_005单词长度的最大乘积.cpp
@@ -23,3 +23,33 @@ public:
         return res;
     }
 };
+
+// 2022.08.29
+// hash
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        unordered_map<int, int> map;
+        for(auto& word : words) {
+            int temp = 0;
+            for(auto c : word) {
+                temp |= 1 << (c - 'a');
+            }
+            if(!map.count(temp)) {
+                map[temp] = word.size();
+            }
+            else {
+                map[temp] = max(map[temp], (int)word.size());
+            }
+        }
+        int res = 0;
+        for(auto [ak, av] : map) {
+            for(auto [bk, bv] : map) {
+                if((ak & bk) == 0) {
+                    res = max(res, av * bv);
+                }
+            }
+        }
+        return res;
+    }
+};
