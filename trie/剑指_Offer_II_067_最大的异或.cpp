0cb5d3ad1134c1ed31530c81855c9667bc34c312
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-29 10:18:00 +0800

    Update 剑指_Offer_II_067_最大的异或.cpp

--- trie/剑指_Offer_II_067_最大的异或.cpp
+++ trie/剑指_Offer_II_067_最大的异或.cpp
@@ -42,3 +42,48 @@ public:
         return res;
     }
 };
+
+// 2022.08.29
+class Solution {
+public:
+    vector<vector<int>> son;
+    void insert(int num) {
+        int p = 0;
+        for(int i = 30; i >= 0; i--) {
+            int u = (num >> i) & 1;
+            if(!son[p][u]) {
+                son[p][u] = son.size();
+                son.push_back({0, 0});
+            }
+            p = son[p][u];
+        }
+    }
+
+    int query(int num) {
+        int res = 0;
+        int p = 0;
+        for(int i = 30; i >= 0; i--) {
+            int u = (num >> i) & 1;
+            if(son[p][!u]) {
+                res = res * 2 + !u;
+                p = son[p][!u];
+            }
+            else {
+                res = res * 2 + u;
+                p = son[p][u];
+            }
+        }
+        return res ^ num;
+    }
+
+    int findMaximumXOR(vector<int>& nums) {
+        int n = nums.size();
+        son.push_back({0, 0});
+        int res = 0;
+        for(auto& num : nums) {
+            res = max(res, query(num));
+            insert(num);
+        }
+        return res;
+    }
+};
