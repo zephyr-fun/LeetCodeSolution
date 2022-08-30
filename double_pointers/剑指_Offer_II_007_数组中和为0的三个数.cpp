061d2e60f4c50b9cab984fded894cd3076e45954
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-30 11:11:36 +0800

    Update 剑指_Offer_II_007_数组中和为0的三个数.cpp

--- double_pointers/剑指_Offer_II_007_数组中和为0的三个数.cpp
+++ double_pointers/剑指_Offer_II_007_数组中和为0的三个数.cpp
@@ -44,3 +44,47 @@ public:
         return res;
     }
 };
+
+// 2022.08.30
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int temp = 0 - nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                if(nums[left] + nums[right] > temp) {
+                    right--;
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if(nums[left] + nums[right] < temp) {
+                    left++;
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                }
+                else {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
