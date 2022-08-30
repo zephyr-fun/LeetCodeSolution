a7314c5191c0321a2de40578420f77db73b45377
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-30 11:00:34 +0800

    Update 剑指_Offer_II_008_和大于等于target的最短子数组.cpp

--- binary_index_tree/剑指_Offer_II_008_和大于等于target的最短子数组.cpp
+++ binary_index_tree/剑指_Offer_II_008_和大于等于target的最短子数组.cpp
@@ -31,3 +31,26 @@ public:
         return res == 0x3f3f3f3f ? 0 : res;
     }
 };
+
+// 2022.08.30
+// double pointers
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        int cur = 0;
+        int res = 0x3f3f3f3f;
+        while(right < n) {
+            cur += nums[right];
+            right++;
+            while(left <= right && cur >= target) {
+                res = min(res, right - left);
+                cur -= nums[left];
+                left++;
+            }
+        }
+        return res == 0x3f3f3f3f ? 0 : res;
+    }
+};
