0012990cbb6b06782546eb1641a5ee7c84ce3250
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-08-31 10:09:35 +0800

    Update 剑指_Offer_41_数据流中的中位数.cpp

--- stack&queue/剑指_Offer_41_数据流中的中位数.cpp
+++ stack&queue/剑指_Offer_41_数据流中的中位数.cpp
@@ -52,3 +52,56 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+// 2022.08.31
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    priority_queue<double, vector<double>, less<double>> left_half;
+    priority_queue<double, vector<double>, greater<double>> right_half;
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+        if(left_half.size() == right_half.size()) {
+            if(left_half.size() == 0) { // note nullptr
+                left_half.push(num);
+            }
+            else {
+                if(num > right_half.top()) {
+                    left_half.push(right_half.top());
+                    right_half.pop();
+                    right_half.push(num);
+                }
+                else {
+                    left_half.push(num);
+                }
+            }
+        }
+        else {
+            if(num < left_half.top()) {
+                right_half.push(left_half.top());
+                left_half.pop();
+                left_half.push(num);
+            }
+            else {
+                right_half.push(num);
+            }
+        }
+    }
+    
+    double findMedian() {
+        if(left_half.size() == right_half.size()) {
+            return (left_half.top() + right_half.top()) / 2;
+        }
+        return left_half.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
