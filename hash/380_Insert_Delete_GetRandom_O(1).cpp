6ebe19c045b39d6de7a61308669cd19c09681201
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-02 18:00:16 +0800

    Update 380_Insert_Delete_GetRandom_O(1).cpp

--- hash/380_Insert_Delete_GetRandom_O(1).cpp
+++ hash/380_Insert_Delete_GetRandom_O(1).cpp
@@ -46,3 +46,44 @@ public:
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
+
+// 2022.09.02
+static int nums[200007];
+class RandomizedSet {
+    mt19937 gen;
+    unordered_map<int, int> map;
+    int idx;
+public:
+    RandomizedSet() : gen(random_device{}()), idx(0) {
+
+    }
+    
+    bool insert(int val) {
+        if(map.count(val)) {
+            return false;
+        }
+        nums[idx] = val;
+        map[val] = idx;
+        idx++;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!map.count(val)) {
+            return false;
+        }
+        int temp = map[val];
+        map.erase(val);
+        if(temp != idx - 1) {
+            nums[temp] = nums[idx - 1];
+            map[nums[idx - 1]] = temp;
+        }
+        idx--;
+        return true;
+    }
+    
+    int getRandom() {
+        uniform_int_distribution<> dist(0, idx - 1);
+        return nums[dist(gen)];
+    }
+};
