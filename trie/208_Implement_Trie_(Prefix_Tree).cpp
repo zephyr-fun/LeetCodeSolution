b8ecb67bcafbba61b2e2741d2cabcbac4ab860d4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-04 10:14:54 +0800

    Update 208_Implement_Trie_(Prefix_Tree).cpp

--- trie/208_Implement_Trie_(Prefix_Tree).cpp
+++ trie/208_Implement_Trie_(Prefix_Tree).cpp
@@ -52,3 +52,59 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+// 2022.09.04
+class Trie {
+public:
+    vector<vector<int>> son;
+    vector<int> cnt;
+    int idx;
+    Trie(): son(1e5 + 7, vector<int>(26, 0)), cnt(1e5 + 7, 0), idx(0) {
+
+    }
+    
+    void insert(string word) {
+        int p = 0;
+        for(auto s : word) {
+            int u = s - 'a';
+            if(!son[p][u]) {
+                idx++;
+                son[p][u] = idx;
+            }
+            p = son[p][u];
+        }
+        cnt[p]++;
+    }
+    
+    bool search(string word) {
+        int p = 0;
+        for(auto s : word) {
+            int u = s - 'a';
+            if(!son[p][u]) {
+                return false;
+            }
+            p = son[p][u];
+        }
+        return cnt[p];
+    }
+    
+    bool startsWith(string prefix) {
+        int p = 0;
+        for(auto s : prefix) {
+            int u = s - 'a';
+            if(!son[p][u]) {
+                return false;
+            }
+            p = son[p][u];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
