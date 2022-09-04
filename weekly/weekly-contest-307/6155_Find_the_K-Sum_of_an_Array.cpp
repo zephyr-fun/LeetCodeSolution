3e5d31d9d249419d73b140cc5812826fa2dba3ce
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-05 00:14:49 +0800

    Update 6155_Find_the_K-Sum_of_an_Array.cpp

--- weekly/weekly-contest-307/6155_Find_the_K-Sum_of_an_Array.cpp
+++ weekly/weekly-contest-307/6155_Find_the_K-Sum_of_an_Array.cpp
@@ -1,1 +1,29 @@
 // 2022.08.29 sorted
+class Solution {
+public:
+    long long kSum(vector<int>& nums, int k) {
+        long sum = 0L;
+        for(int& x : nums) {
+            if(x >= 0) {
+                sum += x;
+            }
+            else {
+               x = -x; 
+            }
+        }
+        sort(nums.begin(), nums.end());
+        priority_queue<pair<long, int>> pq;
+        pq.emplace(sum, 0);
+        while(--k) {
+            auto[sum, i] = pq.top();
+            pq.pop();
+            if(i < nums.size()) {
+                pq.emplace(sum - nums[i], i + 1); // 保留 nums[i-1]
+                if(i) {
+                    pq.emplace(sum - nums[i] + nums[i - 1], i + 1); // 不保留 nums[i-1]，把之前减去的加回来
+                }
+            }
+        }
+        return pq.top().first;
+    }
+};
