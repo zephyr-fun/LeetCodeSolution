ce41c44751571c3eee5fe7868b18cc3def78137e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-04 10:02:43 +0800

    Update 322_Coin_Change.cpp

--- dp/322_Coin_Change.cpp
+++ dp/322_Coin_Change.cpp
@@ -51,3 +51,42 @@ public:
         return dp[amount] == 0x3f3f3f3f ? -1 : dp[amount];
     }
 };
+
+// 2022.09.04
+// 配合yxc 推导
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp(amount + 1, 0x3f3f3f3f);
+        dp[0] = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(j >= coins[i]) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount] == 0x3f3f3f3f ? -1 : dp[amount];
+    }
+};
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0x3f3f3f3f));
+        // for(int i = 0; i <= n; i++) {
+        //     dp[i][0] = 0;
+        // }
+        dp[0][0] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(j >= coins[i - 1]) {
+                    dp[i][j] = min(dp[i][j], dp[i][j - coins[i - 1]] + 1);
+                }
+            }
+        }
+        return dp[n][amount] == 0x3f3f3f3f ? -1 : dp[n][amount];
+    }
+};
