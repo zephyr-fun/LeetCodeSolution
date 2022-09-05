ad0c7fc86dd00a12e2da55162e72d5aead252df8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-05 09:35:51 +0800

    Update 295_Find_Median_from_Data_Stream.cpp

--- stack&queue/295_Find_Median_from_Data_Stream.cpp
+++ stack&queue/295_Find_Median_from_Data_Stream.cpp
@@ -52,3 +52,55 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+// 2022.09.05
+class MedianFinder {
+public:
+    priority_queue<double, vector<double>, less<int>> left;
+    priority_queue<double, vector<double>, greater<int>> right;
+    MedianFinder() {
+
+    }
+    
+    void addNum(int num) {
+        if(left.size() == right.size()) {
+            if(left.size() == 0) {
+                left.push(num);
+            }
+            else {
+                if(num > right.top()) {
+                    left.push(right.top());
+                    right.pop();
+                    right.push(num);
+                }
+                else {
+                    left.push(num);
+                }
+            }
+        }
+        else {
+            if(num < left.top()) {
+                right.push(left.top());
+                left.pop();
+                left.push(num);
+            }
+            else {
+                right.push(num);
+            }
+        }
+    }
+    
+    double findMedian() {
+        if(left.size() == right.size()) {
+            return (left.top() + right.top()) / 2;
+        }
+        return left.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
