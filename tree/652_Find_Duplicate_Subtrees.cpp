41e3c2242eb49f8eecfaa25dba19d19b9cedb9bd
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-05 09:28:15 +0800

    Update 652_Find_Duplicate_Subtrees.cpp

--- tree/652_Find_Duplicate_Subtrees.cpp
+++ tree/652_Find_Duplicate_Subtrees.cpp
@@ -46,3 +46,40 @@ public:
         return cur;
     }
 };
+
+// 2022.09.05
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int p = 131;
+    int q = 13131;
+    unordered_map<string, int> map;
+    vector<TreeNode*> res;
+    string traversal(TreeNode* cur) {
+        if(cur == nullptr) {
+            return "#";
+        }
+        string left = traversal(cur->left);
+        string right = traversal(cur->right);
+        string temp = left + "," + right + "," + to_string(cur->val);
+        map[temp]++;
+        if(map[temp] == 2) {
+            res.emplace_back(cur);
+        }
+        return temp;
+    }
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        traversal(root);
+        return res;
+    }
+};
