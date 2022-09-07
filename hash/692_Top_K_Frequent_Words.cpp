709f79a990d01662dd63c4e3c9b55f54c8fcd882
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-07 09:44:16 +0800

    Create 692_Top_K_Frequent_Words.cpp

---
+++ hash/692_Top_K_Frequent_Words.cpp
@@ -0,0 +1,23 @@
+// 2022.09.07
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> map;
+        int n = words.size();
+        for(int i = 0; i < n; i++) {
+            map[words[i]]++;
+        }
+        vector<pair<string, int>> temp(map.begin(), map.end());
+        sort(temp.begin(), temp.end(), [](pair<string, int>& a, pair<string, int>& b) {
+            if(a.second == b.second) {
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        });
+        vector<string> res;
+        for(int i = 0; i < k; i++) {
+            res.emplace_back(temp[i].first);
+        }
+        return res;
+    }
+};
