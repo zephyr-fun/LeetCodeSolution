28d7fc3f0baf1e335c7ddafec4761ff3e6dc3718
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-10 09:04:05 +0800

    Update 347_Top_K_Frequent_Elements.cpp

--- stack&queue/347_Top_K_Frequent_Elements.cpp
+++ stack&queue/347_Top_K_Frequent_Elements.cpp
@@ -56,3 +56,34 @@ public:
         return res;
     }
 };
+
+// 2022.09.10
+class Solution {
+public:
+    class MyComprarsion {
+    public:
+        bool operator() (const pair<int, int>& a, const pair<int, int>& b) {
+            return a.second > b.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> map;
+        for(int i = 0; i < n; i++) {
+            map[nums[i]]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, MyComprarsion> pq;
+        for(unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++) {
+            pq.push(*it);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> res(k);
+        for(int i = k - 1; i >= 0; i--) {
+            res[i] = pq.top().first;
+            pq.pop();
+        }
+        return res;
+    }
+};
