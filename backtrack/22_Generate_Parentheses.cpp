607138db956bee98d5d1f324ab6927cc18d9cad8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-13 09:17:24 +0800

    Update 22_Generate_Parentheses.cpp

--- backtrack/22_Generate_Parentheses.cpp
+++ backtrack/22_Generate_Parentheses.cpp
@@ -24,4 +24,35 @@ public:
     }
 };
 
-// dp
+// 2022.09.13
+class Solution {
+public:
+    vector<string> res;
+    string path;
+    int total;
+    int n;
+    void dfs(int cur) {
+        if(path.size() == total) {
+            if(cur == 0) {
+                res.emplace_back(path);
+            }
+            return ;
+        }
+        if(cur < n) {
+            path += '(';
+            dfs(cur + 1);
+            path.pop_back();
+        }
+        if(cur >= 1) {
+            path += ')';
+            dfs(cur - 1);
+            path.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n_) {
+        n = n_;
+        total = 2 * n;
+        dfs(0);
+        return res;
+    }
+};
