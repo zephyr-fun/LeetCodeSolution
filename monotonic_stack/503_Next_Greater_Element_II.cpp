7236e6ffc2d098ed5e8d465c0af385213550936b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-14 21:06:25 +0800

    Update 503_Next_Greater_Element_II.cpp

--- monotonic_stack/503_Next_Greater_Element_II.cpp
+++ monotonic_stack/503_Next_Greater_Element_II.cpp
@@ -107,3 +107,21 @@ public:
         return res;
     }
 };
+
+// 2022.09.14
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, -1);
+        stack<int> st;
+        for(int i = 0; i < 2 * n - 1; i++) {
+            while(!st.empty() && nums[st.top()] < nums[i % n]) {
+                res[st.top()] = nums[i % n];
+                st.pop();
+            }
+            st.push(i % n);
+        }
+        return res;
+    }
+};
