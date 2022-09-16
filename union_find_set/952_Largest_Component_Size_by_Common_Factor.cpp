fdfb656b412a1d9916e7bc60f3ec65f428b9e9a0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-16 09:49:30 +0800

    Update 952_Largest_Component_Size_by_Common_Factor.cpp

--- union_find_set/952_Largest_Component_Size_by_Common_Factor.cpp
+++ union_find_set/952_Largest_Component_Size_by_Common_Factor.cpp
@@ -75,3 +75,79 @@ public:
         return res;
     }
 };
+
+// 2022.09.16
+class UnionFindSet {
+public:
+    UnionFindSet(int n, vector<int>& nums): parent(n + 1), rank(n + 1, 0), size(n + 1, 0) {
+        for(int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+        for(auto& num : nums) {
+            size[num] = 1;
+        }
+    }
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+            size[pu] += size[pv];
+        }
+        return true;
+    }
+
+    int getSize(int u) {
+        int pu = Find(u);
+        return size[pu];
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+};
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    int largestComponentSize(vector<int>& nums) {
+        int maxNum = -0x3f3f3f3f;
+        for(auto& num : nums) {
+            maxNum = max(maxNum, num);
+        }
+        UnionFindSet set(maxNum, nums);
+        int res = 0;
+        for(auto& num : nums) {
+            for(int i = sqrt(num); i >= 2; i--) {
+                if(num % i == 0) {
+                    set.Union(num, i);
+                    set.Union(num, num / i);
+                }
+            }
+            res = max(res, set.getSize(num));
+        }
+        return res;
+    }
+};
