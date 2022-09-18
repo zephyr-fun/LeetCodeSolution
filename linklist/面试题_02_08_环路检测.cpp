dac6a5d04dffdddc16b30bf9b19bb973b3983e7a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-18 20:01:54 +0800

    Create 面试题_02_08_环路检测.cpp

---
+++ linklist/面试题_02_08_环路检测.cpp
@@ -0,0 +1,30 @@
+// 2022.09.18
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) {
+                ListNode* index1 = slow;
+                ListNode* index2 = head;
+                while(index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
+    }
+};
