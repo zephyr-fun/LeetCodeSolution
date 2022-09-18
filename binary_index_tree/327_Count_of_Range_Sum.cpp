291dd175297ca1a78b34e62bb7d6111386652811
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-18 10:20:50 +0800

    Update 327_Count_of_Range_Sum.cpp

--- binary_index_tree/327_Count_of_Range_Sum.cpp
+++ binary_index_tree/327_Count_of_Range_Sum.cpp
@@ -116,3 +116,64 @@ public:
         return res;
     }
 };
+
+// 2022.09.18
+typedef long long LL;
+class FenwickTree {
+public:
+    FenwickTree(int n) : sums_(n + 1, 0) {}
+
+    void update(int idx, int delta) {
+        while(idx < sums_.size()) {
+            sums_[idx] += delta;
+            idx += lowbit(idx);
+        }
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        while(idx > 0) {
+            sum += sums_[idx];
+            idx -= lowbit(idx);
+        }
+        return sum;
+    }
+
+private:
+    vector<int> sums_;
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+};
+class Solution {
+public:
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        set<LL> allNum;
+        vector<LL> preSum(n + 1, 0); // preSum[0] = 0, so that we can get the sum of (0 -> i-1)
+        for(int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+        for(auto& x : preSum) {
+            allNum.insert(x);
+            allNum.insert(x - lower);
+            allNum.insert(x - upper);
+        }
+        int m = allNum.size();
+        FenwickTree tree(m);
+        int idx = 0;
+        unordered_map<LL, int> map;
+        for(auto& x : allNum) {
+            map[x] = idx;
+            idx++;
+        }
+        int res = 0;
+        for(auto& x : preSum) {
+            int left = map[x - upper];
+            int right = map[x - lower];
+            res += tree.query(right + 1) - tree.query(left);
+            tree.update(map[x] + 1, 1);
+        }
+        return res;
+    }
+};
