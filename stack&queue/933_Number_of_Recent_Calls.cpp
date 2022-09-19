2fd1d0cc4dc1c44f0500bac4f1985072fc722706
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-19 09:01:31 +0800

    Update 933_Number_of_Recent_Calls.cpp

--- stack&queue/933_Number_of_Recent_Calls.cpp
+++ stack&queue/933_Number_of_Recent_Calls.cpp
@@ -20,3 +20,26 @@ public:
  * RecentCounter* obj = new RecentCounter();
  * int param_1 = obj->ping(t);
  */
+
+// 2022.09.19
+class RecentCounter {
+public:
+    deque<int> dq;
+    RecentCounter() {
+
+    }
+    
+    int ping(int t) {
+        while(!dq.empty() && t - dq.front() > 3000) {
+            dq.pop_front();
+        }
+        dq.push_back(t);
+        return dq.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
