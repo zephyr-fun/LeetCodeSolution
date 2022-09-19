5d1a3d606624006c77b1a6a247364bc403478169
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-19 08:58:13 +0800

    Update 1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp

--- binary_index_tree/1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp
+++ binary_index_tree/1893_Check_if_All_the_Integers_in_a_Range_Are_Covered.cpp
@@ -231,3 +231,24 @@ public:
         return true;
     }
 };
+
+// 2022.09.19
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        vector<int> diff(57, 0);
+        for(auto& range : ranges) {
+            diff[range[0]]++;
+            diff[range[1] + 1]--;
+        }
+        for(int i = 1; i < 57; i++) {
+            diff[i] = diff[i - 1] + diff[i];
+        }
+        for(int i = left; i <= right; i++) {
+            if(diff[i] < 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
