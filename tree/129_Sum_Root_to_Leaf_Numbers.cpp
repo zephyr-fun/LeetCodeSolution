2cb78fee2aa934be580ef0b16894beb2505d5b2c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-19 08:45:41 +0800

    Update 129_Sum_Root_to_Leaf_Numbers.cpp

--- tree/129_Sum_Root_to_Leaf_Numbers.cpp
+++ tree/129_Sum_Root_to_Leaf_Numbers.cpp
@@ -70,3 +70,37 @@ int sumNumbers2(TreeNode* root) {
     }
     return res;
 }
+
+// 2022.09.19
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    void traversal(TreeNode* root, int cur) {
+        cur = cur * 10 + root->val;
+        if(root->left == nullptr && root->right == nullptr) {
+            res += cur;
+            return ;
+        }
+        if(root->left != nullptr) {
+            traversal(root->left, cur);
+        }
+        if(root->right != nullptr) {
+            traversal(root->right, cur);
+        }
+    }
+    int sumNumbers(TreeNode* root) {
+        traversal(root, 0);
+        return res;
+    }
+};
