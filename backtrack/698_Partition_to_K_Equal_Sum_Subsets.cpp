a0b8e7b2669cf87c72f4ced098e2991fb5f24531
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-20 10:18:05 +0800

    Create 698_Partition_to_K_Equal_Sum_Subsets.cpp

---
+++ backtrack/698_Partition_to_K_Equal_Sum_Subsets.cpp
@@ -0,0 +1,44 @@
+// 2022.09.20
+class Solution {
+public:
+    int target;
+    vector<int> nums;
+    vector<bool> vis;
+    bool dfs(int start, int cnt, int k) {
+        if(k == 0) {
+            return true;
+        }
+        if(cnt == target) {
+            return dfs(0, 0, k - 1);
+        }
+        for(int i = start; i < nums.size(); i++) {
+            if(vis[i]) {
+                continue;
+            }
+            if(cnt + nums[i] <= target) {
+                vis[i] = true;
+                if(dfs(i + 1, cnt + nums[i], k)) {
+                    return true;
+                }
+                vis[i] = false;
+            }
+            while(i + 1 < nums.size() && nums[i + 1] == nums[i]) {
+                i++;
+            }
+            if(!cnt || cnt + nums[i] == target) {
+                return false;
+            }
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums_, int k) {
+        nums = nums_;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum % k) {
+            return false;
+        }
+        vis.resize(nums.size());
+        target = sum / k;
+        return dfs(0, 0, k);
+    }
+};
