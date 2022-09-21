15eab888ad391f97849e916e16cee2548b404fcb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-21 08:46:03 +0800

    Update 62_Unique_Paths.cpp

--- dp/62_Unique_Paths.cpp
+++ dp/62_Unique_Paths.cpp
@@ -56,3 +56,23 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// 2022.09.21
+class Solution {
+public:
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < m; j++) {
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
