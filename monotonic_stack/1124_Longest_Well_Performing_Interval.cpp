d189ee797a90350ef9b2fde710a8e968f28950f1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-25 21:30:41 +0800

    Update 1124_Longest_Well_Performing_Interval.cpp

--- monotonic_stack/1124_Longest_Well_Performing_Interval.cpp
+++ monotonic_stack/1124_Longest_Well_Performing_Interval.cpp
@@ -34,3 +34,47 @@ public:
 };
 
 // monotonic stack
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        // 处理数组为每天的得分
+        int n = hours.size();
+        for(int i = 0; i < n; i++) {
+            if(hours[i] > 8) {
+                hours[i] = 1;
+            }
+            else {
+               hours[i] = -1;
+            }
+        }
+        // 求前缀和
+        int pre[n + 1];
+        memset(pre, 0, sizeof(pre));
+        for(int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + hours[i - 1];
+        }
+        // 单调栈记录前缀和的递减下标（★也就是首次最低值下标）
+        stack<int> st;
+        // while(s.size()) s.pop();
+        for(int i = 0; i <= n; i++) {
+            if(st.empty() || pre[st.top()] > pre[i]) {
+                st.push(i);
+            }
+        }
+        int res = 0;
+        // 枚举右边界
+        for(int i = n; i >= 0; i--) {
+            if(st.empty()) {
+                break;
+            }
+            while(!st.empty() && st.top() >= i) {
+                st.pop();
+            }
+            while(!st.empty() && pre[st.top()] < pre[i]) {
+                res = max(res, i - st.top());
+                st.pop();
+            }
+        }
+        return res;
+    }
+};
