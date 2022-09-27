b82a70178d8d2f52e863e9d3cc2919d550583b4c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-27 10:06:19 +0800

    Update 846_Hand_of_Straights.cpp

--- hash/846_Hand_of_Straights.cpp
+++ hash/846_Hand_of_Straights.cpp
@@ -38,3 +38,44 @@ public:
         return true;
     }
 };
+
+// 2022.09.27
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) {
+            return false;
+        }
+        unordered_map<int, int> map;
+        for(auto& h : hand) {
+            map[h]++;
+        }
+        vector<int> temp;
+        for(auto& [k, v] : map) {
+            temp.emplace_back(k);
+        }
+        sort(temp.begin(), temp.end());
+        for(auto& t : temp) {
+            if(map[t] == 0) {
+                continue;
+            }
+            int cnt = map[t];
+            for(int i = t; i < t + groupSize; i++) {
+                if(!map.count(i)) {
+                    return false;
+                }
+                map[i] -= cnt;
+                if(map[i] < 0) {
+                    return false;
+                }
+            }
+        }
+        for(auto& [k, v] : map) {
+            if(v != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
