8e1e47efd8ce242f6e7e746c90236ab86c731dab
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-09-30 08:47:08 +0800

    Update 199_Binary_Tree_Right_side_view.cpp

--- tree/199_Binary_Tree_Right_side_view.cpp
+++ tree/199_Binary_Tree_Right_side_view.cpp
@@ -142,3 +142,44 @@ public:
         return res;
     }
 };
+
+// 2022.09.30
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if(root == nullptr) {
+            return res;
+        }
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            int size = que.size();
+            int temp = 0;
+            for(int i = 0; i < size; i++) {
+                TreeNode* cur = que.front();
+                que.pop();
+                temp = cur->val;
+                if(cur->left) {
+                    que.push(cur->left);
+                }
+                if(cur->right) {
+                    que.push(cur->right);
+                }
+            }
+            res.emplace_back(temp);
+        }
+        return res;
+    }
+};
