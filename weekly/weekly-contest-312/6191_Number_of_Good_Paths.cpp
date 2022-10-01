f59137c8a4b70d3523322ff077a11bfe97fc8d09
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-01 20:27:25 +0800

    Update 6191_Number_of_Good_Paths.cpp

--- weekly/weekly-contest-312/6191_Number_of_Good_Paths.cpp
+++ weekly/weekly-contest-312/6191_Number_of_Good_Paths.cpp
@@ -35,18 +35,84 @@ public:
 };
 
 // union find set
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    vector<vector<int>> time(n, vector<int>(2, 0));
-    for(int i = 0; i < n; i++) {
-        cin >> time[i][0] >> time[i][1];
+class UnionFindSet {
+public:
+    UnionFindSet(int n) : parent(n + 1), rank(n + 1, 0) {
+        for(int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
     }
-    sort()
-}
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        }
+        else if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        return true;
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+};
+class Solution {
+public:
+    int numberOfGoodPaths(vector<int>& vals, vector<vector<int>>& edges) {
+        int n = vals.size();
+        vector<vector<int>> grid(n);
+        for(auto& edge : edges) {
+            grid[edge[0]].emplace_back(edge[1]);
+            grid[edge[1]].emplace_back(edge[0]);
+        }
+        vector<int> q(n);
+        for(int i = 0; i < n; i++) {
+            q[i] = i;
+        }
+        sort(q.begin(), q.end(), [&](int& a, int& b) {
+            return vals[a] < vals[b];
+        });
+        UnionFindSet set(n);
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            int j = i + 1;
+            while(j < n && vals[q[i]] == vals[q[j]]) {
+                j++;
+            }
+            for(int k = i; k < j; k++) {
+                int u = q[k];
+                for(auto& v : grid[u]) {
+                    if(vals[u] >= vals[v]) {
+                        set.Union(u, v);
+                    }
+                }
+            }
+            unordered_map<int, int> map;
+            for(int k = i; k < j; k++) {
+                map[set.Find(q[k])]++;
+            }
+            for(auto& [k, v] : map) {
+                res += v * (v + 1) / 2;
+            }
+            i = j - 1; // cause in the for loop i will ++
+        }
+        return res;
+    }
+};
