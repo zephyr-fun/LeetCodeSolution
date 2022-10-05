c47ad0b747ae50aa06a877516b81f26b69620db7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-05 11:01:10 +0800

    Update 11_Container_With_Most_Water.cpp

--- greedy/11_Container_With_Most_Water.cpp
+++ greedy/11_Container_With_Most_Water.cpp
@@ -13,3 +13,27 @@ public:
         return res;
     }
 };
+
+// 2022.10.05
+// proof: 
+// https://leetcode.cn/problems/container-with-most-water/solution/on-shuang-zhi-zhen-jie-fa-li-jie-zheng-que-xing-tu/
+// https://leetcode.cn/problems/container-with-most-water/solution/shuang-zhi-zhen-fa-zheng-que-xing-zheng-ming-by-r3/
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int res = 0;
+        int left = 0;
+        int right = n - 1;
+        while(left < right) {
+            res = max(res, min(height[left], height[right]) * (right - left));
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return res;
+    }
+};
