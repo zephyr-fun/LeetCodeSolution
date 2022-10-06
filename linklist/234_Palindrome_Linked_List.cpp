9c10764e9096df7f6ce4ffb649b8ce0e35ccbeec
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-06 19:29:05 +0800

    Update 234_Palindrome_Linked_List.cpp

--- linklist/234_Palindrome_Linked_List.cpp
+++ linklist/234_Palindrome_Linked_List.cpp
@@ -31,3 +31,53 @@ public:
         return traversal(head);
     }
 };
+
+// half way reverse
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
+        while(head) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if(slow->next == nullptr) {
+            return true;
+        }
+        slow->next = reverseList(slow->next);
+        ListNode* temp = slow->next;
+        fast = head;
+        while(temp) {
+            if(fast->val != temp->val) {
+                return false;
+            }
+            fast = fast->next;
+            temp = temp->next;
+        }
+        return true;
+    }
+};
