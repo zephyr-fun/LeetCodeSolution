6eea30a4c0d4d8129fa39f4bdfbc45a89f164773
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-06 10:05:23 +0800

    Update 31_Next_Permutation.cpp

--- double_pointers/31_Next_Permutation.cpp
+++ double_pointers/31_Next_Permutation.cpp
@@ -17,3 +17,25 @@ public:
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
+
+// 2022.10.06
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = n - 2;
+        while(idx >= 0 && nums[idx] >= nums[idx + 1]) {
+            idx--;
+        }
+        if(idx < 0) {
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        int candi = n - 1;
+        while(nums[candi] <= nums[idx]) {
+            candi--;
+        }
+        swap(nums[idx], nums[candi]);
+        sort(nums.begin() + idx + 1, nums.end());
+    }
+};
