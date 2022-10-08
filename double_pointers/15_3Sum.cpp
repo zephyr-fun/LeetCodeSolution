9abbf740719b39be571161442a8675a61eb912e6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-08 10:00:35 +0800

    Update 15_3Sum.cpp

--- double_pointers/15_3Sum.cpp
+++ double_pointers/15_3Sum.cpp
@@ -106,3 +106,47 @@ public:
         return res;
     }
 };
+
+// 2022.10.08
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for(int i = 0; i < n - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = 0 - nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right) {
+                if(nums[left] + nums[right] > target) {
+                    right--;
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if(nums[left] + nums[right] < target) {
+                    left++;
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                }
+                else {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
