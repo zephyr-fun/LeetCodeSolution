9cbdd0104f6198e6dc16175cf1fd30bb4a2e07f9
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-09 09:56:24 +0800

    Update 238_Product_of_Array_Except_Self.cpp

--- binary_index_tree/238_Product_of_Array_Except_Self.cpp
+++ binary_index_tree/238_Product_of_Array_Except_Self.cpp
@@ -42,3 +42,24 @@ public:
         return res;
     }
 };
+
+// 2022.10.09
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        left[0] = 1;
+        right[n - 1] = 1;
+        for(int i = 1; i < n; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
+            right[n - 1 - i] = nums[n - i] * right[n - i];
+        }
+        vector<int> res(n);
+        for(int i = 0; i < n; i++) {
+            res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+};
