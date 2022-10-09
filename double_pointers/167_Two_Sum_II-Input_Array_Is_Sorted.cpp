b0c00ef8e88e11234a6b123a575432b60862619f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-09 09:31:28 +0800

    Create 167_Two_Sum_II-Input_Array_Is_Sorted.cpp

---
+++ double_pointers/167_Two_Sum_II-Input_Array_Is_Sorted.cpp
@@ -0,0 +1,21 @@
+// 2022.10.09
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int left = 0;
+        int right = n - 1;
+        while(left < right) {
+            if(numbers[left] + numbers[right] < target) {
+                left++;
+            }
+            else if(numbers[left] + numbers[right] > target) {
+                right--;
+            }
+            else {
+                return {left + 1, right + 1};
+            }
+        }
+        return {-1, -1};
+    }
+};
