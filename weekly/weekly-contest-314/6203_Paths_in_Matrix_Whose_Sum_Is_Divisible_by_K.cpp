cbe2e37c3fb44fb80bb2a52563febe3479abd73f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-09 12:53:02 +0800

    Create 6203_Paths_in_Matrix_Whose_Sum_Is_Divisible_by_K.cpp

---
+++ weekly/weekly-contest-314/6203_Paths_in_Matrix_Whose_Sum_Is_Divisible_by_K.cpp
@@ -0,0 +1,32 @@
+// 2022.10.09
+class Solution {
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int tar) {
+        int mod = 1e9 + 7;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(tar, 0)));
+        dp[0][0][grid[0][0] % tar]++;
+        for(int i = 1; i < n; i++) {
+            for(int k = 0; k < tar; k++) {
+                dp[i][0][(grid[i][0] + k) % tar] = (dp[i][0][(grid[i][0] + k) % tar] + dp[i - 1][0][k]) % mod;
+            }
+        }
+        for(int j = 1; j < m; j++) {
+            for(int k = 0; k < tar; k++) {
+                dp[0][j][(grid[0][j] + k) % tar] = (dp[0][j][(grid[0][j] + k) % tar] + dp[0][j - 1][k]) % mod;
+            }
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++) {
+                for(int k = 0; k < tar; k++) {
+                    dp[i][j][(grid[i][j] + k) % tar] = (dp[i][j][(grid[i][j] + k) % tar] + dp[i - 1][j][k]) % mod;
+                }
+                for(int k = 0; k < tar; k++) {
+                    dp[i][j][(grid[i][j] + k) % tar] = (dp[i][j][(grid[i][j] + k) % tar] + dp[i][j - 1][k]) % mod;
+                }
+            }
+        }
+        return dp[n - 1][m - 1][0];
+    }
+};
