c50eeec665964190b9d2a450defe0d8d593f5c08
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-12 10:27:03 +0800

    Update 42_Trapping_Rain_Water.cpp

--- monotonic_stack/42_Trapping_Rain_Water.cpp
+++ monotonic_stack/42_Trapping_Rain_Water.cpp
@@ -271,3 +271,26 @@ public:
         return res;
     }
 };
+
+// 2022.10.12
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        for(int i = 1; i < n; i++) {
+            left[i] = max(left[i - 1], height[i - 1]);
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            right[i] = max(right[i + 1], height[i + 1]);
+        }
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(min(left[i], right[i]) > height[i]) {
+                res += min(left[i], right[i]) - height[i];
+            }
+        }
+        return res;
+    }
+};
