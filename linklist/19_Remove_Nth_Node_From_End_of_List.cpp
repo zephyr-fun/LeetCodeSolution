d74ebdfe2ef590ade2da3d1a80dcebdb70ace710
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-18 11:26:07 +0800

    Update 19_Remove_Nth_Node_From_End_of_List.cpp

--- linklist/19_Remove_Nth_Node_From_End_of_List.cpp
+++ linklist/19_Remove_Nth_Node_From_End_of_List.cpp
@@ -118,3 +118,26 @@ public:
         return dummy->next;
     }
 };
+
+// 2022.10.18
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+        while(n >= 0) {
+            fast = fast->next;
+            n--;
+        }
+        while(fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+        return dummy->next;
+    }
+};
