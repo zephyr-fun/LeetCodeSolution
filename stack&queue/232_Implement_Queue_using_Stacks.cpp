9610f972ffb8ea559d9c52f31541d1e19e4d9150
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-19 09:01:00 +0800

    Update 232_Implement_Queue_using_Stacks.cpp

--- stack&queue/232_Implement_Queue_using_Stacks.cpp
+++ stack&queue/232_Implement_Queue_using_Stacks.cpp
@@ -123,3 +123,43 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+
+// 2022.10.19
+class MyQueue {
+public:
+    stack<int> in;
+    stack<int> out;
+    MyQueue() {
+
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        if(out.empty()) {
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        int temp = out.top();
+        out.pop();
+        return temp;
+    }
+    
+    int peek() {
+        if(out.empty()) {
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
