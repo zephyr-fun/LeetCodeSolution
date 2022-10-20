d33b6357b02292730aad27d2a6d97ca47123b3fc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-20 16:40:11 +0800

    Update 287_Find_the_Duplicate_Number.cpp

--- hash/287_Find_the_Duplicate_Number.cpp
+++ hash/287_Find_the_Duplicate_Number.cpp
@@ -33,3 +33,23 @@ public:
         return 0;
     }
 };
+
+// 2022.10.20
+// same as 142
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
