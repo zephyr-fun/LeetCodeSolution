86a51eefa09aeccc8466162748e2bdbcfa740acb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-20 16:40:13 +0800

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.cpp

--- hash/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
+++ hash/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -35,3 +35,21 @@ public:
         return res;
     }
 };
+
+// 2022.10.20
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        for(int i = 0; i < n; i++) {
+            nums[(nums[i] - 1) % n] += n;
+        }
+        for(int i = 0; i < n; i++) {
+            if(nums[i] <= n) {
+                res.emplace_back(i + 1);
+            }
+        }
+        return res;
+    }
+};
