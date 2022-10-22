3a73690ef72a36ada1c05b90e05ca7a7d5c8746a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-22 09:23:37 +0800

    Update 215_Kth_Largest_Element_in_an_Array.cpp

--- sort/215_Kth_Largest_Element_in_an_Array.cpp
+++ sort/215_Kth_Largest_Element_in_an_Array.cpp
@@ -85,3 +85,32 @@ public:
         return quick_select(nums, 0, nums.size() - 1, k);
     }
 };
+
+// 2022.10.22
+class Solution {
+public:
+    int quick_select(vector<int>& nums, int l, int r, int k) {
+        if(l == r) {
+            return nums[l];
+        }
+        int x = nums[(l + r) >> 1];
+        int i = l - 1;
+        int j = r + 1;
+        while(i < j) {
+            while(nums[++i] > x);
+            while(nums[--j] < x);
+            if(i < j) {
+                swap(nums[i], nums[j]);
+            }
+        }
+        int sl = j - l + 1;
+        if(k <= sl) {
+            return quick_select(nums, l, j, k);
+        }
+        return quick_select(nums, j + 1, r, k - sl);
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        return quick_select(nums, 0, n - 1, k);
+    }
+};
