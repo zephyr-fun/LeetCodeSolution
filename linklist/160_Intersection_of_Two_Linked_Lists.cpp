ea583be180914c1884a4fb409e2464c34af5a2d2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-22 09:15:05 +0800

    Update 160_Intersection_of_Two_Linked_Lists.cpp

--- linklist/160_Intersection_of_Two_Linked_Lists.cpp
+++ linklist/160_Intersection_of_Two_Linked_Lists.cpp
@@ -89,3 +89,47 @@ public:
         return nullptr;
     }
 };
+
+// 2022.10.22
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int n = 0;
+        int m = 0;
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        while(curA != nullptr) {
+            n++;
+            curA = curA->next;
+        }
+        while(curB != nullptr) {
+            m++;
+            curB = curB->next;
+        }
+        if(m > n) {
+            return getIntersectionNode(headB, headA);
+        }
+        int len = n - m;
+        curA = headA;
+        curB = headB;
+        while(len--) {
+            curA = curA->next;
+        }
+        while(curA != nullptr) {
+            if(curA == curB) {
+                return curA;
+            }
+            curA = curA->next;
+            curB = curB->next;
+        }
+        return nullptr;
+    }
+};
