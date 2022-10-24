944be06d0a374accc9e070b35ad8482d46f8263d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-24 09:01:16 +0800

    Update 560_Subarray_Sum_Equals_K.cpp

--- binary_index_tree/560_Subarray_Sum_Equals_K.cpp
+++ binary_index_tree/560_Subarray_Sum_Equals_K.cpp
@@ -130,3 +130,23 @@ public:
         return res;
     }
 };
+
+// 2022.10.24
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int cur = 0;
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            cur += nums[i];
+            if(map.count(cur - k)) {
+                res += map[cur - k];
+            }
+            map[cur]++;
+        }
+        return res;
+    }
+};
