24ebfdedd6adc1ee761b8f69e2e6eba8ccab28d6
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-25 20:47:27 +0800

    Update 303_Range_Sum_Query-Immutable.cpp

--- binary_index_tree/303_Range_Sum_Query-Immutable.cpp
+++ binary_index_tree/303_Range_Sum_Query-Immutable.cpp
@@ -93,3 +93,26 @@ private:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
+
+// 2022.10.25
+class NumArray {
+public:
+    vector<int> sum;
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        sum.resize(n + 1, 0);
+        for(int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
