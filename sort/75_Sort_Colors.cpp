12577b59f6d2983f9f7a7e9fec73b3f82306fdb1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-10-25 20:47:25 +0800

    Update 75_Sort_Colors.cpp

--- sort/75_Sort_Colors.cpp
+++ sort/75_Sort_Colors.cpp
@@ -92,3 +92,23 @@ public:
         quick_sort(0, nums.size() - 1, nums);
     }
 };
+
+// 2022.10.25
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int zero = 0;
+        int two = n - 1;
+        for(int i = 0; i <= two; i++) {
+            while(i <= two && nums[i] == 2) {
+                swap(nums[i], nums[two]);
+                two--;
+            }
+            while(zero <= i && nums[i] == 0) {
+                swap(nums[i], nums[zero]);
+                zero++;
+            }
+        }
+    }
+};
