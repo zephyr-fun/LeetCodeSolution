95eeb4a7ca09954b8bdfbd7cfa1840e7aa786077
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-07 16:01:21 +0800

    Update 21_Merge_Two_Sorted_Lists.cpp

--- linklist/21_Merge_Two_Sorted_Lists.cpp
+++ linklist/21_Merge_Two_Sorted_Lists.cpp
@@ -54,3 +54,46 @@ public:
         return list2;
     }
 };
+
+// 2022.10.28
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* head = dummy;
+        while(list1 != nullptr && list2 != nullptr) {
+            if(list1->val <= list2->val) {
+                head->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                head->next = list2;
+                list2 = list2->next;
+            }
+            head = head->next;
+        }
+        // if
+        while(list1) {
+            head->next = list1;
+            list1 = list1->next;
+            head = head->next;
+        }
+        // if
+        while(list2) {
+            head->next = list2;
+            list2 = list2->next;
+            head = head->next;
+        }
+        return dummy->next;
+    }
+};
