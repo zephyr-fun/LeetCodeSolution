1802c497a3b5ba1f270eca73c579ad6adc647742
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-08 13:12:58 +0800

    Update 470_Implement_Rand10()_Using_Rand7().cpp

--- math/470_Implement_Rand10()_Using_Rand7().cpp
+++ math/470_Implement_Rand10()_Using_Rand7().cpp
@@ -30,3 +30,22 @@ public:
         return -1;
     }
 };
+
+// 2022.11.08
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int cur = 0; // 0-48
+        while(cur == 0 || cur > 40) {
+            cur = (rand7() - 1) * 7 + rand7() - 1;
+        }
+        if(cur % 10 == 0) {
+            return 10;
+        }
+        return cur % 10;
+    }
+};
