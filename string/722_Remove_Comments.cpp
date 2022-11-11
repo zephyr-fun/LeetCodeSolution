f1de492988caf84d37be48b25cdbd9780336f3c0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-11 10:30:19 +0800

    Create 722_Remove_Comments.cpp

---
+++ string/722_Remove_Comments.cpp
@@ -0,0 +1,39 @@
+// 2022.11.11
+class Solution {
+public:
+    vector<string> removeComments(vector<string>& source) {
+        string str;
+        // flatten in to a string
+        for(auto& s: source) {
+            str += s + '\n';
+        }
+        vector<string> res;
+        string line;
+        for(int i = 0; i < str.size(); ) {
+            if(i + 1 < str.size() && str[i] == '/' && str[i + 1] == '/') {
+                while(str[i] != '\n') {
+                    i++;
+                }
+            }
+            else if(i + 1 < str.size() && str[i] == '/' && str[i + 1] == '*') {
+                i += 2;
+                while(str[i] != '*' || str[i + 1] != '/') {
+                    i++;
+                }
+                i += 2;
+            }
+            else if(str[i] == '\n') {
+                if(line.size()) {
+                    res.emplace_back(line);
+                    line.clear();
+                }
+                i++;
+            }
+            else {
+                line += str[i];
+                i++;
+            }
+        }
+        return res;
+    }
+};
