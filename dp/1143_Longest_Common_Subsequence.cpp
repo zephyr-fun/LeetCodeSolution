730949019c6189ebd0da451197517ac79341c1d2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-12 10:46:11 +0800

    Update 1143_Longest_Common_Subsequence.cpp

--- dp/1143_Longest_Common_Subsequence.cpp
+++ dp/1143_Longest_Common_Subsequence.cpp
@@ -55,3 +55,26 @@ public:
         return dp[text1.size()][text2.size()];
     }
 };
+
+// 2022.11.12
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        text1 = ' ' + text1;
+        text2 = ' ' + text2;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(text1[i] == text2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
