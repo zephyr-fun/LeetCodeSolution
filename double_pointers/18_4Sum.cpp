2ec4cd5dee21ed6253f50514fd9f5b8dbc8e1889
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-17 15:09:47 +0800

    Update 18_4Sum.cpp

--- double_pointers/18_4Sum.cpp
+++ double_pointers/18_4Sum.cpp
@@ -49,3 +49,55 @@ public:
         return res;
     }
 };
+
+// 2022.11.17
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        if(n == 0) {
+            return res;
+        }
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for(int j = i + 1; j < n; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                long long temp = 0LL + target - nums[i] - nums[j];
+                int left = j + 1;
+                int right = n - 1;
+                while(left < right) {
+                    if(nums[left] + nums[right] > temp) {
+                        right--;
+                        while(left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                    else if(nums[left] + nums[right] < temp) {
+                        left++;
+                        while(left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                    }
+                    else {
+                        res.emplace_back(initializer_list<int>{nums[i], nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+                        while(left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
