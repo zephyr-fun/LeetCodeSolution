14e772e3c10e51cf0f39d2846039d02ff3a1db78
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-19 20:01:39 +0800

    Update 202_Happy_Number.cpp

--- hash/202_Happy_Number.cpp
+++ hash/202_Happy_Number.cpp
@@ -81,3 +81,24 @@ public:
         }
     }
 };
+
+// 2022.11.19
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(n != 1) {
+            if(set.count(n)) {
+                return false;
+            }
+            set.insert(n);
+            int temp = 0;
+            while(n) {
+                temp += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = temp;
+        }
+        return true;
+    }
+};
