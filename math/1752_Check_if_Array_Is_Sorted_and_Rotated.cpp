aa5d43d2e9f09baae8cd728359b9fe246b1dc7cb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-27 22:29:45 +0800

    Create 1752_Check_if_Array_Is_Sorted_and_Rotated.cpp

---
+++ math/1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,14 @@
+// 2022.11.27
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int cnt = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > nums[(i + 1) % n]) {
+                cnt++;
+            }
+        }
+        return cnt <= 1;
+    }
+};
