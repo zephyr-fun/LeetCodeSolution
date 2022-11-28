2f58216b8bd2c00d3db337c3bac7555e360a8c08
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-28 11:35:13 +0800

    Update 191_Number_of_1_Bits.cpp

--- math/191_Number_of_1_Bits.cpp
+++ math/191_Number_of_1_Bits.cpp
@@ -10,3 +10,33 @@ public:
         return res;
     }
 };
+
+// 2022.11.28
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            res += (n >> i) & 1;
+        }
+        return res;
+    }
+};
+
+// 2022.11.28
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n) {
+            res++;
+            n -= lowbit(n);
+        }
+        return res;
+    }
+
+private:
+    static inline uint32_t lowbit(uint32_t x) {
+        return x & (-x);
+    }
+};
