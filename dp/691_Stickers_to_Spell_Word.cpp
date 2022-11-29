89c1e777301407c77a63347b0235d46d3d759df4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-29 10:26:43 +0800

    Update 691_Stickers_to_Spell_Word.cpp

--- dp/691_Stickers_to_Spell_Word.cpp
+++ dp/691_Stickers_to_Spell_Word.cpp
@@ -42,3 +42,80 @@ public:
         return res;
     }
 };
+
+// 2022.11.29
+int N = 20;
+int M = 1 << 20;
+int INF = 50;
+int dp[1 << 20];
+class Solution {
+public:
+    vector<string> ss;
+    string t;
+    int dfs(int state) {
+        int n = t.size();
+        if(state == ((1 << n)) - 1) {
+            return 0;
+        }
+        if(dp[state] != -1) {
+            return dp[state];
+        }
+        int res = INF;
+        for(auto s : ss) {
+            int nstate = state;
+            for(auto c : s) {
+                for(int i = 0; i < n; i++) {
+                    if(t[i] == c && ((nstate >> i) & 1) == 0) {
+                        nstate |= (1 << i);
+                        break;
+                    }
+                }
+            }
+            if(nstate != state) {
+                res = min(res, dfs(nstate) + 1);
+            }
+        }
+        dp[state] = res;
+        return res;
+    }
+    int minStickers(vector<string>& stickers, string target) {
+        memset(dp, -1, sizeof(dp));
+        ss = stickers;
+        t = target;
+        int res = dfs(0);
+        return res == INF ? -1 : res;
+    }
+};
+
+// dp
+class Solution {
+public:
+    int minStickers(vector<string>& ss, string t) {
+        int INF = 20;
+        int n = ss.size();
+        int m = t.size();
+        int mask = 1 << m;
+        vector<int> dp(mask, INF);
+        dp[0] = 0;
+        for(int state = 0; state < mask; state++) {
+            if(dp[state] == INF) {
+                continue;
+            }
+            for(auto s : ss) {
+                int nstate = state;
+                int len = s.size();
+                for(int i = 0; i < len; i++) {
+                    int c = s[i] - 'a';
+                    for(int j = 0; j < m; j++) {
+                        if(t[j] - 'a' == c && ((nstate >> j) & 1) == 0) {
+                            nstate |= (1 << j);
+                            break;
+                        }
+                    }
+                }
+                dp[nstate] = min(dp[nstate], dp[state] + 1);
+            }
+        }
+        return dp[mask - 1] == INF ? -1 : dp[mask - 1];
+    }
+};
