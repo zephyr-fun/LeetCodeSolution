91ec06ebbee71a7252ba41c34f57068c1c9f97f2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-11-30 09:51:01 +0800

    Update 2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp

--- dp/2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp
+++ dp/2044_Count_Number_of_Maximum_Bitwise-OR_Subsets.cpp
@@ -25,3 +25,30 @@ public:
         return res;
     }
 };
+
+// 2022.11.30
+class Solution {
+public:
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+        int mask = 1 << n;
+        int res = 0;
+        int max = 0;
+        for(int state = 0; state < mask; state++) {
+            int cur = 0;
+            for(int i = 0; i < n; i++) {
+                if(((state >> i) & 1) == 1) { // diff with ? (state & (1 << i)) == 1
+                    cur |= nums[i];
+                }
+            }
+            if(cur > max) {
+                max = cur;
+                res = 1;
+            }
+            else if(cur == max) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
