d69c1e69cf40a1be6b9150108851bfff5dbc656d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-02 10:03:16 +0800

    Update 27_Remove_Element.cpp

--- array/27_Remove_Element.cpp
+++ array/27_Remove_Element.cpp
@@ -72,3 +72,25 @@ public:
         return left;
     }
 };
+
+// 2022.12.02
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int j = 0;
+        int n = nums.size();
+        while(i < n) {
+            if(nums[i] == val) {
+                i++;
+            }
+            else {
+                nums[j] = nums[i];
+                i++;
+                j++;
+            }
+        }
+        nums.resize(j);
+        return j;
+    }
+};
