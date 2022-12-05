1f7138139f94e5f51d1dfdc16a5e6b17e91782da
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-05 14:55:06 +0800

    Update 30_Substring_with_Concatenation_of_All_Words.cpp

--- hash/30_Substring_with_Concatenation_of_All_Words.cpp
+++ hash/30_Substring_with_Concatenation_of_All_Words.cpp
@@ -61,3 +61,45 @@ public:
 };
 
 // sliding window && hash by mod
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int n = s.size();
+        int m = words.size();
+        int w = words[0].size();
+        vector<int> res;
+        unordered_map<string, int> mapw;
+        for(auto& word : words) {
+            mapw[word]++;
+        }
+        for(int i = 0; i < w; i++) {
+            unordered_map<string, int> maps;
+            for(int j = i; j <= n - w; j += w) {
+                string cur = s.substr(j, w);
+                maps[cur]++;
+                if(j >= i + (m * w)) {
+                    int idx = j - m * w;
+                    string prev = s.substr(idx, w);
+                    if(maps[prev] == 1) {
+                        maps.erase(prev);
+                    }
+                    else {
+                        maps[prev]--;
+                    }
+                    // mapw.count(prev) is needed, otherwise mapw[prev] will be written as 0
+                    if(mapw.count(prev) && maps[prev] != mapw[prev]) {
+                        continue;
+                    }
+                }
+                // mapw.count(cur) is needed
+                if(mapw.count(cur) && maps[cur] != mapw[cur]) {
+                    continue;
+                }
+                if(maps == mapw) {
+                    res.emplace_back(j - (m - 1) * w);
+                }
+            }
+        }
+        return res;
+    }
+};
