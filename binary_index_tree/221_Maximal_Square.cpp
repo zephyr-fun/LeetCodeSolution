44eae4feda4ca923cbb6d67aa5308b42be1a3b86
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-08 11:50:28 +0800

    Update 221_Maximal_Square.cpp

--- binary_index_tree/221_Maximal_Square.cpp
+++ binary_index_tree/221_Maximal_Square.cpp
@@ -28,3 +28,30 @@ public:
 };
 
 // dp
+// https://leetcode.cn/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode-solution/
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int res = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        if(n == 0 || m == 0) {
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1; // draw a figure
+                    }
+                    res = max(res, dp[i][j]);
+                }
+            }
+        }
+        return res * res;
+    }
+};
