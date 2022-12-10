ed71216d2d937416c5f29a49f122ae181e6ab4da
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-10 09:49:31 +0800

    Update 1260_Shift_2D_Grid.cpp

--- array/1260_Shift_2D_Grid.cpp
+++ array/1260_Shift_2D_Grid.cpp
@@ -26,3 +26,32 @@ public:
         return res;
     }
 };
+
+// 2022.12.10
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        k = k % (n * m);
+        if(k == 0) {
+            return grid;
+        }
+        int temp[n * m];
+        memset(temp, 0, sizeof(temp));
+        int idx = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                temp[idx++] = grid[i][j];
+            }
+        }
+        idx = n * m - k;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                grid[i][j] = temp[idx];
+                idx = (idx + 1) % (n * m);
+            }
+        }
+        return grid;
+    }
+};
