f2234bcde37d8716df4662ecc3d5f4124e9eb8aa
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-11 09:34:59 +0800

    Update 110_Balanced_Binary_Tree.cpp

--- tree/110_Balanced_Binary_Tree.cpp
+++ tree/110_Balanced_Binary_Tree.cpp
@@ -112,3 +112,60 @@ public:
         return res == -1 ? false : true;
     }
 };
+
+// 2022.12.11
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        return max(left, right) + 1; // tota; recursion
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr) {
+            return true;
+        }
+        if(isBalanced(root->left) == false) { // could move below
+            return false;
+        }
+        if(isBalanced(root->right) == false) { // could move below
+            return false;
+        }
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        return abs(left - right) <= 1;
+    }
+};
+
+// optim
+class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+        return max(dfs(root->left), dfs(root->right)) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr) {
+            return true;
+        }
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
