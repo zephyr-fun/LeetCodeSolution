14f55aad2033e55efff862ec5c6420c925df14ec
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-17 16:28:57 +0800

    Update 664_Strange_Printer.cpp

--- dp/664_Strange_Printer.cpp
+++ dp/664_Strange_Printer.cpp
@@ -20,3 +20,25 @@ public:
         return dp[0][n - 1];
     }
 };
+
+// 2022.12.17
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.size();
+        int dp[n + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        for(int len = 1; len <= n; len++) {
+            for(int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+                dp[l][r] = dp[l + 1][r] + 1;
+                for(int k = l + 1; k <= r; k++) {
+                    if(s[l] == s[k]) {
+                        dp[l][r] = min(dp[l][r], dp[l][k - 1] + dp[k + 1][r]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
