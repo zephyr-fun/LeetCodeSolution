2897efb0d8c1ece6a214cd1d686ddb2f89362e00
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-18 15:20:15 +0800

    Update 1021_Remove_Outermost_Parentheses.cpp

--- double_pointers/1021_Remove_Outermost_Parentheses.cpp
+++ double_pointers/1021_Remove_Outermost_Parentheses.cpp
@@ -24,3 +24,28 @@ public:
         return res;
     }
 };
+
+// 2022.12.18
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int left = 0;
+        int right = 1;
+        int n = s.size();
+        int cur = s[0] == '(' ? 1 : -1;
+        string res;
+        while(right < n) {
+            cur += s[right] == '(' ? 1 : -1;
+            if(cur == 0) {
+                left++;
+                while(left < right) {
+                    res += s[left];
+                    left++;
+                }
+                left = right + 1;
+            }
+            right++;
+        }
+        return res;
+    }
+};
