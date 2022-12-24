2d10728eb7b73786923d0bc62b74667233a53191
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-24 10:50:52 +0800

    Update 264_Ugly_Number_II.cpp

--- math/264_Ugly_Number_II.cpp
+++ math/264_Ugly_Number_II.cpp
@@ -25,3 +25,31 @@ public:
         return ug[n];
     }
 };
+
+// 2022.12.24
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int p2 = 1;
+        int p3 = 1;
+        int p5 = 1;
+        vector<int> ug(n + 1, 0);
+        ug[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            int num1 = 2 * ug[p2];
+            int num2 = 3 * ug[p3];
+            int num3 = 5 * ug[p5];
+            ug[i] = min(num1, min(num2, num3));
+            if(ug[i] == num1) {
+                p2++;
+            }
+            if(ug[i] == num2) {
+                p3++;
+            }
+            if(ug[i] == num3) {
+                p5++;
+            }
+        }
+        return ug[n];
+    }
+};
