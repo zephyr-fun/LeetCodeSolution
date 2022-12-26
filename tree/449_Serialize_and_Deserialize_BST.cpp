6480e107bea7210a20a066e91fd33b569dcfb0d3
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-26 15:05:39 +0800

    Update 449_Serialize_and_Deserialize_BST.cpp

--- tree/449_Serialize_and_Deserialize_BST.cpp
+++ tree/449_Serialize_and_Deserialize_BST.cpp
@@ -170,3 +170,77 @@ public:
 // string tree = ser->serialize(root);
 // TreeNode* ans = deser->deserialize(tree);
 // return ans;
+
+// 2022.12.26
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string res;
+        if(root == nullptr) {
+            return res;
+        }
+        vector<int> arr;
+        postOrderTraversal(root, arr);
+        for(int i = 0; i < arr.size() - 1; i++) {
+            res += to_string(arr[i]);
+            res += ',';
+        }
+        res += to_string(arr[arr.size() - 1]);
+        return res;
+    }
+
+    void postOrderTraversal(TreeNode* root, vector<int>& arr) {
+        if(root == nullptr) {
+            return ;
+        }
+        postOrderTraversal(root->left, arr);
+        postOrderTraversal(root->right, arr);
+        arr.push_back(root->val);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size() == 0) {
+            return nullptr;
+        }
+        vector<int> arr;
+        split(data, arr);
+        stack<int> st;
+        for(int i = 0; i < arr.size(); i++) {
+            st.push(arr[i]);
+        }
+        TreeNode* res;
+        res = construct(st, INT_MAX, INT_MIN);
+        return res;
+    }
+
+    void split(string data, vector<int>& arr) {
+        int left = 0;
+        int right = 0;
+        while(right < data.size()) {
+            while(right < data.size() && data[right] != ',') {
+                right++;
+            }
+            arr.push_back(stoi(data.substr(left, right - left)));
+            while(right < data.size() && data[right] == ',') {
+                right++;
+            }
+            left = right;
+        }
+    }
+
+    TreeNode* construct(stack<int>& st, int upper, int lower) {
+        if(st.size() == 0 || st.top() > upper || st.top() < lower) {
+            return nullptr;
+        }
+        int val = st.top();
+        // cout << val;
+        st.pop();
+        TreeNode* root = new TreeNode(val);
+        root->right = construct(st, upper, val);
+        root->left = construct(st, val, lower);
+        return root;
+    }
+};
