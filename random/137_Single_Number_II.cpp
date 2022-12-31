b0f2edcd7654f2981a95223a52c604636d03c2fc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2022-12-31 09:53:42 +0800

    Update 137_Single_Number_II.cpp

--- random/137_Single_Number_II.cpp
+++ random/137_Single_Number_II.cpp
@@ -56,3 +56,18 @@ public:
         return res;
     }
 };
+
+// 2022.12.31
+// DFA
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0;
+        int two = 0;
+        for(auto& num : nums) {
+            one = one ^ num & ~two;
+            two = two ^ num & ~one;
+        }
+        return one;
+    }
+};
