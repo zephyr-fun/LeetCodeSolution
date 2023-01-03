f91623ec1d2749f25449189a1fd8a2d1ed29726a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-03 10:16:04 +0800

    Update 162_Find_Peak_Element.cpp

--- array/162_Find_Peak_Element.cpp
+++ array/162_Find_Peak_Element.cpp
@@ -15,3 +15,23 @@ public:
         return right;
     }
 };
+
+// 2023.01.03
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[mid + 1]) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};
