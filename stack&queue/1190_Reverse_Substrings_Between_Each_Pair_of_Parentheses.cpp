c3dcc8f1f5aff16933f1abb71139381d022c45fc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-05 22:22:29 +0800

    Update 1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp

--- stack&queue/1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp
+++ stack&queue/1190_Reverse_Substrings_Between_Each_Pair_of_Parentheses.cpp
@@ -27,3 +27,42 @@ public:
         return cur;
     }
 };
+
+// 2023.01.05
+class Solution {
+public:
+    string helper(string& s) {
+        // 先找到最里面的左括号
+        int l = s.find_last_of('(', s.size() - 1);
+        // 如果没有括号直接返回
+        if(l == string::npos) {
+            return s;
+        }
+        // 找到最里面的右括号的位置
+        int r = l + 1;
+        while(s[r] != ')'){
+            r++;
+        }
+        // 初始化双指针，指向最里面括号的两端字符
+        int left = l + 1;
+        int right = r - 1;
+        // 反转括号内的字符串
+        while(left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        // 用之前找到的左边括号的位置调用erase方法删除左括号
+        s.erase(l, 1);
+        // 因为上一行代码删除了左括号，字符串长度少了1，所以这一步r要减1
+        s.erase(r - 1, 1);
+        // 递归调用
+        return helper(s);
+    }
+
+    string reverseParentheses(string s) {
+        return helper(s);
+    }
+};
