0c3248c5c2e8e0d4efec7b953a036082af78e0fc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-09 11:01:21 +0800

    Update 1631_Path_With_Minimum_Effort.cpp

--- binary_search/1631_Path_With_Minimum_Effort.cpp
+++ binary_search/1631_Path_With_Minimum_Effort.cpp
@@ -165,3 +165,82 @@ public:
         return dist[m * n - 1];
     }
 };
+
+// 2023.01.09
+class UnionFindSet {
+public:
+    UnionFindSet(int n) : parent(n + 7), rank(n + 7, 0) {
+        for(int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+    }
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+        }
+        else if(rank[pv] > rank[pu]) {
+            parent[pu] = pv;
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        return true;
+    }
+
+    bool isConnect(int u, int v) {
+        return Find(u) == Find(v);
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+};
+class Solution {
+public:
+    int getIdx(int x, int y, int m) {
+        return x * m + y;
+    }
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        vector<vector<int>> grid;
+        int n = heights.size();
+        int m = heights[0].size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i + 1 < n) {
+                    grid.emplace_back(initializer_list<int>{getIdx(i, j, m), getIdx(i + 1, j, m), abs(heights[i][j] - heights[i + 1][j])});
+                }
+                if(j + 1 < m) {
+                    grid.emplace_back(initializer_list<int>{getIdx(i, j, m), getIdx(i, j + 1, m), abs(heights[i][j] - heights[i][j + 1])});
+                }
+            }
+        }
+        sort(grid.begin(), grid.end(), [](vector<int>& a, vector<int>& b) {
+            return a[2] < b[2];
+        });
+        UnionFindSet set(n * m);
+        int cost = 0;
+        auto it = grid.begin();
+        while(!set.isConnect(getIdx(0, 0, m) + 1, getIdx(n - 1, m - 1, m) + 1)) {
+            int u = (*it)[0];
+            int v = (*it)[1];
+            cost = (*it)[2];
+            set.Union(u + 1, v + 1);
+            it++;
+        }
+        return cost;
+    }
+};
