a4f7e64f0bb15682f07a596f768ebb5954a90049
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-14 12:40:20 +0800

    Update 1819_Number_of_Different_Subsequences_GCDs.cpp

--- math/1819_Number_of_Different_Subsequences_GCDs.cpp
+++ math/1819_Number_of_Different_Subsequences_GCDs.cpp
@@ -40,3 +40,62 @@ public:
         return res;
     }
 };
+
+// 2023.01.14
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        int res = 0;
+        int mx = *max_element(nums.begin(), nums.end());
+        bool has[mx + 1];
+        memset(has, 0, sizeof(has));
+        for(auto& num : nums) {
+            has[num] = true;
+        }
+        for(int i = 1; i <= mx; i++) {
+            int g = 0;
+            for(int j = i; j <= mx && g != i; j += i) {
+                if(has[j]) {
+                    g = gcd(g, j);
+                }
+            }
+            if(g == i) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+// https://leetcode.cn/problems/number-of-different-subsequences-gcds/solution/ji-bai-100mei-ju-gcdxun-huan-you-hua-pyt-get7/
+// optim
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        int res = 0;
+        int mx = *max_element(nums.begin(), nums.end());
+        bool has[mx + 1];
+        memset(has, 0, sizeof(has));
+        for(auto& num : nums) {
+            if(!has[num]) {
+                has[num] = true;
+                res++;
+            }
+        }
+        for(int i = 1; i <= mx / 3; i++) {
+            if(has[i]) {
+                continue;
+            }
+            int g = 0;
+            for(int j = 2 * i; j <= mx && g != i; j += i) {
+                if(has[j]) {
+                    g = gcd(g, j);
+                }
+            }
+            if(g == i) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
