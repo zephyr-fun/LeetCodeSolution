5b2f52d8e889cf330e345332364b8c28f37356d7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-18 23:31:07 +0800

    Update 257_Binary_Tree_Paths.cpp

--- tree/257_Binary_Tree_Paths.cpp
+++ tree/257_Binary_Tree_Paths.cpp
@@ -180,3 +180,44 @@ public:
         return res;
     }
 };
+
+// 2023.01.18
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> path;
+    vector<string> res;
+    void traversal(TreeNode* root) {
+        if(root == nullptr) {
+            return ;
+        }
+        if(root->left == nullptr && root->right == nullptr) {
+            string temp = "";
+            for(int p : path) {
+                temp += to_string(p);
+                temp += "->";
+            }
+            temp += to_string(root->val);
+            res.emplace_back(temp);
+            return ;
+        }
+        path.emplace_back(root->val);
+        traversal(root->left);
+        traversal(root->right);
+        path.pop_back();
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        traversal(root);
+        return res;
+    }
+};
