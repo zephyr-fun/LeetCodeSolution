011216072df25dab0e9b7efabea09107814fee4a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-21 22:12:41 +0800

    Update 141_Linked_List_Cycle.cpp

--- double_pointers/141_Linked_List_Cycle.cpp
+++ double_pointers/141_Linked_List_Cycle.cpp
@@ -49,3 +49,20 @@ public:
         return false;
     }
 };
+
+// 2023.01.21
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
