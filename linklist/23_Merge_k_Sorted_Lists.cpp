d5603287bf10b8f5ce9160185c0e99c297e188e2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-26 19:16:52 +0800

    Update 23_Merge_k_Sorted_Lists.cpp

--- linklist/23_Merge_k_Sorted_Lists.cpp
+++ linklist/23_Merge_k_Sorted_Lists.cpp
@@ -213,3 +213,44 @@ public:
         return res->next;
     }
 };
+
+// 2023.01.26
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct Cmp{
+        bool operator() (ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        priority_queue<ListNode*, vector<ListNode*>, Cmp> que;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        for(auto& list : lists) {
+            if(list) {
+                que.push(list);
+            }
+        }
+        while(!que.empty()) {
+            ListNode* cur = que.top();
+            que.pop();
+            temp->next = cur;
+            temp = temp->next;
+            if(cur->next) {
+                que.push(cur->next);
+            }
+        }
+        return dummy->next;
+    }
+};
