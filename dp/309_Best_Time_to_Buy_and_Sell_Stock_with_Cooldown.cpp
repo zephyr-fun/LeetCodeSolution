6c79d44297c681ad362667e539b5157134fc4a3d
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-28 12:02:28 +0800

    Update 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp

--- dp/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
+++ dp/309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -24,3 +24,27 @@ public:
 // dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
 // dp[i][1] = max(dp[i - 1][1], dp[i - 1][2]);
 // dp[i][2] = dp[i - 1][0] + prices[i];
+
+// 2023.01.28
+// 状态机
+// 0：冷冻期（包括平常期）
+// 1：持有期（持有状态）
+// 2：今日卖出
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 0) {
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(3, -0x3f3f3f3f));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = dp[i - 1][1] + prices[i];
+        }
+        return max(dp[n - 1][0], max(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
