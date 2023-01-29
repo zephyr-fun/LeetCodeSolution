0a1d67415467e9eca95a356987bb571d8bf67477
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-29 09:55:41 +0800

    Update 6104_Count_Asterisks.cpp

--- weekly/biweekly-contest-81/6104_Count_Asterisks.cpp
+++ weekly/biweekly-contest-81/6104_Count_Asterisks.cpp
@@ -4,12 +4,30 @@ public:
     int countAsterisks(string s) {
         int res = 0;
         int cnt = 0;
-        for(int i = 0;i<s.size();i++){
-            if(s[i]=='*'&&cnt%2!=1){
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '*' && cnt % 2 != 1){
                 res++;
             }
-            else if(s[i]=='|'){
+            else if(s[i] == '|'){
                 cnt++;
+            }
+        }
+        return res;
+    }
+};
+
+// 2023.01.29
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int res = 0;
+        int cnt = 0;
+        for(auto& c : s) {
+            if(c == '|') {
+                cnt++;
+            }
+            else if(c == '*') {
+                res += (cnt & 1) ^ 1;
             }
         }
         return res;
