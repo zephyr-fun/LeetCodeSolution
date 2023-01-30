0caceef585adbdc6a3f37cd21695f13abbeaf99a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-01-30 10:08:00 +0800

    Update 1669_Merge_In_Between_Linked_Lists.cpp

--- linklist/1669_Merge_In_Between_Linked_Lists.cpp
+++ linklist/1669_Merge_In_Between_Linked_Lists.cpp
@@ -32,3 +32,38 @@ public:
         return dummy->next;
     }
 };
+
+// 2023.01.30
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = list1;
+        ListNode* prea = dummy;
+        while(a--) {
+            prea = prea->next;
+        }
+        ListNode* nxtb = dummy->next;
+        while(b--) {
+            nxtb = nxtb->next;
+        }
+        nxtb = nxtb->next;
+        prea->next = list2;
+        ListNode* preb = list2;
+        while(preb->next) {
+            preb = preb->next;
+        }
+        preb->next = nxtb;
+        return dummy->next;
+    }
+};
