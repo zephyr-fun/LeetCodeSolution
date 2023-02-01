e4ad1e46c738594da34f31a8d212861b9afab3bc
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-01 09:34:42 +0800

    Update 6108_Decode_the_Message.cpp

--- weekly/weekly-contest-300/6108_Decode_the_Message.cpp
+++ weekly/weekly-contest-300/6108_Decode_the_Message.cpp
@@ -25,3 +25,28 @@ public:
         return res;
     }
 };
+
+// 2023.02.01
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char, char> map;
+        char cur = 'a';
+        for(auto& c : key) {
+            if(!map.count(c) && c >= 'a' && c <= 'z') {
+                map[c] = cur;
+                cur++;
+            }
+        }
+        string res = "";
+        for(auto& c : message) {
+            if(c >= 'a' && c <= 'z') {
+                res += map[c];
+            }
+            else {
+                res += c;
+            }
+        }
+        return res;
+    }
+};
