07cec55e6c9c0cb9d620021ad80af54583050b9a
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-01 09:49:15 +0800

    Update 2180_Count_Integers_With_Even_Digit_Sum.cpp

--- math/2180_Count_Integers_With_Even_Digit_Sum.cpp
+++ math/2180_Count_Integers_With_Even_Digit_Sum.cpp
@@ -20,3 +20,36 @@ public:
         return res;
     }
 };
+
+// 2023.02.01
+// math
+// xyz0 -> xyzd
+// 0 -> xyz0 [a, a + 10]
+class Solution {
+public:
+    int countEven(int num) {
+        int t = 0;
+        int d = num % 10;
+        int a = num - d;
+        num = a;
+        while(num) {
+            t += num % 10;
+            num /= 10;
+        }
+        return a / 2 - 1 + (t % 2 == 0 ? d / 2 + 1 : ++d / 2); 
+    }
+};
+
+// optim
+class Solution {
+public:
+    int countEven(int num) {
+        int t = 0;
+        int cur = num;
+        while(cur) {
+            t += cur % 10;
+            cur /= 10;
+        }
+        return t % 2 == 1 ? --num / 2 : num / 2;
+    }
+};
