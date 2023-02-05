a82fde7dbc00b27903ad60b37ea6b75c9f731278
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-05 14:22:03 +0800

    Update 1710_Maximum_Units_on_a_Truck.cpp

--- greedy/1710_Maximum_Units_on_a_Truck.cpp
+++ greedy/1710_Maximum_Units_on_a_Truck.cpp
@@ -10,9 +10,34 @@ public:
         for(int i = 0, cnt = 0; i < n && cnt < k; i++) {
             int a = bs[i][0];
             int b = bs[i][1];
-            int c = min(a, k - cnt);
+            int c = min(a, k - cnt); // introducing a c is a good way to simplify the code
             res += b * c;
             cnt += c;
+        }
+        return res;
+    }
+};
+
+// 2023.02.05
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        int n = boxTypes.size();
+        int res = 0;
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] > b[1];
+        });
+        int idx = 0;
+        while(truckSize && idx < n) {
+            if(truckSize >= boxTypes[idx][0]) {
+                truckSize -= boxTypes[idx][0];
+                res += boxTypes[idx][0] * boxTypes[idx][1];
+                idx++;
+            }
+            else {
+                res += truckSize * boxTypes[idx][1];
+                truckSize = 0;
+            }
         }
         return res;
     }
