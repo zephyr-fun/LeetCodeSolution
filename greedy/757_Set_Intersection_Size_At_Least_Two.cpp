561df01742a3ec336a28c5c79065054fd6a238c9
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-08 11:02:22 +0800

    Update 757_Set_Intersection_Size_At_Least_Two.cpp

--- greedy/757_Set_Intersection_Size_At_Least_Two.cpp
+++ greedy/757_Set_Intersection_Size_At_Least_Two.cpp
@@ -25,3 +25,35 @@ public:
         return cnt;
     }
 };
+
+// 2023.02.08
+// optim vector to maxi, submax and res
+class Solution {
+public:
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [] (vector<int>& a, vector<int>& b) {
+            if(a[1] == b[1]) {
+                return a[0] > b[0];
+            }
+            return a[1] < b[1];
+        });
+        int maxi = -1;
+        int submax = -1;
+        int res = 0;
+        for(auto& it : intervals) {
+            int left = it[0];
+            int right = it[1];
+            if(left > maxi) {
+                maxi = right;
+                submax = right - 1;
+                res += 2;
+            }
+            else if(left > submax) {
+                submax = maxi;
+                maxi = right;
+                res += 1;
+            }
+        }
+        return res;
+    }
+};
