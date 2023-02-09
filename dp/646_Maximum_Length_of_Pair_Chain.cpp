c14ec20f286d958fb1864b7fda71fd81e7b98683
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-09 16:54:21 +0800

    Update 646_Maximum_Length_of_Pair_Chain.cpp

--- dp/646_Maximum_Length_of_Pair_Chain.cpp
+++ dp/646_Maximum_Length_of_Pair_Chain.cpp
@@ -55,3 +55,43 @@ public:
         return res;
     }
 };
+
+// 2023.02.09
+// LIS optim
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        int len = 0;
+        sort(pairs.begin(), pairs.end(), [] (vector<int>& a, vector<int>& b) {
+            if(a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            return a[0] < b[0];
+        });
+        vector<int> dp(n + 7, -0x3f3f3f3f); // note pairs[i] -> [-1000, 1000]
+        for(int i = 0; i < n; i++) {
+            if(pairs[i][0] > dp[len]) {
+                len++;
+                dp[len] = pairs[i][1];
+            }
+            else {
+                int left = 0;
+                int right = len - 1;
+                int pos = 0;
+                while(left <= right) {
+                    int mid = (left + right) >> 1;
+                    if(dp[mid] >= pairs[i][0]) {
+                        right = mid - 1;
+                    }
+                    else {
+                        pos = mid;
+                        left = mid + 1;
+                    }
+                }
+                dp[pos + 1] = min(dp[pos + 1], pairs[i][1]);
+            }
+        }
+        return len;
+    }
+};
