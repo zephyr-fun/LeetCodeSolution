93a4017c1a68b9a41f1629ae4af898e000ea93b1
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-16 16:58:45 +0800

    Update 334_Increasing_Triplet_Subsequence.cpp

--- dp/334_Increasing_Triplet_Subsequence.cpp
+++ dp/334_Increasing_Triplet_Subsequence.cpp
@@ -44,3 +44,25 @@ public:
         return false;
     }
 };
+
+// 2023.02.16
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int dp[2];
+        dp[0] = INT_MAX;
+        dp[1] = INT_MAX;
+        for(auto& num : nums) {
+            if(num > dp[1]) {
+                return true;
+            }
+            else if(num > dp[0] && num < dp[1]) {
+                dp[1] = num;
+            }
+            else if(num < dp[0]){
+                dp[0] = num;
+            }
+        }
+        return false;
+    }
+};
