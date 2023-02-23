029e2a2b74c6c7d1dc14c6830b4acde78538f3e5
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-23 10:08:57 +0800

    Update 1877_Minimize_Maximum_Pair_Sum_in_Array.cpp

--- greedy/1877_Minimize_Maximum_Pair_Sum_in_Array.cpp
+++ greedy/1877_Minimize_Maximum_Pair_Sum_in_Array.cpp
@@ -17,3 +17,20 @@ public:
         return res;
     }
 };
+
+// 2023.02.23
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        int right = nums.size() - 1;
+        int res = 0;
+        while(left < right) {
+            res = max(res, nums[left] + nums[right]);
+            left++;
+            right--;
+        }
+        return res;
+    }
+};
