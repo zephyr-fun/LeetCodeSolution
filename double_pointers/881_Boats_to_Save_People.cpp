d88504b15db470fa5858396c133efe01c16aee49
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-25 10:15:41 +0800

    Create 881_Boats_to_Save_People.cpp

---
+++ double_pointers/881_Boats_to_Save_People.cpp
@@ -0,0 +1,19 @@
+// 2023.02.25
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int left = 0;
+        int right = n - 1;
+        int res = 0;
+        sort(people.begin(), people.end());
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            res++;
+        }
+        return res;
+    }
+};
