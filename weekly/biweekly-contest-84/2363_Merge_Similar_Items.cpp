c1738eb7918cf4875b5e2f6f497b644369ec0bf2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-02-28 09:45:25 +0800

    Update 2363_Merge_Similar_Items.cpp

--- weekly/biweekly-contest-84/2363_Merge_Similar_Items.cpp
+++ weekly/biweekly-contest-84/2363_Merge_Similar_Items.cpp
@@ -20,3 +20,25 @@ public:
         return res;
     }
 };
+
+// 2023.02.28
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        unordered_map<int, int> map;
+        for(auto& item : items1) {
+            map[item[0]] += item[1];
+        }
+        for(auto& item : items2) {
+            map[item[0]] += item[1];
+        }
+        vector<vector<int>> res;
+        for(auto& [k, v] : map) {
+            res.push_back({k, v});
+        }
+        sort(res.begin(), res.end(), [] (vector<int>& a, vector<int>& b) {
+            return a[0] < b[0];
+        });
+        return res;
+    }
+};
