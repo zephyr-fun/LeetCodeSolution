407e9877fb09b97d7021c48bd8bfb06998cedd80
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-01 16:04:14 +0800

    Update 6148_Largest_Local_Values_in_a_Matrix.cpp

--- weekly/weekly-contest-306/6148_Largest_Local_Values_in_a_Matrix.cpp
+++ weekly/weekly-contest-306/6148_Largest_Local_Values_in_a_Matrix.cpp
@@ -16,3 +16,24 @@ public:
         return res;
     }
 };
+
+// 2023.03.01
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> res(n - 2, vector<int>(n - 2, 0));
+        // i, j -> (n - 2, n - 2)
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = 0; j < n - 2; j++) {
+                // x, y -> (n, n)
+                for(int x = i; x <= i + 2; x++) { 
+                    for(int y = j; y <= j + 2; y++) {
+                        res[i][j] = max(res[i][j], grid[x][y]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
