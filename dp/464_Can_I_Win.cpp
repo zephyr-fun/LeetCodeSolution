b41368e78cb3a0b414edca11e367ddfd84f7ac64
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-02 10:03:32 +0800

    Update 464_Can_I_Win.cpp

--- dp/464_Can_I_Win.cpp
+++ dp/464_Can_I_Win.cpp
@@ -27,3 +27,43 @@ public:
         return dp[state] = 0;
     }
 };
+
+// 2023.03.02
+class Solution {
+public:
+    int t;
+    int n;
+    int dp[1 << 20];
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        t = desiredTotal;
+        n = maxChoosableInteger;
+        memset(dp, -1, sizeof(dp));
+        if(t == 0) {
+            return true;
+        }
+        if(n * (n + 1) / 2 < t) {
+            return false;
+        }
+        return dfs(0, 0);
+    }
+    bool dfs(int tot, int state) {
+        if(dp[state] != -1) {
+            return dp[state];
+        }
+        for(int i = 0; i < n; i++) {
+            if((state >> i) & 1) {
+                continue;
+            }
+            if(tot + i + 1 >= t) {
+                dp[state] = 1; // notice
+                return true;
+            }
+            if(dfs(tot + i + 1, state | (1 << i)) == 0) {
+                dp[state] = 1; // notice
+                return true;
+            }
+        }
+        dp[state] = 0;
+        return false;
+    }
+};
