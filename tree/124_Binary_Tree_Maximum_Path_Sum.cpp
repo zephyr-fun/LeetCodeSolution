cdb8446e7167686383fa3f9c1e950b6e187b991f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-07 22:18:00 +0800

    Update 124_Binary_Tree_Maximum_Path_Sum.cpp

--- tree/124_Binary_Tree_Maximum_Path_Sum.cpp
+++ tree/124_Binary_Tree_Maximum_Path_Sum.cpp
@@ -49,3 +49,33 @@ public:
         return root->val + max(left, right);
     }
 }
+
+// 2023.03.07
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = -0x3f3f3f3f;
+    int oneSideMax(TreeNode* cur) {
+        if(cur == nullptr) {
+            return 0;
+        }
+        int left = max(0, oneSideMax(cur->left));
+        int right = max(0, oneSideMax(cur->right));
+        res = max(res, left + right + cur->val);
+        return max(left, right) + cur->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        oneSideMax(root);
+        return res;
+    }
+};
