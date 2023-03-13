d92b72a2583218c116c42a735ea0b5733ca05ce2
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-13 16:51:50 +0800

    Update 78_Subsets.cpp

--- backtrack/78_Subsets.cpp
+++ backtrack/78_Subsets.cpp
@@ -42,4 +42,47 @@ public:
     }
 };
 
+// 2023.03.13
 // bit operation
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        for(int mask = 0; mask < (1 << n); mask++) {
+            vector<int> temp;
+            for(int i = 0; i < n; i++) {
+                if(mask & (1 << i)) {
+                    temp.emplace_back(nums[i]);
+                }
+            }
+            res.emplace_back(temp);
+        }
+        return res;
+    }
+};
+
+// chose or not
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> path;
+        int n = nums.size();
+        function<void(int)> dfs = [&] (int cur) {
+            if(cur == n) {
+                res.emplace_back(path);
+                return ;
+            }
+            // not chose
+            dfs(cur + 1);
+
+            // chose and recover(return to last layer)
+            path.emplace_back(nums[cur]);
+            dfs(cur + 1);
+            path.pop_back();
+        };
+        dfs(0);
+        return res;
+    }
+};
