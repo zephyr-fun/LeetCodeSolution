4c255b89112ec2f3875843534561b0cb4b4705ba
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-13 16:52:07 +0800

    Update 6152_Minimum_Hours_of_Training_to_Win_a_Competition.cpp

--- weekly/weekly-contest-307/6152_Minimum_Hours_of_Training_to_Win_a_Competition.cpp
+++ weekly/weekly-contest-307/6152_Minimum_Hours_of_Training_to_Win_a_Competition.cpp
@@ -18,3 +18,22 @@ public:
         return ensum + ex;
     }
 };
+
+// 2023.03.13
+// 对于engry，需要最少更改初始化多少，才能满足任意时刻的值都大于0，只需要将缺口最大的值弥补即可
+class Solution {
+public:
+    int minNumberOfHours(int ieng, int iexp, vector<int>& energy, vector<int>& experience) {
+        int n = energy.size();
+        int ensum = accumulate(energy.begin(), energy.end(), 0);
+        ensum = ieng - ensum;
+        ensum = ensum > 0 ? 0 : abs(ensum) + 1;
+        int diffexp = iexp;
+        for(int i = 0; i < n; i++) {
+            diffexp = min(diffexp, iexp - experience[i]);
+            iexp += experience[i];
+        }
+        diffexp = diffexp > 0 ? 0 : abs(diffexp) + 1;
+        return ensum + diffexp;
+    }
+};
