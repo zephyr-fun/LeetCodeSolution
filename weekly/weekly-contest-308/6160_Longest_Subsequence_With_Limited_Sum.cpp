fa4c83e2cdd69bcecc3ff656f5b5ad71b305729e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-17 10:08:27 +0800

    Update 6160_Longest_Subsequence_With_Limited_Sum.cpp

--- weekly/weekly-contest-308/6160_Longest_Subsequence_With_Limited_Sum.cpp
+++ weekly/weekly-contest-308/6160_Longest_Subsequence_With_Limited_Sum.cpp
@@ -30,3 +30,37 @@ public:
         return queries;
     }
 };
+
+// 2023.03.17
+// optim with preSum
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int preSum[n + 1];
+        memset(preSum, 0, sizeof(preSum));
+        for(int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+        int m = queries.size();
+        vector<int> res(m, 0);
+        for(int i = 0; i < m; i++) {
+            int left = 0;
+            int right = n;
+            int pos = 0;
+            while(left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if(preSum[mid] <= queries[i]) {
+                    pos = mid;
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            res[i] = pos;
+        }
+        return res;
+    }
+};
