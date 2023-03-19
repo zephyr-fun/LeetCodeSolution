7204ada82dba59ef9857338538870f938518d094
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-19 16:52:52 +0800

    Update 456_132_Pattern.cpp

--- monotonic_stack/456_132_Pattern.cpp
+++ monotonic_stack/456_132_Pattern.cpp
@@ -45,3 +45,24 @@ public:
         return false;
     }
 };
+
+// 2023.03.19
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        int maxVal = -0x3f3f3f3f;
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            if(nums[i] < maxVal) {
+                return true;
+            }
+            while(!st.empty() && st.top() < nums[i]) {
+                maxVal = max(maxVal, st.top());
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
