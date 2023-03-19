a9eb1c9a40dabb3fcbfb9c9e81330752aeee9793
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-19 16:52:55 +0800

    Update 654_Maximum_Binary_Tree.cpp

--- tree/654_Maximum_Binary_Tree.cpp
+++ tree/654_Maximum_Binary_Tree.cpp
@@ -193,3 +193,42 @@ public:
         return traversal(0, n - 1);
     }
 };
+
+// 2023.03.19
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nums;
+    TreeNode* build(int left, int right) {
+        if(left > right) {
+            return nullptr;
+        }
+        int maxIdx = left;
+        int maxVal = nums[left];
+        for(int i = left; i <= right; i++) {
+            if(nums[i] > maxVal) {
+                maxVal = nums[i];
+                maxIdx = i;
+            }
+        }
+        TreeNode* cur = new TreeNode(maxVal);
+        cur->left = build(left, maxIdx - 1);
+        cur->right = build(maxIdx + 1, right);
+        return cur;
+    }
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums_) {
+        nums = nums_;
+        int n = nums_.size();
+        return build(0, n - 1);
+    }
+};
