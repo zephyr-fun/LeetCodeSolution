c36b5428698f27511b009ef82ca09262403ca7ac
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-20 22:23:21 +0800

    Update 233_Number_of_Digit_One.cpp

--- dp/233_Number_of_Digit_One.cpp
+++ dp/233_Number_of_Digit_One.cpp
@@ -38,3 +38,66 @@ public:
 //             if (!is_limit) dp[i][cnt1] = res;
 //             return res;
 //         };
+
+// 2023.03.20
+// modify the template
+class Solution {
+public:
+    int countDigitOne(int n) {
+        string s = to_string(n);
+        int m = s.size();
+        int dp[m][m];
+        memset(dp, -1, sizeof(dp));
+        function<int(int, int, bool, bool)> dfs = [&] (int i, int cnt, bool is_limit, bool is_num) {
+            if(i == m) {
+                return cnt;
+            }
+            if(!is_limit && is_num && dp[i][cnt] != -1) {
+                return dp[i][cnt];
+            }
+            int res = 0;
+            if(!is_num) {
+                res = dfs(i + 1, cnt, false, false);
+            }
+            int up = is_limit ? s[i] - '0' : 9;
+            for(int d = 1 - (int) is_num; d <= up; d++) {
+                res += dfs(i + 1, cnt + (d == 1), is_limit && d == up, true);
+            }
+            if(!is_limit && is_num) {
+                dp[i][cnt] = res;
+            }
+            return res;
+        };
+        return dfs(0, 0, true, false);
+    }
+};
+
+// in this case, the pre 0 is not an issue, so you can remove the para is_num
+// this works fine
+class Solution {
+public:
+    int countDigitOne(int n) {
+        string s = to_string(n);
+        int m = s.size();
+        int dp[m][m];
+        memset(dp, -1, sizeof(dp));
+        function<int(int, int, bool)> dfs = [&] (int i, int cnt, bool is_limit) {
+            if(i == m) {
+                return cnt;
+            }
+            if(!is_limit && dp[i][cnt] != -1) {
+                return dp[i][cnt];
+            }
+            int res = 0;
+            int up = is_limit ? s[i] - '0' : 9;
+            for(int d = 0; d <= up; d++) {
+                res += dfs(i + 1, cnt + (d == 1), is_limit && d == up);
+            }
+            if(!is_limit) {
+                dp[i][cnt] = res;
+            }
+            return res;
+        };
+        return dfs(0, 0, true);
+    }
+};
