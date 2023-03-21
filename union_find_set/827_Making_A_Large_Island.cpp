3f0001cd7e1a22e21608c0240b48254aa3aa14ab
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-21 14:58:26 +0800

    Update 827_Making_A_Large_Island.cpp

--- union_find_set/827_Making_A_Large_Island.cpp
+++ union_find_set/827_Making_A_Large_Island.cpp
@@ -198,3 +198,104 @@ public:
         return res == 0 ? n * m : res;
     }
 };
+
+// 2023.03.21
+class UnionFindSet {
+public:
+    UnionFindSet(int k, vector<vector<int>>& g) : parent(k + 1), rank(k + 1, 0), size(k + 1, 0) {
+        int n = g.size();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                parent[i * n + j] = i * n + j;
+                if(g[i][j] == 1) {
+                    size[i * n + j] = 1;
+                }
+            }
+        }
+    }
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else if(rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+            size[pu] += size[pv];
+        }
+        return true;
+    }
+
+    int getSize(int u) {
+        int pu = Find(u);
+        return size[pu];
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+};
+class Solution {
+public:
+    const int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int getIdx(int i, int j, int m) {
+        return i * m + j;
+    }
+    int largestIsland(vector<vector<int>>& grid) {
+        int res = 1;
+        int n = grid.size();
+        UnionFindSet set(n * n, grid);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    if(i + 1 < n && grid[i + 1][j] == 1) {
+                        set.Union(getIdx(i, j, n), getIdx(i + 1, j, n));
+                    }
+                    if(j + 1 < n && grid[i][j + 1] == 1) {
+                        set.Union(getIdx(i, j, n), getIdx(i, j + 1, n));
+                    }
+                }
+                res = max(res, set.getSize(getIdx(i, j, n))); // extra cost, you can optim this by delete it
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) {
+                    unordered_set<int> root_set;
+                    int temp = 1;
+                    for(int k = 0; k < 4; k++) {
+                        int ni = i + dirs[k][0];
+                        int nj = j + dirs[k][1];
+                        if(ni < 0 || ni >= n || nj < 0 || nj >= n || grid[ni][nj] == 0) { // grid[ni][nj] == 0 matters
+                            continue;
+                        }
+                        if(!root_set.count(set.Find(getIdx(ni, nj, n)))) {
+                            root_set.insert(set.Find(getIdx(ni, nj, n)));
+                            temp += set.getSize(getIdx(ni, nj, n));
+                        }
+                    }
+                    res = max(res, temp);
+                }
+            }
+        }
+        return res;
+    }
+};
