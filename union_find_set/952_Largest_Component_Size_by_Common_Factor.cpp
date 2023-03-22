4f235f1030dc589a57535d4bc1b33692102d7af4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-22 11:36:49 +0800

    Update 952_Largest_Component_Size_by_Common_Factor.cpp

--- union_find_set/952_Largest_Component_Size_by_Common_Factor.cpp
+++ union_find_set/952_Largest_Component_Size_by_Common_Factor.cpp
@@ -151,3 +151,73 @@ public:
         return res;
     }
 };
+
+// 2023.03.22
+class UnionFindSet {
+public:
+    UnionFindSet(int n, vector<int>& nums) : parent(n + 1), rank(n + 1, 0), size(n + 1) {
+        for(int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+        for(auto& num : nums) {
+            size[num] = 1;
+        }
+    }
+
+    int Find(int u) {
+        if(u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if(pu == pv) {
+            return false;
+        }
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else if(rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+            rank[pu]++;
+        }
+        return true;
+    }
+
+    int getSize(int u) {
+        int pu = Find(u);
+        return size[pu];
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+};
+class Solution {
+public:
+    int largestComponentSize(vector<int>& nums) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        UnionFindSet set(maxVal, nums);
+        int res = 0;
+        for(auto& num : nums) {
+            for(int i = sqrt(num); i > 1; i--) {
+                if(num % i == 0) {
+                    set.Union(num, i);
+                    set.Union(num, num / i);
+                }
+            }
+            res = max(res, set.getSize(num));
+        }
+        return res; 
+    }
+};
