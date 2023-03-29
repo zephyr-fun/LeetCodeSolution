e66120635122c1bd56d798bc247623fc7085232e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-29 10:50:17 +0800

    Update 593_Valid_Square.cpp

--- math/593_Valid_Square.cpp
+++ math/593_Valid_Square.cpp
@@ -14,3 +14,20 @@ public:
         return set.size() == 2 && *set.begin() * 2 == *set.rbegin() ? true : false;
     }
 };
+
+// 2023.03.29
+class Solution {
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        vector<vector<int>> node = {p1, p2, p3, p4};
+        set<int> set; // 最终只会有短边和对角线两种长度
+        for(int i = 0; i < 3; i++) {
+            for(int j = i + 1; j < 4; j++) {
+                int len1 = node[i][0] - node[j][0];
+                int len2 = node[i][1] - node[j][1];
+                set.insert(len1 * len1 + len2 * len2);
+            }
+        }
+        return set.size() == 2 && *set.begin() * 2 == *set.rbegin() ? true : false;
+    }
+};
