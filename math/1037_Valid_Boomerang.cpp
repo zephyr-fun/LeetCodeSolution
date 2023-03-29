eb77361b42b88774c5eb77789d297d1c3ed98d66
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-03-29 10:50:15 +0800

    Update 1037_Valid_Boomerang.cpp

--- math/1037_Valid_Boomerang.cpp
+++ math/1037_Valid_Boomerang.cpp
@@ -9,3 +9,16 @@ public:
         return s == 0 ? false : true;
     }
 };
+
+// 2023.03.29
+// 海伦公式
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        auto point1 = points[0];
+        auto point2 = points[1];
+        auto point3 = points[2];
+        int s = point1[0] * point2[1] + point2[0] * point3[1] + point3[0] * point1[1] - point1[0] * point3[1] - point2[0] * point1[1] - point3[0] * point2[1];
+        return s != 0;
+    }
+};
