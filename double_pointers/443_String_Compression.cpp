ca74942f78841fb9c3c9ae87fa5f921cb86abcc8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-04-02 11:41:16 +0800

    Update 443_String_Compression.cpp

--- double_pointers/443_String_Compression.cpp
+++ double_pointers/443_String_Compression.cpp
@@ -24,3 +24,29 @@ public:
         return idx;
     }
 };
+
+// 2023.04.02
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int left = 0;
+        int right = 0;
+        int idx = 0;
+        while(right < n) {
+            while(right < n && chars[right] == chars[left]) {
+                right++;
+            }
+            int len = right - left;
+            chars[idx++] = chars[left];
+            if(len > 1) {
+                string temp = to_string(len);
+                for(auto& c : temp) {
+                    chars[idx++] = c;
+                }
+            }
+            left = right;
+        }
+        return idx;
+    }
+};
