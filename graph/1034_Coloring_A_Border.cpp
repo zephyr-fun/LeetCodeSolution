2be8c87d9c7e03ec7e1a4bcf74610a62c4ef1ea4
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-04-04 13:34:33 +0800

    Update 1034_Coloring_A_Border.cpp

--- graph/1034_Coloring_A_Border.cpp
+++ graph/1034_Coloring_A_Border.cpp
@@ -240,3 +240,42 @@ public:
         return grid;
     }
 };
+
+// 2023.04.04
+class Solution {
+public:
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> res(n, vector<int>(m, 0));
+        int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        queue<pair<int, int>> que;
+        que.push({row, col});
+        while(!que.empty()) {
+            auto [x, y] = que.front();
+            que.pop();
+            int cnt = 0;
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dirs[i][0];
+                int ny = y + dirs[i][1];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || grid[x][y] != grid[nx][ny]) {
+                    continue;
+                }
+                cnt++;
+                if(res[nx][ny] != 0) {
+                    continue;
+                }
+                que.push({nx, ny});
+            }
+            res[x][y] = cnt == 4 ? grid[x][y] : color;
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(res[i][j] == 0) {
+                    res[i][j] = grid[i][j];
+                }
+            }
+        }
+        return res;
+    }
+};
