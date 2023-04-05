1007f6829c8f3e7b0cf22b3f98baf1c4a96fc322
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-04-05 11:27:31 +0800

    Update 6192_Number_of_Common_Factors.cpp

--- weekly/weekly-contest-313/6192_Number_of_Common_Factors.cpp
+++ weekly/weekly-contest-313/6192_Number_of_Common_Factors.cpp
@@ -12,3 +12,22 @@ public:
         return res;
     }
 };
+
+// 2023.04.05
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return b ? gcd(b, a % b) : a;
+    }
+    int commonFactors(int a, int b) {
+        int g = gcd(a, b);
+        int res = 0;
+        for(int i = 1; i * i <= g; i++) {
+            if(g % i == 0) {
+                res++;
+                res += i * i < g;
+            }
+        }
+        return res;
+    }
+};
