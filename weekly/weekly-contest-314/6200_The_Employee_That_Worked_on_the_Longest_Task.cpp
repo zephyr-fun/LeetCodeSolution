292e0a6be2c7a6f45297a8262692b44e64f1175e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-05 20:00:06 +0800

    Update 6200_The_Employee_That_Worked_on_the_Longest_Task.cpp

--- weekly/weekly-contest-314/6200_The_Employee_That_Worked_on_the_Longest_Task.cpp
+++ weekly/weekly-contest-314/6200_The_Employee_That_Worked_on_the_Longest_Task.cpp
@@ -16,3 +16,24 @@ public:
         return res;
     }
 };
+
+// 2023.05.05
+// 2432_The_Employee_That_Worked_on_the_Longest_Task
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int m = logs.size();
+        int res = logs[0][0];
+        int length = logs[0][1];
+        for(int i = 1; i < m; i++) {
+            if(logs[i][1] - logs[i - 1][1] > length) {
+                res = logs[i][0];
+                length = logs[i][1] - logs[i - 1][1];
+            }
+            else if(logs[i][1] - logs[i - 1][1] == length) {
+                res = min(res, logs[i][0]);
+            }
+        }
+        return res;
+    }
+};
