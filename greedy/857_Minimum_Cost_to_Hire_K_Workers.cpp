5d58f5d6260eba8207b5a4cf0fc62b64a6cc85a8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-08 15:16:21 +0800

    Update 857_Minimum_Cost_to_Hire_K_Workers.cpp

--- greedy/857_Minimum_Cost_to_Hire_K_Workers.cpp
+++ greedy/857_Minimum_Cost_to_Hire_K_Workers.cpp
@@ -26,3 +26,34 @@ public:
         return res;
     }
 };
+
+// 2023.05.08
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n = quality.size();
+        int id[n];
+        int sum_q = 0;
+        iota(id, id + n, 0);
+        // r1 < r2 < ... < rn
+        sort(id, id + n, [&] (int i, int j) {
+            return wage[i] * quality[j] < wage[j] * quality[i];
+        });
+        priority_queue<int> pque;
+        for(int i = 0; i < k; i++) {
+            pque.push(quality[id[i]]);
+            sum_q += quality[id[i]];
+        }
+        double res = sum_q * (double) wage[id[k - 1]] / quality[id[k - 1]];
+        for(int i = k; i < n; i++) {
+            int q = quality[id[i]];
+            if(q < pque.top()) {
+                sum_q -= pque.top() - q;
+                pque.pop();
+                pque.push(q);
+                res = min(res, sum_q * (double) wage[id[i]] / quality[id[i]]);
+            }
+        }
+        return res;
+    }
+};
