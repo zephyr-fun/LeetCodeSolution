deef69198d45121545517bc14c4096651e6e1dbb
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-10 13:06:13 +0800

    Update 648_Replace_Words.cpp

--- trie/648_Replace_Words.cpp
+++ trie/648_Replace_Words.cpp
@@ -50,3 +50,153 @@ public:
         return res.substr(0, res.size() - 1);
     }
 };
+
+// 2023.05.10
+class Trie {
+public:
+    Trie() {
+        memset(son, 0, sizeof(son));
+        memset(cnt, 0, sizeof(cnt));
+    }
+    void insert(string& word) {
+        int p = 0;
+        for(int i = 0; i < word.size(); i++) {
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                son[p][u] = ++idx;
+            }
+            p = son[p][u];
+        }
+        cnt[p]++;
+    }
+
+    string search(string& word) {
+        int p = 0;
+        string res;
+        for(int i = 0; i < word.size(); i++) {
+            res += word[i];
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                return "";
+            }
+            p = son[p][u];
+            if(cnt[p]) {
+                return res;
+            }
+        }
+        return "";
+    }
+
+    // another way for construct searh
+    string query(string& word) {
+        int p = 0;
+        string res;
+        for(int i = 0; i < word.size(); i++) {
+            res += word[i];
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                break;
+            }
+            p = son[p][u];
+            if(cnt[p]) {
+                return res;
+            }
+        }
+        return word;
+    }
+
+private:
+    int son[100007][26];
+    int cnt[100007];
+    int idx = 0;
+};
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        Trie trie;
+        for(auto dict : dictionary) {
+            trie.insert(dict);
+        }
+        int i = 0;
+        int j = 0;
+        int n = sentence.size();
+        string res = "";
+        while(j < n) {
+            while(j < n && sentence[j] != ' ') {
+                j++;
+            }
+            string cur = sentence.substr(i, j - i);
+            string temp = trie.search(cur);
+            res += temp == "" ? cur : temp;
+            res += ' ';
+            i = j + 1;
+            j = i;
+        }
+        return res.substr(0, res.size() - 1);
+    }
+};
+
+// with another described above way
+class Trie {
+public:
+    Trie() {
+        memset(son, 0, sizeof(son));
+        memset(cnt, 0, sizeof(cnt));
+    }
+    void insert(string word) {
+        int p = 0;
+        for(int i = 0; i < word.size(); i++) {
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                son[p][u] = ++idx;
+            }
+            p = son[p][u];
+        }
+        cnt[p]++;
+    }
+
+    string search(string word) {
+        int p = 0;
+        string res;
+        for(int i = 0; i < word.size(); i++) {
+            res += word[i];
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                break;
+            }
+            p = son[p][u];
+            if(cnt[p]) {
+                return res;
+            }
+        }
+        return word;
+    }
+
+private:
+    int son[100007][26];
+    int cnt[100007];
+    int idx = 0;
+};
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        Trie trie;
+        for(auto dict : dictionary) {
+            trie.insert(dict);
+        }
+        int i = 0;
+        int j = 0;
+        int n = sentence.size();
+        string res = "";
+        while(j < n) {
+            while(j < n && sentence[j] != ' ') {
+                j++;
+            }
+            res += trie.search(sentence.substr(i, j - i));
+            res += ' ';
+            i = j + 1;
+            j = i;
+        }
+        return res.substr(0, res.size() - 1);
+    }
+};
