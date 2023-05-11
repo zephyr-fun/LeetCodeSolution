f2c7e1bcde6ec2587084ec396f63d75496444698
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-11 09:28:22 +0800

    Update 1032_Stream_of_Characters.cpp

--- trie/1032_Stream_of_Characters.cpp
+++ trie/1032_Stream_of_Characters.cpp
@@ -145,3 +145,54 @@ public:
         return false;
     }
 };
+
+// 2023.05.11
+static int son[40007][26];
+static int cnt[40007];
+class StreamChecker {
+public:
+    string cur;
+    int idx;
+    void insert(string& word) {
+        int p = 0;
+        for(int i = word.size() - 1; i >= 0; i--) {
+            int u = word[i] - 'a';
+            if(!son[p][u]) {
+                son[p][u] = ++idx;
+            }
+            p = son[p][u];
+        }
+        cnt[p]++;
+    }
+
+    StreamChecker(vector<string>& words) {
+        idx = 0;
+        memset(son, 0, sizeof(son));
+        memset(cnt, 0, sizeof(cnt));
+        for(auto& word : words) {
+            insert(word);
+        }
+    }
+    
+    bool query(char letter) {
+        cur += letter;
+        int p = 0;
+        for(int i = cur.size() - 1; i >= 0; i--) {
+            int u = cur[i] - 'a';
+            if(!son[p][u]) {
+                return false;
+            }
+            p = son[p][u];
+            if(cnt[p]) {
+                break; // optim
+            }
+        }
+        return cnt[p];
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
