8deebf33b99e0afad08836521b68a2db59db7f6f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-12 16:49:45 +0800

    Update 380_Insert_Delete_GetRandom_O(1).cpp

--- hash/380_Insert_Delete_GetRandom_O(1).cpp
+++ hash/380_Insert_Delete_GetRandom_O(1).cpp
@@ -87,3 +87,51 @@ public:
         return nums[dist(gen)];
     }
 };
+
+
+// 2023.05.12
+static int nums[200007];
+class RandomizedSet {
+public:
+    mt19937 gen;
+    unordered_map<int, int> map;
+    int idx;
+    RandomizedSet() : gen(random_device{}()), idx(0) {}
+    
+    bool insert(int val) {
+        if(map.count(val)) {
+            return false;
+        }
+        nums[idx] = val;
+        map[val] = idx;
+        idx++;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!map.count(val)) {
+            return false;
+        }
+        int temp_idx = map[val];
+        map.erase(val);
+        if(temp_idx != idx - 1) {
+            nums[temp_idx] = nums[idx - 1];
+            map[nums[idx - 1]] = temp_idx;
+        }
+        idx--;
+        return true;
+    }
+    
+    int getRandom() {
+        uniform_int_distribution<> dist(0, idx - 1);
+        return nums[dist(gen)];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
