99ca6c54d2ca697a597b4305c68de67d4054a0e7
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-13 23:32:20 +0800

    Update 6204_Largest_Positive_Integer_That_Exists_With_Its_Negative.cpp

--- weekly/weekly-contest-315/6204_Largest_Positive_Integer_That_Exists_With_Its_Negative.cpp
+++ weekly/weekly-contest-315/6204_Largest_Positive_Integer_That_Exists_With_Its_Negative.cpp
@@ -18,3 +18,21 @@ public:
         return res;
     }
 };
+
+// 2023.05.13
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        unordered_set<int> set;
+        int res = -1;
+        for(auto num : nums) {
+            if(set.count(-num)) {
+                res = max(res, abs(num));
+            }
+            else {
+                set.insert(num);
+            }
+        }
+        return res;
+    }
+};
