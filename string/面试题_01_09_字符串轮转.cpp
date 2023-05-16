8185a1bb18f2053919480e3e5b5f64451f6dc5e8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-16 10:25:40 +0800

    Update 面试题_01_09_字符串轮转.cpp

--- string/面试题_01_09_字符串轮转.cpp
+++ string/面试题_01_09_字符串轮转.cpp
@@ -12,3 +12,29 @@ public:
         return false;
     }
 };
+
+// 2023.05.14
+class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        if(s1.size() != s2.size()) {
+            return false;
+        }
+        else if(s1.size() == 0) {
+            return true;
+        }
+        s1 = s1 + s1;
+        return s1.find(s2) != string::npos;
+    }
+};
+
+class Solution {
+public:
+    bool isFlipedString(string s1, string s2) {
+        if(s1.size() != s2.size()) {
+            return false;
+        }
+        s1 += s1;
+        return s1.find(s2) != string::npos;
+    }
+};
