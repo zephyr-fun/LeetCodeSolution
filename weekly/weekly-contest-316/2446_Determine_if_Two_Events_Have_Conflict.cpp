ec3890e09542bc7994d844e8de9803b53f8e29e8
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-17 10:49:43 +0800

    Update 2446_Determine_if_Two_Events_Have_Conflict.cpp

--- weekly/weekly-contest-316/2446_Determine_if_Two_Events_Have_Conflict.cpp
+++ weekly/weekly-contest-316/2446_Determine_if_Two_Events_Have_Conflict.cpp
@@ -23,3 +23,11 @@ public:
         return true;
     }
 };
+
+// 2023.05.17
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        return !(event1[0] > event2[1] || event1[1] < event2[0]);
+    }
+};
