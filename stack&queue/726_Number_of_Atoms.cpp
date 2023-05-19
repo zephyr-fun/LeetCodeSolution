0435c4f326bfd810472ccc16c28b6d507260da3c
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-19 15:57:21 +0800

    Update 726_Number_of_Atoms.cpp

--- stack&queue/726_Number_of_Atoms.cpp
+++ stack&queue/726_Number_of_Atoms.cpp
@@ -130,3 +130,48 @@ public:
         return res;
     }
 };
+
+// 2023.05.19
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+        int n = formula.size();
+        string form = "";
+        string num = "";
+        int cur = 1;
+        map<string, int> map;
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            if(formula[i] >= '0' && formula[i] <= '9') {
+                num = formula[i] + num;
+            }
+            else if(formula[i] == ')') {
+                int v = num == "" ? 1 : stoi(num);
+                cur *= v;
+                st.push(v);
+                num = "";
+            }
+            else if(formula[i] == '(') {
+                cur /= st.top();
+                st.pop();
+                num = "";
+            }
+            else {
+                form = formula[i] + form;
+                if(formula[i] >= 'A' && formula[i] <= 'Z') {
+                    map[form] += (num == "" ? 1 : stoi(num)) * cur;
+                    form = "";
+                    num = "";
+                }
+            }
+        }
+        string res = "";
+        for(auto& [key, val] : map) {
+            res += key;
+            if(val > 1) {
+                res += to_string(val);
+            }
+        }
+        return res;
+    }
+};
