f9d3b2fddfcd0c7ef799e6d656eab44dec5ca395
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-20 23:21:57 +0800

    Update 1206_Design_Skiplist.cpp

--- skip_list/1206_Design_Skiplist.cpp
+++ skip_list/1206_Design_Skiplist.cpp
@@ -1,5 +1,5 @@
 // 2022.07.26
-// learning process
+// learning
 constexpr int MAX_LEVEL = 32;
 constexpr double P_FACTOR = 0.25;
 
@@ -96,6 +96,81 @@ public:
             lv++;
         }
         return lv;
+    }
+};
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist* obj = new Skiplist();
+ * bool param_1 = obj->search(target);
+ * obj->add(num);
+ * bool param_3 = obj->erase(num);
+ */
+
+// 2023.05.20
+// with yxc version
+class Skiplist {
+public:
+    static const int level = 8;
+
+    struct Node {
+        int val;
+        vector<Node*> next;
+        Node(int val_): val(val_) {
+            next.resize(level, nullptr);
+        }
+    }*head;
+
+    Skiplist() {
+        head = new Node(-1);
+    }
+    
+    ~Skiplist() {
+        delete head;
+    }
+
+    void find(int target, vector<Node*>& pre) {
+        auto p = head;
+        for(int i = level - 1; i >= 0; i--) {
+            while(p->next[i] && p->next[i]->val < target) {
+                p = p->next[i];
+            }
+            pre[i] = p;
+        }
+    }
+
+    bool search(int target) {
+        vector<Node*> pre(level);
+        find(target, pre);
+        auto p = pre[0]->next[0];
+        return p && p->val == target;
+    }
+    
+    void add(int num) {
+        vector<Node*> pre(level);
+        find(num, pre);
+        auto p = new Node(num);
+        for(int i = 0; i < level; i++) {
+            p->next[i] = pre[i]->next[i];
+            pre[i]->next[i] = p;
+            if(rand() % 2) {
+                break;
+            }
+        }
+    }
+    
+    bool erase(int num) {
+        vector<Node*> pre(level);
+        find(num, pre);
+        auto p = pre[0]->next[0];
+        if(!p || p->val != num) {
+            return false;
+        }
+        for(int i = 0; i < level && pre[i]->next[i] == p; i++) {
+            pre[i]->next[i] = p->next[i];
+        }
+        delete p;
+        return true;
     }
 };
 
