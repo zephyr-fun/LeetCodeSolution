911890d1be11b5b521e77d0eca067956583b395f
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-21 10:50:32 +0800

    Update 2104_Sum_of_Subarray_Ranges.cpp

--- monotonic_stack/2104_Sum_of_Subarray_Ranges.cpp
+++ monotonic_stack/2104_Sum_of_Subarray_Ranges.cpp
@@ -40,3 +40,47 @@ public:
         return res;
     }
 };
+
+// 2023.05.21
+// 将子数组转化为nums中的每个数，在多少个子数组中是最大值，在多少个子数组中是最小值，进而计算出每个数对结果的贡献
+class Solution {
+public:
+    int n;
+    long long subArrayRanges(vector<int>& nums) {
+        n = nums.size();
+        vector<long long> minVal = getCnt(nums, true);
+        vector<long long> maxVal = getCnt(nums, false);
+        long long res = 0;
+        for(int i = 0; i < n; i++) {
+            res += (maxVal[i] - minVal[i]) * nums[i];
+        }
+        return res;
+    }
+    vector<long long> getCnt(vector<int>& nums, bool isMin) {
+        stack<int> st;
+        vector<int> a(n, 0);
+        vector<int> b(n, 0);
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && (isMin ? nums[st.top()] >= nums[i] : nums[st.top()] <= nums[i])) {
+                st.pop();
+            }
+            a[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty()) {
+            st.pop();
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && (isMin ? nums[st.top()] > nums[i] : nums[st.top()] < nums[i])) {
+                st.pop();
+            }
+            b[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        vector<long long> res(n, 0);
+        for(int i = 0; i < n; i++) {
+            res[i] = (i - a[i]) * 1LL * (b[i] - i);
+        }
+        return res;
+    }
+};
