7eb9cd6f73ed0f30837fd96baad0ff9fbd06ab2b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-21 10:50:35 +0800

    Update 795_Number_of_Subarrays_with_Bounded_Maximum.cpp

--- double_pointers/795_Number_of_Subarrays_with_Bounded_Maximum.cpp
+++ double_pointers/795_Number_of_Subarrays_with_Bounded_Maximum.cpp
@@ -18,3 +18,25 @@ public:
         return res;
     }
 };
+
+// 2023.05.21
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int n = nums.size();
+        int i0 = -1; // 上一个最近的不能取的大于right的位置
+        int i1 = -1; // 上一个最近的一定要取的在left和right之间的最大值的位置
+        // i1和i0之间维护的就是对于当前枚举的右端点来说可行的左端点区间，累加即为答案
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > right) {
+                i0 = i;
+            }
+            if(nums[i] >= left) {
+                i1 = i;
+            }
+            res += i1 - i0;
+        }
+        return res;
+    }
+};
