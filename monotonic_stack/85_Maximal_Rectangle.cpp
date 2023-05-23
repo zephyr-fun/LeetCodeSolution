cf33d2dc22871c2ee5e68cbb52aeea1116d65e7b
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-23 14:07:17 +0800

    Update 85_Maximal_Rectangle.cpp

--- monotonic_stack/85_Maximal_Rectangle.cpp
+++ monotonic_stack/85_Maximal_Rectangle.cpp
@@ -84,3 +84,45 @@ public:
         return res;
     }
 };
+
+// 2023.05.23
+// 加引用是真的快
+class Solution {
+public:
+    int maxRow(vector<int>& nums) {
+        nums.insert(nums.begin(), -1);
+        nums.push_back(-1);
+        stack<int> st;
+        int n = nums.size();
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && nums[st.top()] > nums[i]) {
+                int val = nums[st.top()];
+                st.pop();
+                int left = st.top();
+                int right = i;
+                res = max(res, (right - left - 1) * val);
+            }
+            st.push(i);
+        }
+        return res;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> temp(n, vector<int>(m, 0));
+        for(int i = 0; i < m; i++) {
+            temp[0][i] = matrix[0][i] == '0' ? 0 : 1;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                temp[i][j] = matrix[i][j] == '0' ? 0 : matrix[i][j] - '0' + temp[i - 1][j];
+            }
+        }
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            res = max(res, maxRow(temp[i]));
+        }
+        return res;
+    }
+};
