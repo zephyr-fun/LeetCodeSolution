6f417164a1c25b770bf5de5775d5a1098486254e
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-05-25 11:02:34 +0800

    Update 591_Tag_Validator.cpp

--- stack&queue/591_Tag_Validator.cpp
+++ stack&queue/591_Tag_Validator.cpp
@@ -72,3 +72,79 @@ public:
         return st.empty();
     }
 };
+
+// 2023.05.25
+// 栈加仔细分类讨论模拟
+class Solution {
+public:
+    string CDATA1 = "<![CDATA[";
+    string CDATA2 = "]]>";
+    bool isValid(string code) {
+        stack<string> st;
+        int size = code.size();
+        int idx = 0;
+        while(idx < size) {
+            if(idx + 8 < size && code.substr(idx, 9) == CDATA1) {
+                if(idx == 0) {
+                    return false;
+                }
+                int supIdx = idx + 9;
+                bool findCDATA2 = false;
+                while(supIdx < size && !findCDATA2) {
+                    if(supIdx + 2 < size && code.substr(supIdx, 3) == CDATA2) {
+                        findCDATA2 = true;
+                    }
+                    else {
+                        supIdx++;
+                    }
+                }
+                if(!findCDATA2) {
+                    return false;
+                }
+                idx = supIdx;
+            }
+            else if(code[idx] == '<') {
+                if(idx == size - 1) {
+                    return false;
+                }
+                bool isEnd = code[idx + 1] == '/';
+                int p = isEnd ? idx + 2 : idx + 1;
+                int supIdx = p;
+                while(supIdx < size && code[supIdx] != '>') {
+                    if(!isupper(code[supIdx])) {
+                        return false;
+                    }
+                    supIdx++;
+                }
+                if(supIdx == size) {
+                    return false;
+                }
+                int len = supIdx - p;
+                if(len < 1 || len > 9) {
+                    return false;
+                }
+                string tagName = code.substr(p, len);
+                idx = supIdx + 1;
+                if(!isEnd) {
+                    st.push(tagName);
+                }
+                else {
+                    if(st.empty() || st.top() != tagName) {
+                        return false;
+                    }
+                    st.pop();
+                    if(st.empty() && idx < size) {
+                        return false;
+                    }
+                }
+            }
+            else {
+                if(idx == 0) {
+                    return false;
+                }
+                idx++;
+            }
+        }
+        return st.empty();
+    }
+};
