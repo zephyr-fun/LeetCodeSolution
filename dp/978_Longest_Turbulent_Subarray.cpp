7e7d9a2826b46d13c547c4243ee0483e0b91aa48
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-02 15:16:01 +0800

    Update 978_Longest_Turbulent_Subarray.cpp

--- dp/978_Longest_Turbulent_Subarray.cpp
+++ dp/978_Longest_Turbulent_Subarray.cpp
@@ -149,3 +149,22 @@ public:
         return res;
     }
 };
+
+// 2023.06.02
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size();
+        int dp[n][2];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        int res = 1;
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = arr[i] > arr[i - 1] ? dp[i - 1][1] + 1 : 1;
+            dp[i][1] = arr[i] < arr[i - 1] ? dp[i - 1][0] + 1 : 1;
+            res = max(res, max(dp[i][0], dp[i][1]));
+        }
+        return res;
+    }
+};
