bec3ace4ecf7995fafe72cf224a7615dc7d63726
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-05 14:40:19 +0800

    Update 583_Delete_Operation_for_Two_Strings.cpp

--- dp/583_Delete_Operation_for_Two_Strings.cpp
+++ dp/583_Delete_Operation_for_Two_Strings.cpp
@@ -83,3 +83,25 @@ public:
         return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
     }
 };
+
+// 2023.06.05
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        int dp[n + 1][m + 1];
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return n + m - 2 * dp[n][m];
+    }
+};
