046ef5e71b63f3d5c7c4e6b2d74b614c97bb9168
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-06 23:13:00 +0800

    Update 2352_Equal_Row_and_Column_Pairs.cpp

--- weekly/weekly-contest-303/2352_Equal_Row_and_Column_Pairs.cpp
+++ weekly/weekly-contest-303/2352_Equal_Row_and_Column_Pairs.cpp
@@ -21,3 +21,35 @@ public:
         return res;
     }
 };
+
+// 2023.06.06
+// O(n^2)
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        map<string, int> map;
+        int n = grid.size();
+        int m = grid[0].size();
+        string temp;
+        for(int i = 0; i < n; i++) {
+            temp = "";
+            for(int j = 0; j < m; j++) {
+                temp += to_string(grid[i][j]);
+                temp += ',';
+            }
+            temp.pop_back();
+            map[temp]++;
+        }
+        int res = 0;
+        for(int i = 0; i < m; i++) {
+            temp = "";
+            for(int j = 0; j < n; j++) {
+                temp += to_string(grid[j][i]);
+                temp += ',';
+            }
+            temp.pop_back();
+            res += map[temp];
+        }
+        return res;
+    }
+};
