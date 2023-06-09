0f495529fad29f74813ef2712090df995dbc6426
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-09 22:40:13 +0800

    Update 437_Path_Sum_III.cpp

--- tree/437_Path_Sum_III.cpp
+++ tree/437_Path_Sum_III.cpp
@@ -108,3 +108,42 @@ public:
         return dfs(root, 0, targetSum);
     }
 };
+
+// 2023.06.09
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int target;
+    int res;
+    unordered_map<long long, int> map;
+
+    void dfs(TreeNode* root, long long sum) {
+        if(root == nullptr) {
+            return ;
+        }
+        long long cur = sum + root->val;
+        res += map[cur - target];
+        map[cur]++;
+        dfs(root->left, cur);
+        dfs(root->right, cur);
+        map[cur]--;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        target = targetSum;
+        res = 0;
+        map[0] = 1;
+        dfs(root, 0);
+        return res;
+    }
+};
