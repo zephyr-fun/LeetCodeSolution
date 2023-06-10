35f126a121813947b23e65650e0060b445d68f17
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-10 21:46:29 +0800

    Update 987_Vertical_Order_Traversal_of_a_Binary_Tree.cpp

--- tree/987_Vertical_Order_Traversal_of_a_Binary_Tree.cpp
+++ tree/987_Vertical_Order_Traversal_of_a_Binary_Tree.cpp
@@ -176,3 +176,43 @@ public:
         return res;
     }
 };
+
+// 2023.06.10
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<int, map<int, multiset<int>>> map;
+
+    void traversal(TreeNode* root, int row, int col) {
+        if(root == nullptr) {
+            return ;
+        }
+        map[col][row].insert(root->val);
+        traversal(root->left, row + 1, col - 1);
+        traversal(root->right, row + 1, col + 1);
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        traversal(root, 0, 0);
+        vector<vector<int>> res;
+        for(auto&[k, v] : map) {
+            vector<int> temp;
+            for(auto&[kk, vv] : v) {
+                vector<int> seg(vv.begin(), vv.end());
+                temp.insert(temp.end(), seg.begin(), seg.end());
+            }
+            res.emplace_back(temp);
+        }
+        return res;
+    }
+};
