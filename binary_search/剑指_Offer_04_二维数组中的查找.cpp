33cde0db79577390ce0554bb573fda0dd4b2cbe0
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-13 22:02:25 +0800

    Update 剑指_Offer_04_二维数组中的查找.cpp

--- binary_search/剑指_Offer_04_二维数组中的查找.cpp
+++ binary_search/剑指_Offer_04_二维数组中的查找.cpp
@@ -49,3 +49,29 @@ public:
         return false;
     }
 };
+
+// 2023.06.13
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() == 0) {
+            return false;
+        }
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0;
+        int col = m - 1;
+        while(row < n && col >= 0) {
+            if(matrix[row][col] > target) {
+                col--;
+            }
+            else if(matrix[row][col] < target) {
+                row++;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
