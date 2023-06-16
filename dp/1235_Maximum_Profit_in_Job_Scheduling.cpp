20ea5d7700cf34979c8c19391306e0e444408c85
fan-xuan-zhe <35688381+zephyr-fun@users.noreply.github.com>
2023-06-16 17:16:05 +0800

    Update 1235_Maximum_Profit_in_Job_Scheduling.cpp

--- dp/1235_Maximum_Profit_in_Job_Scheduling.cpp
+++ dp/1235_Maximum_Profit_in_Job_Scheduling.cpp
@@ -32,3 +32,40 @@ public:
         return dp[n];
     }
 };
+
+// 2023.06.16
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        vector<vector<int>> jobs;
+        int n = startTime.size();
+        for(int i = 0; i < n; i++) {
+            vector<int> temp = {startTime[i], endTime[i], profit[i]};
+            jobs.emplace_back(temp);
+        }
+        sort(jobs.begin(), jobs.end(), [] (vector<int>& a, vector<int>& b) {
+            if(a[1] == b[1]) {
+                return a[0] > b[0];
+            }
+            return a[1] < b[1];
+        });
+        vector<int> dp(n + 1, 0);
+        for(int i = 0; i < n; i++) {
+            int l = 0;
+            int r = i - 1;
+            int pos = -1;
+            while(l <= r) {
+                int mid = l + (r - l) / 2;
+                if(jobs[mid][1] <= jobs[i][0]) {
+                    pos = mid;
+                    l = mid + 1;
+                }
+                else {
+                    r = mid - 1;
+                }
+            }
+            dp[i + 1] = max(dp[i], dp[pos + 1] + jobs[i][2]);
+        }
+        return dp[n];
+    }
+};
