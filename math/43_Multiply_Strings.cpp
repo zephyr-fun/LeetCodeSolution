7ff2a9cc9f53aaef2992fd7fb97848880437dd0b
zephyr <fanxuanzhe0106@gmail.com>
2023-09-12 20:41:54 +0800

    update math/43_Multiply_Strings.cpp

--- math/43_Multiply_Strings.cpp
+++ math/43_Multiply_Strings.cpp
@@ -72,3 +72,53 @@ public:
         return output;
     }
 };
+
+// 2023.09.12
+// 19. N! Plus
+// Big Integer 大数阶乘 
+// we have defined the necessary header files here for this problem.
+// If additional header files are needed in your program, please import here.
+#include <string>
+#include <vector>
+
+string multi(string num1, string num2) {
+    int len1 = num1.size();
+    int len2 = num2.size();
+    vector<int> result(len1 + len2, 0);
+    for (int i = 0; i < len1; i++) {
+        for (int j = 0; j < len2; j++) {
+            result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+        }
+    }
+    for (int i = result.size() - 1; i >= 0; i--) {
+        if (result[i] > 9) {
+            result[i - 1] += result[i] / 10;
+            result[i] = result[i] % 10;
+        }
+    }
+    string output = "";
+    for(int& x : result) {
+        if(output.empty() && x == 0) {
+            continue;
+        }
+        output.push_back(x + '0');
+    }
+    if(output.empty()) {
+        return "0";
+    }
+    return output;
+}
+int main()
+{  
+  // please define the C++ input here. For example: int a,b; cin>>a>>b;;  
+  // please finish the function body here.  
+  // please define the C++ output here. For example:cout<<____<<endl;
+    int num = 0;
+    cin >> num;
+    string result = "1";
+    for (int i = 2; i <= num; i++) {
+        result = multi(result, to_string(i));
+    }
+    cout << result << endl;
+    return 0;
+}
