7b7f45824c4bc09f4d35c5431eca880ea224d3bb
zephyr <fanxuanzhe0106@gmail.com>
2023-09-13 18:37:08 +0800

    update greedy/630_Course_Schedule_III.cpp

--- greedy/630_Course_Schedule_III.cpp
+++ greedy/630_Course_Schedule_III.cpp
@@ -51,10 +51,39 @@ public:
         priority_queue<int> pque;
         for (auto& c : courses) {
             sum += c[0];
-            pque.push(c[0]);
+            pque.push(c[0]); // emplace is much faster than push
             if (sum > c[1]) {
                 sum -= pque.top();
                 pque.pop();
+            }
+        }
+        return pque.size();
+    }
+};
+
+// another way to optim is considering every branches
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int sum = 0;
+        sort(courses.begin(), courses.end(), [] (vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
+        });
+        priority_queue<int> pque;
+        for (auto& c : courses) {
+            if (c[0] > c[1]) {
+                continue;
+            }
+            if (c[0] + sum > c[1]) {
+                if (c[0] < pque.top()) {
+                    sum = sum - pque.top() + c[0];
+                    pque.pop();
+                    pque.emplace(c[0]);
+                }
+            }
+            else {
+                pque.emplace(c[0]);
+                sum += c[0];
             }
         }
         return pque.size();
