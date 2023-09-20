b517e676720ed19a9273cf14b662b211c38a63b4
zephyr <fanxuanzhe0106@gmail.com>
2023-09-20 09:41:39 +0800

    update monotonic_stack/739_Daily_Temperatures.cpp

--- monotonic_stack/739_Daily_Temperatures.cpp
+++ monotonic_stack/739_Daily_Temperatures.cpp
@@ -115,3 +115,21 @@ public:
         return res;
     }
 };
+
+// 2023.09.20
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> st;
+        vector<int> res(n, 0);
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                res[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
