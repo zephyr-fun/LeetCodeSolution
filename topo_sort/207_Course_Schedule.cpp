fecba36181826d764dd3efd6aa7aecb6854f7afb
zephyr <fanxuanzhe0106@gmail.com>
2023-09-22 09:45:00 +0800

    update topo_sort/207_Course_Schedule.cpp

--- topo_sort/207_Course_Schedule.cpp
+++ topo_sort/207_Course_Schedule.cpp
@@ -139,3 +139,37 @@ public:
         return true;
     }
 };
+
+// 2023.09.21
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> g(numCourses);
+        vector<int> in(numCourses);
+        for (auto& pre : prerequisites) {
+            g[pre[1]].emplace_back(pre[0]);
+            in[pre[0]]++;
+        }
+        queue<int> que;
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) {
+                que.push(i);
+            }
+        }
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            for (auto& v : g[u]) {
+                if (--in[v] == 0) {
+                    que.push(v);
+                }
+            }
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
