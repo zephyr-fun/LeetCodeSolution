96e63e6f701288f403a2062fa1cb46a5e2693cac
zephyr <fanxuanzhe0106@gmail.com>
2023-09-22 18:06:41 +0800

    update topo_sort/802_Find_Eventual_Safe_States.cpp

--- topo_sort/802_Find_Eventual_Safe_States.cpp
+++ topo_sort/802_Find_Eventual_Safe_States.cpp
@@ -185,3 +185,47 @@ public:
         return vector<int>(set.begin(), set.end());
     }
 };
+
+// 2023.09.21
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> res;
+        int n = graph.size();
+        vector<int> in(n, 0);
+        for (int i = 0; i < n; i++) {
+            in[i] = graph[i].size();
+        }
+
+        vector<vector<int>> revg(n);
+
+        for (int i = 0; i < n; i++) {
+            for (auto& v : graph[i]) {
+                revg[v].emplace_back(i);
+            }
+        }
+
+        queue<int> que;
+
+        for (int i = 0; i < n; i++) {
+            if (in[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            res.emplace_back(u);
+            for (auto& v : revg[u]) {
+                if (--in[v] == 0) {
+                    que.push(v);
+                }
+            }
+        }
+
+        sort(res.begin(), res.end());
+        
+        return res;
+    }
+};
