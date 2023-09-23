03e9f1d3d154a30d41471d594c43e256deae0667
zephyr <fanxuanzhe0106@gmail.com>
2023-09-23 19:29:24 +0800

    update union_find_set/684_Redundant_Connection.cpp

--- union_find_set/684_Redundant_Connection.cpp
+++ union_find_set/684_Redundant_Connection.cpp
@@ -48,3 +48,60 @@ public:
         return {};
     }
 };
+
+// 2023.09.23
+class UnionFindSet {
+public:
+    UnionFindSet(int n) : parent(n + 1, 0), rank(n + 1, 0), size(n + 1, 1) {
+        for (int i = 0; i < parent.size(); i++) {
+            parent[i] = i;
+        }
+    }
+
+    int Find(int u) {
+        if (u != parent[u]) {
+            parent[u] = Find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if (pu == pv) {
+            return false;
+        }
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else if (rank[pv] < rank[pu]) {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+            rank[pu]++;
+        }
+        return true;
+    }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+    vector<int> size;
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        UnionFindSet set(n);
+        for (auto& edge : edges) {
+            if (!set.Union(edge[0], edge[1])) {
+                return {edge[0], edge[1]};
+            }
+        }
+        return vector<int>();
+    }
+};
